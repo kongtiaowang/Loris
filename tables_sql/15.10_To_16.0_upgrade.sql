58c9ce0ba6f6db4e25d1d10eaa0c3847e916ad0b
Gregory Luneau <gregory.luneau@gmail.com>
2016-05-11 15:49:20 -0400

    IBIS specific Menu cleanup SQL

---
+++ tables_sql/15.10_To_16.0_upgrade.sql
@@ -0,0 +1,5 @@
+update LorisMenu SET Link='genomic_browser/' WHERE Link="main.php?test_name=genomic_browser";
+update LorisMenu SET Link='videos/' WHERE Link="main.php?test_name=videos";
+update LorisMenu SET Link='blind_coding/' WHERE Link="main.php?test_name=blind_coding";
+update LorisMenu SET Link='dataquery/' WHERE Link="/dataquery/";
+
