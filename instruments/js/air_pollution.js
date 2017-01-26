36e55942a29e1df6c6248a270f8956c2be41584e
Gregory Luneau <gluneau.mcin@gmail.com>
2017-01-26 16:14:03 -0500

    field vvalidation

--- instruments/js/air_pollution.js
+++ instruments/js/air_pollution.js
@@ -1,24 +1,90 @@
-$(document).ready(function() {
+ajaxSubmit = function(e) {
+  var formEl = document.getElementById("test_form"),
+      nextpageNode = document.getElementById("nextpage"),
+      prevpageNode = document.getElementById("prevpage"),
+      nextPage;
 
-  $('[name="city"]').click(function() {
+  if(e.currentTarget.id === 'savecontinue') {
+      nextPage = nextpageNode.textContent;
+  } else if(e.currentTarget.id === 'finalize') {
+      nextPage = 'complete';
+  } else if(e.currentTarget.id === 'complete') {
+      nextPage = 'complete';
+  } else if (e.currentTarget.id === 'goback') {
+      nextPage = prevpageNode.textContent;
+  }
+  $("<input>").attr({
+      type: 'hidden',
+      name: 'nextpage',
+      value: nextPage
+  }).appendTo("#test_form");
+
+  //formEl.action = document.documentURI;
+  formEl.action = "survey.php?key=" + document.getElementById("key").textContent + "&pageNum=complete";
+  console.log(formEl.action);
+
+  $("#test_form").submit();
+}
+
+$(document).ready(function() {
+  console.log("ready");
+
+//  var formEl = document.getElementById("test_form");
+//  formEl.action = "survey.php?key=" + document.getElementById("key").textContent + "&pageNum=complete"; // + "&pageNum=finalpage";
+//  console.log(formEl.action);
+  
+  $("#finalize").unbind( "click" );
+  console.log("unbind click");
+
+  $("#finalize").click(function(e) {
+    console.log("save clicked");
 
     // Check user inputs
-
-    // all class to be encrypted
-    $('.encrypt').each(function() {
-      var crypted = sjcl.encrypt($('[name="code"]').val(), $(this).val());
-      $(this).val(crypted);
-      $(this).attr("disabled", "disabled");
+    var errors = 0;
+    
+    if ( !$('[name="code"]').val() ) { errors++; }
+    if ( !$('[name="Date_taken[Y]"]').val() ) { errors++; }
+    if ( !$('[name="Date_taken[M]"]').val() ) { errors++; }
+    if ( !$('[name="Date_taken[d]"]').val() ) { errors++; }   
+    $('.encrypt').each(function() { 
+      if ( !$(this).val() ) { errors++; }
     });
+    if ( !$('[name="address_line2"]').val() ) { errors--; }
+    
+    if(errors > 0) {
+      alert("Please fill in all required fields");
+      return false;
+    } else {
+    
+        // are you sure?
+        var question = "Are you sure all fields are filled correctly and OK to be submitted?";
+        var status = confirm(question);
+        
+        if( status !== false ){
+            // all class to be encrypted
+            $('.encrypt').each(function() {
+              var crypted = sjcl.encrypt($('[name="code"]').val(), $(this).val());
+              $(this).val(crypted);
 
-    // text area exception
-    var crypted = sjcl.encrypt($('[name="code"]').val(), $('[name="additional_comments"]').val());
-    $('[name="additional_comments"]').val(crypted);
-    $('[name="additional_comments"]').attr("disabled" , "disabled");
+              // rename encrypt field to be submitable
+              $(this).attr('name', $(this).attr('name').replace('_remove',''));
+            });
 
-    // empty code before submission
-    $('[name="code"]').val("*Deleted for Submission*");
-    $('[name="code"]').attr("disabled" , "disabled");
+            // text area exception
+            var crypted = sjcl.encrypt($('[name="code"]').val(), $('[name="additional_comments"]').val());
+            $('[name="additional_comments"]').val(crypted);
+
+            // empty code before submission
+            $('[name="code"]').val("*Deleted for Submission*");
+            console.log("fields crypted, submitting..."); //status is TRUE
+
+            ajaxSubmit(e);
+        } else {
+            console.log("User chose to cancel at this point, since satus is " + status); //status is FALSE
+            return false;
+        }
+    }
   });
-
+  
 });
+
