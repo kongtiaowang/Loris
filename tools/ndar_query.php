bbc35959f6488eb81bdd535366d2768d7784a0dc
Gregory Luneau <gluneau.mcin@gmail.com>
2017-04-05 14:03:16 -0400

    basic ndar changes for 2017 release

--- tools/ndar_query.php
+++ tools/ndar_query.php
@@ -3,7 +3,7 @@ set_include_path(get_include_path().":../libraries:../../php/libraries:");
 require_once '../../vendor/autoload.php';
 require_once "Database.class.inc";
 require_once "Utility.class.inc";
-require_once "NDAR_Release_2016.class.inc";
+require_once "NDAR_Release_2017.class.inc";
 
 $client = new NDB_Client();
 $client->makeCommandLine();
@@ -11,19 +11,29 @@ $client->initialize('../config.xml');
 
 $db        =& Database::singleton();
 
-//$IBIS = array('charge');
-$IBIS = array ('adi_r_proband', 'aosi','aosi','csbs','edi','fyi','head_measurements_subject','ibq_r','m_chat_proband','macarthur_words_gestures','mullen','prefrontal_task','rbs_r','scq_proband','scq_subject','seq','vineland_proband','vineland_subject','charge');
-foreach($IBIS as $instrument) {
-    $run = new NDAR_Release_2016 ($instrument, 1);
-    $run->run();
+$run_ibis1 = true;
+$run_ibis2 = false;
+
+if ($run_ibis1) 
+{
+  // $IBIS = array('vineland_subject');
+  $IBIS = array ('adi_r_proband','aosi','csbs','edi','fyi','ibq_r','m_chat_proband','macarthur_words_gestures','mullen','prefrontal_task','rbs_r','scq_proband','scq_subject','seq','vineland_proband','vineland_subject','charge');
+  foreach($IBIS as $instrument) {
+      $run = new NDAR_Release_2017 ($instrument, 1);
+      $run->run();
+  }
 }
 
-//$IBIS2 = array('edi2');
-$IBIS2 = array('adi_r_proband','aosi','csbs','head_measurements_subject','ibq_r','macarthur_words_gestures', 'm_chat_proband','ndar_subject', 'scq_proband','mullen','rbs_r','vineland_proband','charge','edi2');
-foreach($IBIS2 as $instrument) {
-    $run = new NDAR_Release_2016 ($instrument, 2);
-    $run->run();
+if ($run_ibis2) 
+{
+  //$IBIS2 = array('edi2');
+  $IBIS2 = array('adi_r_proband','aosi','csbs','head_measurements_subject','ibq_r','macarthur_words_gestures', 'm_chat_proband','ndar_subject', 'scq_proband','mullen','rbs_r','vineland_proband','charge','edi2');
+  foreach($IBIS2 as $instrument) {
+      $run = new NDAR_Release_2017 ($instrument, 2);
+      $run->run();
 
+  }
 }
+
 ?>
 
