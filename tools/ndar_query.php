bc545ac92e169f4e910745bdaeb92840f339dae9
Gregory Luneau <gluneau.mcin@gmail.com>
2017-04-11 13:52:36 -0400

    ndar changes for ibis2 run

--- tools/ndar_query.php
+++ tools/ndar_query.php
@@ -11,23 +11,23 @@ $client->initialize('../config.xml');
 
 $db        =& Database::singleton();
 
-$run_ibis1 = true;
-$run_ibis2 = false;
+$run_ibis1 = false;
+$run_ibis2 = true;
 
-if ($run_ibis1) 
+if ($run_ibis1)
 {
-  // $IBIS = array('ados_module1','ados2_module1','ados2_module2','ados2_module3');
-  $IBIS = array ('ados_module1','ados2_module1','ados2_module2','ados2_module3','adi_r_proband','aosi','csbs','edi','fyi','ibq_r','m_chat_proband','macarthur_words_gestures','mullen','prefrontal_task','rbs_r','scq_proband','scq_subject','seq','vineland_proband','vineland_subject','charge');
+  // $IBIS = array('ados_module1', 'ados_module2', 'ados_module3', 'ados2_module1','ados2_module2','ados2_module3');
+  $IBIS = array ('ados_module1', 'ados_module2', 'ados_module3', 'ados2_module1','ados2_module2','ados2_module3','adi_r_proband','aosi','csbs','edi','fyi','ibq_r','m_chat_proband','macarthur_words_gestures','mullen','prefrontal_task','rbs_r','scq_proband','scq_subject','seq','vineland_proband','vineland_subject','charge');
   foreach($IBIS as $instrument) {
       $run = new NDAR_Release_2017 ($instrument, 1);
       $run->run();
   }
 }
 
-if ($run_ibis2) 
+if ($run_ibis2)
 {
   //$IBIS2 = array('edi2');
-  $IBIS2 = array('adi_r_proband','aosi','csbs','head_measurements_subject','ibq_r','macarthur_words_gestures', 'm_chat_proband','ndar_subject', 'scq_proband','mullen','rbs_r','vineland_proband','charge','edi2');
+  $IBIS2 = array('ados_module1', 'ados_module2', 'ados_module3', 'ados2_module1','ados2_module2','ados2_module3','adi_r_proband','aosi','csbs','head_measurements_subject','ibq_r','macarthur_words_gestures', 'm_chat_proband','ndar_subject', 'scq_proband','mullen','rbs_r','vineland_proband','charge','edi2');
   foreach($IBIS2 as $instrument) {
       $run = new NDAR_Release_2017 ($instrument, 2);
       $run->run();
