bb09a83efb9d56a3dcfd8f00da3ef514345503b0
Sruthy Mathew <sruthy.mcin@gmail.com>
2017-04-27 10:56:13 -0400

    Add CISS to DS project at V03, V06,V12,V15

---
+++ tables_sql/20170427_add_ciss_DS.sql
@@ -0,0 +1,9 @@
+--  add CISS to DS project at V03, V06,V12,V15
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ciss',0,0,'Y','Visit',11,'V03') ;
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ciss',0,0,'Y','Visit',11,'V06') ;
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ciss',0,0,'Y','Visit',11,'V12') ;
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ciss',0,0,'Y','Visit',11,'V15') ;
