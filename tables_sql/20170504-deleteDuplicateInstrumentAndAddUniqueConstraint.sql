6f1d860d1f7e4e9ea8c3782ed7a6fc7f414ae38f
Gregory Luneau <gluneau.mcin@gmail.com>
2017-05-04 11:57:26 -0400

    Delete Duplicate Instrument And Add Unique Constraint

---
+++ tables_sql/20170504-deleteDuplicateInstrumentAndAddUniqueConstraint.sql
@@ -0,0 +1,6 @@
+DELETE FROM test_battery WHERE ID=611 ;
+DELETE FROM test_battery WHERE ID=622 ;
+DELETE FROM test_battery WHERE ID=942 ;
+DELETE FROM test_battery WHERE ID=953 ;
+ALTER TABLE test_battery ADD UNIQUE `unique_test` (`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`, `CenterID`, `firstVisit`, `instr_order`, `ProjectID`);
+
