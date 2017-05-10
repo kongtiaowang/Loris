dd6ec40bae8749f65e85f5889ed94d92cead5a37
Sruthy Mathew <sruthy.mcin@gmail.com>
2017-05-10 13:00:13 -0400

    Add BSRC to DS V03

---
+++ tables_sql/20170510_add_BSRC_V03_DS.sql
@@ -0,0 +1,3 @@
+--  add BSRC to DS subproject at V03
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('BSRC',0,0,'Y','Visit',11,'V03') ;
