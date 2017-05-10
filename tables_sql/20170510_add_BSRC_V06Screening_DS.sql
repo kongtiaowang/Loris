f87f54b9eafc97aea9af70943acf59d83afddbfe
Sruthy Mathew <sruthy.mcin@gmail.com>
2017-05-10 15:18:12 -0400

    Adding BSRC to V03 & Screening for DS

---
+++ tables_sql/20170510_add_BSRC_V06Screening_DS.sql
@@ -0,0 +1,5 @@
+--  add BSRC to DS subproject at V06 & Screening
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('BSRC',0,0,'Y','Visit',11,'V06') ;
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('BSRC',0,0,'Y','Visit',11,'Screening') ;
