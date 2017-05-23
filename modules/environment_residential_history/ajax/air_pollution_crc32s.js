51bef2db8e199cfdfc43ea89b3730b9294b72434
Gregory Luneau <gluneau.mcin@gmail.com>
2017-05-23 13:09:02 -0400

    CRC-32 code from STL

--- modules/environment_residential_history/ajax/air_pollution_crc32s.js
+++ modules/environment_residential_history/ajax/air_pollution_crc32s.js
@@ -1,5 +1,6 @@
 [
   {"site": "DCC", "code": 2615402659},
+  {"site": "STL", "code": 2848567914},
   {"site": "SEA", "code": 4086070377},
   {"site": "PHI", "code": 1317682923},
   {"site": "UNC", "code": 3608339031}
