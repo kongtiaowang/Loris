ed8c5582aaea4fe90b672b9140ecc45c1d99a9c3
kongtiaowang <kongtiaowangshen@gmail.com>
2017-06-08 10:51:04 -0400

    react

--- modules/training/js/trainIndex.js
+++ modules/training/js/trainIndex.js
@@ -8,8 +8,84 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var TrainIndex = function (_React$Component) {
-  _inherits(TrainIndex, _React$Component);
+var Title = function (_React$Component) {
+  _inherits(Title, _React$Component);
+
+  function Title() {
+    _classCallCheck(this, Title);
+
+    return _possibleConstructorReturn(this, (Title.__proto__ || Object.getPrototypeOf(Title)).apply(this, arguments));
+  }
+
+  _createClass(Title, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "h1",
+          null,
+          " ",
+          this.props.title,
+          " "
+        )
+      );
+    }
+  }]);
+
+  return Title;
+}(React.Component);
+
+var Footer = function (_React$Component2) {
+  _inherits(Footer, _React$Component2);
+
+  function Footer() {
+    _classCallCheck(this, Footer);
+
+    return _possibleConstructorReturn(this, (Footer.__proto__ || Object.getPrototypeOf(Footer)).apply(this, arguments));
+  }
+
+  _createClass(Footer, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        "This is a footer!"
+      );
+    }
+  }]);
+
+  return Footer;
+}(React.Component);
+
+var Header = function (_React$Component3) {
+  _inherits(Header, _React$Component3);
+
+  function Header() {
+    _classCallCheck(this, Header);
+
+    return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
+  }
+
+  _createClass(Header, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(Title, { title: this.props.title }),
+        "This is a Header!"
+      );
+    }
+  }]);
+
+  return Header;
+}(React.Component);
+
+var TrainIndex = function (_React$Component4) {
+  _inherits(TrainIndex, _React$Component4);
 
   function TrainIndex() {
     _classCallCheck(this, TrainIndex);
@@ -20,11 +96,11 @@ var TrainIndex = function (_React$Component) {
   _createClass(TrainIndex, [{
     key: "render",
     value: function render() {
-      // Waiting for async data to load
       return React.createElement(
-        "button",
+        "div",
         null,
-        "Loading"
+        React.createElement(Header, { title: "nihao wow!!!" }),
+        React.createElement(Footer, null)
       );
     }
   }]);
