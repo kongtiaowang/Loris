eddc796e6ec32f7c642867c2f33a24a85c217d97
kongtiaowang <kongtiaowangshen@gmail.com>
2017-06-13 15:33:44 -0400

    f

--- modules/brainbrowser/__tests__/Brainbrowser.test.js
+++ modules/brainbrowser/__tests__/Brainbrowser.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  Brainbrowser from '../jsx/Brainbrowser';
+import  BrainBrowser from '../jsx/Brainbrowser';
 
-describe('Brainbrowser', () => {
+describe('BrainBrowser', () => {
     it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<Brainbrowser />, div);
+        ReactDOM.render(<BrainBrowser />, div);
     });
 });
