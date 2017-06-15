6231699d91972bab7d9cda78ec8359b31955ae42
Gregory Luneau <gluneau.mcin@gmail.com>
2017-06-15 13:39:22 -0400

    new batteries

---
+++ tables_sql/20170615_screening_ace_batteries.sql
@@ -0,0 +1,7 @@
+INSERT INTO `test_battery` (`Test_name`,`AgeMinDays`,  `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)  VALUES ('ACESubjectMedicalHistory',0,0,'Y','Visit',9,'Screening') ;
+INSERT INTO `test_battery` (`Test_name`,`AgeMinDays`,  `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)  VALUES ('ACEFamilyMedicalHistory',0,0,'Y','Visit',9,'Screening') ;
+INSERT INTO `test_battery` (`Test_name`,`AgeMinDays`,  `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)  VALUES ('ACESubjectPhysicalExam',0,0,'Y','Visit',9,'Screening') ;
+INSERT INTO `test_battery` (`Test_name`,`AgeMinDays`,  `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)  VALUES ('ACESubjectMedicalHistory',0,0,'Y','Visit',10,'Screening') ;
+INSERT INTO `test_battery` (`Test_name`,`AgeMinDays`,  `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)  VALUES ('ACEFamilyMedicalHistory',0,0,'Y','Visit',10,'Screening') ;
+INSERT INTO `test_battery` (`Test_name`,`AgeMinDays`,  `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)  VALUES ('ACESubjectPhysicalExam',0,0,'Y','Visit',10,'Screening') ;
+
