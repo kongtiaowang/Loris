d79d7f04c27bc01d7e4d2003fe814db375b618c9
kongtiaowang <kongtiaowangshen@gmail.com>
2017-06-19 16:24:35 -0400

    Contact

--- modules/training/jsx/ContactList.js
+++ modules/training/jsx/ContactList.js
@@ -1,18 +1,39 @@
 import Contact from "./Contact";
 class ContactList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {search: '',
+                  searchphone:''};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+    this.handleChangephone = this.handleChangephone.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({search: event.target.value});
+  }
+  handleChangephone(event) {
+    this.setState({searchphone: event.target.value});
+  }
 
       render() {
+       let filterContacts = this.props.contacts.filter(
+           (contact) => {
+               return contact.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+           }  
+       );
+
        return (
         <div>
           <h1> Training List </h1>
           <ul>
-             {this.props.contacts.map((contact)=> {
-             //console.log("map loop");             
+             {filterContacts.map((contact)=> {
               return <Contact contact={contact} key={contact.name} />
-              // return <li>eeee</li>
-//https://www.youtube.com/watch?v=It9iL4EXFWc
              })}
           </ul>
+          <input type="text" value={this.state.search} onChange={this.handleChange} />
+          <input type="text" value={this.state.searchphone} onChange={this.handleChangephone} />
         </div>
        );
       }
