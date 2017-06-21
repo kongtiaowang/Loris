be58047d6e8223d466de64c50078bc766f7e8c36
kongtiaowang <kongtiaowangshen@gmail.com>
2017-06-21 12:37:37 -0400

    index

---
+++ modules/module_distribution/jsx/index.js
@@ -0,0 +1,15 @@
+class Demo extends React.Component {
+
+  render() {
+      return (
+        <div>hello world</div>
+      );
+    }
+
+}
+
+$(function() {
+
+  ReactDOM.render("hello", document.getElementById("lorisworkspace"));
+
+});
