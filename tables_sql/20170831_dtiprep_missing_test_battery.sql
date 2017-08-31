7394bfbff38aed18c38763035e36a1074f116b0d
Gregory Luneau <gluneau.mcin@gmail.com>
2017-08-31 14:08:18 -0400

    adding missing test batteries

---
+++ tables_sql/20170831_dtiprep_missing_test_battery.sql
@@ -0,0 +1,16 @@
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('dtiprep', 0, 0, 'Y', 'Visit', 1, 'V06', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('dtiprep', 0, 0, 'Y', 'Visit', 3, 'V06', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('dtiprep', 0, 0, 'Y', 'Visit', 10, 'V06', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('dtiprep', 0, 0, 'Y', 'Visit', 1, 'V12', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('dtiprep', 0, 0, 'Y', 'Visit', 3, 'V12', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('dtiprep', 0, 0, 'Y', 'Visit', 4, 'V12', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('dtiprep', 0, 0, 'Y', 'Visit', 9, 'V12', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('dtiprep', 0, 0, 'Y', 'Visit', 4, 'V18', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('dtiprep', 0, 0, 'Y', 'Visit', 5, 'V18', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('dtiprep', 0, 0, 'Y', 'Visit', 1, 'V24', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('dtiprep', 0, 0, 'Y', 'Visit', 2, 'V24', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('dtiprep', 0, 0, 'Y', 'Visit', 3, 'V24', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('dtiprep', 0, 0, 'Y', 'Visit', 1, 'V36', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('dtiprep', 0, 0, 'Y', 'Visit', 2, 'V36', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('dtiprep', 0, 0, 'Y', 'Visit', 3, 'V36', NULL, NULL, NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID, firstVisit, instr_order) VALUES('dtiprep', 0, 0, 'Y', 'Visit', 5, 'V36', NULL, NULL, NULL);
