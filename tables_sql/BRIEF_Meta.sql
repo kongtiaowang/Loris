cb01335dd065e1f2046c5c7b7b542c5171234504
suzanne-lee <suzannelee.mcin@gmail.com>
2017-09-21 11:40:37 -0400

    Behaviour Rating Inventory of Executive Function (BRIEF) Instrument (#717)

---
+++ tables_sql/BRIEF_Meta.sql
@@ -0,0 +1,8 @@
+INSERT INTO test_names (Test_name, Full_name, Sub_group, IsDirectEntry) VALUES ('BRIEF', 'Behavior Rating Inventory of Executive Function', '1','1');
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('BRIEF', 'BRIEF_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('BRIEF', 'BRIEF_page2', "Page 2", 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('BRIEF', 'BRIEF_page3', "Page 3", 3);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('BRIEF', '0', '0', 'Y', 'Visit', '12', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('BRIEF', '0', '0', 'Y', 'Visit', '13', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('BRIEF', '0', '0', 'Y', 'Visit', '14', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('BRIEF', '0', '0', 'Y', 'Visit', '15', 'VSA', NULL);
