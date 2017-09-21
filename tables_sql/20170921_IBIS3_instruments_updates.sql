120537fdc1eb9f56e8ddff2d8d26c784cc1333d6
sruthymathew123 <sruthy.mcin@gmail.com>
2017-09-21 12:46:29 -0400

    IBIS3 SQL patches update  (#726)
    * IBIS3 SQL patches with no complications
    
    * fixing spacing issues
    
    * deleting misunderstood file

--- tables_sql/20170921_IBIS3_instruments_updates.sql
+++ tables_sql/20170921_IBIS3_instruments_updates.sql
@@ -3,10 +3,10 @@ UPDATE `test_names` SET `IsDirectEntry`=1 WHERE `Test_name`='CBCL';
 UPDATE `test_names` SET `IsDirectEntry`=1 WHERE `Test_name`='rbs_r';
 UPDATE `test_names` SET `IsDirectEntry`=1 WHERE `Test_name`='ADULT_SELF_REPORT';
 
---ASR instrument should populate across all subprojects for VSA in IBIS3
---Update already inserted  with ASR to Active 'N' to avoid confusions
+-- ASR instrument should populate across all subprojects for VSA in IBIS3
+-- Update already inserted  with ASR to Active 'N' to avoid confusions
 UPDATE `test_battery` SET `Active` ='N' WHERE `Test_name`='ADULT_SELF_REPORT';
---fresh and correct test battery inserts for ASR
+-- fresh and correct test battery inserts for ASR
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('ADULT_SELF_REPORT', '0', '0', 'Y', 'Visit', '12', 'VSA', NULL);
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('ADULT_SELF_REPORT', '0', '0', 'Y', 'Visit', '13', 'VSA', NULL);
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('ADULT_SELF_REPORT', '0', '0', 'Y', 'Visit', '14', 'VSA', NULL);
