3946c2ca10c6666b8febde73ccc0b7079f6e51b2
Liza Levitis <elizabeth.levitis@mail.mcgill.ca>
2017-09-21 11:44:56 -0400

    adding all files for CDI Words and Sentences instrument (#723)

---
+++ tables_sql/CDI_Words_Sentences_Meta.sql
@@ -0,0 +1,17 @@
+INSERT INTO test_names(Test_name, Full_name, Sub_Group) VALUES('CDI_Words_Sentences', "MacArthur-Bates CDI: Words and Sentences", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CDI_Words_Sentences','CDI_Words_Sentences_page1', "I - A. (1-4)", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CDI_Words_Sentences','CDI_Words_Sentences_page2', "I - A. (5-7)", 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CDI_Words_Sentences','CDI_Words_Sentences_page3', "I - A. (8-10)", 3);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CDI_Words_Sentences','CDI_Words_Sentences_page4', "I - A. (11-14)", 4);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CDI_Words_Sentences','CDI_Words_Sentences_page5', "I - A. (15-19)", 5);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CDI_Words_Sentences','CDI_Words_Sentences_page6', "I - A. (20-22)", 5);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CDI_Words_Sentences','CDI_Words_Sentences_page7', "I - B. How Children Use Words", 7);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CDI_Words_Sentences','CDI_Words_Sentences_page8', "II - Sentences and Grammar", 8);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('CDI_Words_Sentences', '0', '0', 'Y', 'Visit', 12, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('CDI_Words_Sentences', '0', '0', 'Y', 'Visit', 13, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('CDI_Words_Sentences', '0', '0', 'Y', 'Visit', 14, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('CDI_Words_Sentences', '0', '0', 'Y', 'Visit', 15, 'VSA', NULL);
