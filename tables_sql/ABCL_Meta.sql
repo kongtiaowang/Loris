bc1616b5bc0b2625426abac289b6bb1c855855ed
suzanne-lee <suzannelee.mcin@gmail.com>
2017-09-21 11:41:14 -0400

    Adult Behavior Checklist (ABCL) instrument (#718)

---
+++ tables_sql/ABCL_Meta.sql
@@ -0,0 +1,12 @@
+INSERT INTO test_names (Test_name, Full_name, Sub_group) VALUES ('ABCL', 'Adult Behavior Checklist For Ages 18-59', '1');
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('ABCL', 'ABCL_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('ABCL', 'ABCL_page2', "Page 2", 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('ABCL', 'ABCL_page3', "Page 3", 3);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('ABCL', 'ABCL_page4', "Page 4", 4);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('ABCL', 'ABCL_page5', "Page 5", 5);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('ABCL', 'ABCL_page6', "Page 6", 6);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('ABCL', '0', '0', 'Y', 'Visit', '12', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('ABCL', '0', '0', 'Y', 'Visit', '13', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('ABCL', '0', '0', 'Y', 'Visit', '14', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('ABCL', '0', '0', 'Y', 'Visit', '15', 'VSA', NULL);
+
