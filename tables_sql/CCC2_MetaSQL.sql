fdfadef1ecb7e7e0fcb349feb2e3d30e106196ff
sruthymathew123 <sruthy.mcin@gmail.com>
2017-09-28 10:15:48 -0400

    [Instrument] CCC2- Children's Communication Checklist (#728)

---
+++ tables_sql/CCC2_MetaSQL.sql
@@ -0,0 +1,7 @@
+INSERT INTO test_names (Test_name, Full_name, Sub_group, IsDirectEntry) VALUES ('CCC2', 'Children Communication Checklist 2', '1', '1');
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CCC2', 'CCC2_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CCC2', 'CCC2_page2', "Page 2", 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CCC2', 'CCC2_page3', "Page 3", 3);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CCC2', 'CCC2_page4', "Page 4", 4);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('CCC2', '0', '0', 'Y', 'Visit', '12', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('CCC2', '0', '0', 'Y', 'Visit', '13', 'VSA', NULL);
