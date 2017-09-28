acf93ebb1d11a49159f05306f1a79a53b9bb910e
Liza Levitis <elizabeth.levitis@mail.mcgill.ca>
2017-09-28 10:18:00 -0400

    [Instruments] Connors Parent & Connors Teacher (#730)

---
+++ tables_sql/connors_teacher_meta.sql
@@ -0,0 +1,6 @@
+INSERT INTO test_names(Test_name, Full_name, Sub_Group, IsDirectEntry) VALUES('connors_teacher', "CONNORS 3 - Teacher Short", 1, 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('connors_teacher','connors_teacher_page1', "Page 1", 1);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('connors_teacher', '0', '0', 'Y', 'Visit', 12, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('connors_teacher', '0', '0', 'Y', 'Visit', 13, 'VSA', NULL);
