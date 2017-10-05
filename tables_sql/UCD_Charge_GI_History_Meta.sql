1bd92993b3b36dd2740cca7831bdc41201c6d63f
Liza Levitis <elizabeth.levitis@mail.mcgill.ca>
2017-10-05 11:03:28 -0400

    [Instruments] UCD Charge GI History  (#736)
    * UCD Charge GI History instrument coded and tested
    
    * added description

---
+++ tables_sql/UCD_Charge_GI_History_Meta.sql
@@ -0,0 +1,6 @@
+INSERT INTO test_names(Test_name, Full_name, Sub_Group) VALUES('UCD_Charge_GI_History', "CHARGE GI History", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('UCD_Charge_GI_History','UCD_Charge_GI_History_page1', "Page 1", 1);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('UCD_Charge_GI_History', '0', '0', 'Y', 'Visit', 12, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('UCD_Charge_GI_History', '0', '0', 'Y', 'Visit', 13, 'VSA', NULL);
