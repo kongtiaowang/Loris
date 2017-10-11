26d06c180523871b49499c54221f481308677ae3
suzanne-lee <suzannelee.mcin@gmail.com>
2017-10-11 15:51:31 -0400

    ABC (Aberrant Behavior Checklist) Instrument  (#738)
    * abberant behavior checklist instrument class, sql, and Meta
    
    * aberrant behavior checklist instrument class, SQL and Meta file
    
    * removing subproject 14 and 15 from Meta file

---
+++ tables_sql/Aberrant_Behavior_Checklist_Meta.sql
@@ -0,0 +1,5 @@
+INSERT INTO test_names (Test_name, Full_name, Sub_group) VALUES ('Aberrant_Behavior_Checklist', 'ABC (Aberrant Behavior Checklist)', '1');
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Aberrant_Behavior_Checklist', 'Aberrant_Behavior_Checklist_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Aberrant_Behavior_Checklist', 'Aberrant_Behavior_Checklist_page2', "Page 2", 2);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Aberrant_Behavior_Checklist', '0', '0', 'Y', 'Visit', '12', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Aberrant_Behavior_Checklist', '0', '0', 'Y', 'Visit', '13', 'VSA', NULL);
