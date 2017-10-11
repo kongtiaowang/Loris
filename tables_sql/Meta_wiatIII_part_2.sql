408edc9121cb9cea90f54ab81b82f3f9e3764bda
Leo Thomas <leothomas@users.noreply.github.com>
2017-10-11 15:57:52 -0400

    WiatIII (Part 1 & 2) - code and SQL schema (#711)
    * Adult self report, some issues with disabling questions based on previous answers
    
    * 17/52 pages on wiat3
    
    * initial code and schema for wiatIII
    
    * instrument code and sql
    
    * remove adult self report from pull request
    
    * finished through math problem section
    
    * reformatted listening comprehension section
    
    * reformatted first 2 sections
    
    * added sentence building and word reading sections
    
    * page 25/52
    
    * added pseudoword decoding
    
    * added database insert statement to tables_sql file
    
    * removed trailing bracket
    
    * scoring inital commit
    
    * initial things
    
    * more..Its never gonna finish
    
    * all done instrument pages
    
    * instrument into two
    
    * almost final
    
    * adding missed
    
    * again

---
+++ tables_sql/Meta_wiatIII_part_2.sql
@@ -0,0 +1,11 @@
+INSERT INTO test_names (Test_name, Full_name, Sub_group) VALUES ('wiatIII_part_2', 'WiatIII PART 2', '1');
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('wiatIII_part_2', 'wiatIII_part_2_page1', 'Essay Composition', 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('wiatIII_part_2', 'wiatIII_part_2_page2', 'PseudoWord Decoding', 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('wiatIII_part_2',' wiatIII_part_2_page3', 'Numerical Operations', 3);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('wiatIII_part_2', 'wiatIII_part_2_page4', 'Oral Expression', 4);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('wiatIII_part_2', 'wiatIII_part_2_page5', 'Oral Reading Fluency', 5);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('wiatIII_part_2', 'wiatIII_part_2_page6', 'Spelling', 6);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('wiatIII_part_2', 'wiatIII_part_2_page7', 'Math Fluency', 7);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('wiatIII_part_2', '0', '0', 'Y', 'Visit', '12', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('wiatIII_part_2', '0', '0', 'Y', 'Visit', '13', 'VSA', NULL);
+
