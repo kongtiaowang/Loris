77f3d9cb04fb1d08c7b5360a9088cd62ed3f1850
Liza Levitis <elizabeth.levitis@mail.mcgill.ca>
2017-10-19 11:05:30 -0400

    Fix broken SQL patches (#748)
    Add isDirectEntry to SQL patches where column count didn't match values count

--- tables_sql/MASC2_Parent_Meta.sql
+++ tables_sql/MASC2_Parent_Meta.sql
@@ -1,4 +1,4 @@
-INSERT INTO test_names(Test_name, Full_name, Sub_Group) VALUES('MASC2_Parent', "MASC2: PARENT", 1, 1);
+INSERT INTO test_names(Test_name, Full_name, Sub_Group, isDirectEntry) VALUES('MASC2_Parent', "MASC2: PARENT", 1, 1);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('MASC2_Parent','MASC2_Parent_page1', "Page 1", 1);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('MASC2_Parent','MASC2_Parent_page2', "Page 2", 2);
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
