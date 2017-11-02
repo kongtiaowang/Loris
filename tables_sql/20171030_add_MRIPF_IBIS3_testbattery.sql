8c652061e6dcd15dc88a2908d93abf35ea30c95e
sruthymathew123 <sruthy.mcin@gmail.com>
2017-11-02 09:30:25 -0400

    Add MRI PF into test battery across all IBIS subprojects and visits (#754)

---
+++ tables_sql/20171030_add_MRIPF_IBIS3_testbattery.sql
@@ -0,0 +1,5 @@
+-- Insert mri pf into test battery table across all IBIS3 subprojects and visits
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('mri_parameter_form',0,0,'Y','Visit',12,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('mri_parameter_form',0,0,'Y','Visit',13,'VSA');
