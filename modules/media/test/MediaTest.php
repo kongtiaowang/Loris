cf14e276e53fdb0dcd8e6473092ceec9dd57ebda
kongtiaowang <kongtiaowangshen@gmail.com>
2017-11-20 14:46:20 -0500

    test media

--- modules/media/test/MediaTest.php
+++ modules/media/test/MediaTest.php
@@ -27,7 +27,37 @@ require_once __DIR__ .
  */
 class MediaTest extends LorisIntegrationTest
 {
-
+    /**
+     * UI elements and locations
+     * breadcrumb - 'Access Profile'
+     * Table headers
+     */
+    private $_loadingBrowseUI
+        =  array(
+            //Browse UIs
+            'Browse'  => '#tab-browse',
+            //Browse table header
+            'No.'     => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
+            'File Name' => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
+            'PSCID'  => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
+            'Visit Label' => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
+            'Instrument' => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
+            'Site' => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
+            'Uploaded By' => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
+            'Date Taken'=>'#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
+            'Comments'=> '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
+           );
+    private $_loadingUploadUI
+        =  array(
+//            'Date Uploaded'=>'#dynamictable > thead > tr > th.dynamictableFrozenColumn',
+            //Upload UIs
+            'Upload a media file' => '#upload > div > div > form > div > div:nth-child(1) > h3',
+            'PSCID' => '#upload > div > div > form > div > div:nth-child(4) > div > label',
+            'Visit Label' => '#upload > div > div > form > div > div:nth-child(5) > div > label',
+            'Instrument' => '#upload > div > div > form > div > div:nth-child(7) > div > label',
+            'Browse' => '#upload > div > div > form > div > div:nth-child(10) > div > div > div > div.input-group-btn > div',
+            'Upload File' => '#upload > div > div > form > div > div:nth-child(11) > div > div > button',
+           );
     /**
      * Tests that the page does not load if the user does not have correct
      * permissions
@@ -60,6 +90,39 @@ class MediaTest extends LorisIntegrationTest
         $this->assertContains("You do not have access to this page.", $bodyText);
         $this->resetPermissions();
     }
-
+    /**
+      * Testing UI elements when page loads
+      *
+      * @return void
+      */
+    function testPageBrowseUIs()
+    {
+        $this->safeGet($this->url . "/media/");
+        sleep(1);
+        foreach ($this->_loadingBrowseUI as $key => $value) {
+            $text = $this->webDriver->executescript(
+                "return document.querySelector('$value').textContent"
+            );
+            $this->assertContains($key, $text);
+        }
+    }   
+    /**
+      * Testing UI elements when page loads
+      *
+      * @return void
+      */
+    function testPageUploadUIs()
+    {
+        $this->safeGet($this->url . "/media/#upload");
+        $page = $this->webDriver->getPageSource();
+        $var_dump($page);
+        sleep(1);
+        foreach ($this->_loadingUploadUI as $key => $value) {
+            $text = $this->webDriver->executescript(
+                "return document.querySelector('$value').textContent"
+            );
+            $this->assertContains($key, $text);
+        }
+    }
 }
 ?>
