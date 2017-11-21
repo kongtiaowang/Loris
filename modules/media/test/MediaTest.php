94c5e354e8f2f1563a5014f2a78c796bc669a0d5
kongtiaowang <kongtiaowangshen@gmail.com>
2017-11-21 12:33:25 -0500

    phpcs

--- modules/media/test/MediaTest.php
+++ modules/media/test/MediaTest.php
@@ -35,17 +35,17 @@ class MediaTest extends LorisIntegrationTest
     private $_loadingBrowseUI
         =  array(
             //Browse UIs
-            'Browse'  => '#tab-browse',
+            'Browse'      => '#tab-browse',
             //Browse table header
-            'No.'     => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
-            'File Name' => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
-            'PSCID'  => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
+            'No.'         => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
+            'File Name'   => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
+            'PSCID'       => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
             'Visit Label' => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
-            'Instrument' => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
-            'Site' => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
+            'Instrument'  => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
+            'Site'        => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
             'Uploaded By' => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
-            'Date Taken'=>'#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
-            'Comments'=> '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
+            'Date Taken'  => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
+            'Comments'    => '#dynamictable > thead:nth-child(1) > tr:nth-child(1)',
            );
     /**
      * Tests that the page does not load if the user does not have correct
@@ -99,28 +99,43 @@ class MediaTest extends LorisIntegrationTest
       * Testing React filter in this page.
       *
       * @return void
-      */    
+      */
     function testBrowseFilter()
     {
         $this->_testFilter("/media/", "pSCID", "MTL010", "1 rows");
         $this->_testFilter("/media/", "fileName", "MTL010", "1 rows");
-        $this->_testFilter("/media/", "fileType", "text/plain", 
-                           "20 rows displayed of 26"
+        $this->_testFilter(
+            "/media/",
+            "fileType",
+            "text/plain",
+            "20 rows displayed of 26"
         );
-        $this->_testFilter("/media/", "visitLabel", "V1",
-                           "20 rows displayed of 26"
+        $this->_testFilter(
+            "/media/",
+            "visitLabel",
+            "V1",
+            "20 rows displayed of 26"
         );
-        $this->_testFilter("/media/", "site", "Data Coordinating Center",
-                           "20 rows displayed of 26"
+        $this->_testFilter(
+            "/media/",
+            "site",
+            "Data Coordinating Center",
+            "20 rows displayed of 26"
         );
-        $this->_testFilter("/media/", "uploadedBy", "admin",
-                           "20 rows displayed of 26"
+        $this->_testFilter(
+            "/media/",
+            "uploadedBy",
+            "admin",
+            "20 rows displayed of 26"
         );
-        $this->_testFilter("/media/", "instrument", "BMI Calculator",
-                           "20 rows displayed of 26"
+        $this->_testFilter(
+            "/media/",
+            "instrument",
+            "BMI Calculator",
+            "20 rows displayed of 26"
         );
 
-    }   
+    }
     /**
       * Testing the link React filter in this page.
       *
@@ -132,14 +147,14 @@ class MediaTest extends LorisIntegrationTest
         sleep(1);
         // click the file Name link
         $this->webDriver->executescript(
-               "document.querySelector('#dynamictable > tbody > tr:nth-child(1)".
+            "document.querySelector('#dynamictable > tbody > tr:nth-child(1)".
                " > td:nth-child(4) > a').click()"
         );
         $text = $this->webDriver->executescript(
-               "return document.querySelector(' #bc2 > a:nth-child(4) > div').textContent"
+            "return document.querySelector('".
+            " #bc2 > a:nth-child(4) > div').textContent"
         );
         $this->assertContains("TimePoint", $text);
-
 
     }
     /**
@@ -156,13 +171,14 @@ class MediaTest extends LorisIntegrationTest
     {
         $this->safeGet($this->url . $url);
         $this->safeFindElement(
-             WebDriverBy::Name($filter)
+            WebDriverBy::Name($filter)
         )->sendKeys($testData);
         $text = $this->webDriver->executescript(
-               "return document.querySelector('.table-header').textContent"
+            "return document.querySelector('.table-header').textContent"
         );
         $this->webDriver->executescript(
-               "document.querySelector('#media_filter_form > div > div:nth-child(9) > div > div > button').click()"
+            "document.querySelector('#media_filter_form > div >".
+            " div:nth-child(9) > div > div > button').click()"
         );
 
         $this->assertContains($expectDataRows, $text);
