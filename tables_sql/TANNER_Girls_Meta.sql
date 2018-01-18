4f10c7a8b64cbf66588165004809ef34e78b76cf
suzanne-lee <suzannelee.mcin@gmail.com>
2018-01-18 11:47:54 -0500

    tanner questionnaire for boys and girls (#768)

---
+++ tables_sql/TANNER_Girls_Meta.sql
@@ -0,0 +1,4 @@
+INSERT INTO test_names (Test_name, Full_name, Sub_group) VALUES ('TANNER_Girls', 'TANNER Staging Questionnaire (Girls)', '1');
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('TANNER_Girls', 'TANNER_Girls_page1', "Page 1", 1);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('TANNER_Girls', '0', '0', 'Y', 'Visit', '12', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('TANNER_Girls', '0', '0', 'Y', 'Visit', '13', 'VSA', NULL);
