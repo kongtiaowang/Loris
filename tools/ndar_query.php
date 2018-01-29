b647c4eda9f71dc896bf127da90f29f5cbfb699b
sruthymathew123 <sruthy.mcin@gmail.com>
2018-01-29 12:16:39 -0500

    NDAR_2018  (#772)
    This is the revised new library class added for NDAR 2018 submission.

--- tools/ndar_query.php
+++ tools/ndar_query.php
@@ -3,7 +3,7 @@ set_include_path(get_include_path().":../libraries:../../php/libraries:");
 require_once '../../vendor/autoload.php';
 require_once "Database.class.inc";
 require_once "Utility.class.inc";
-require_once "NDAR_Release_2017.class.inc";
+require_once "NDAR_Release_2018.class.inc";
 
 $client = new NDB_Client();
 $client->makeCommandLine();
@@ -16,10 +16,10 @@ $run_ibis2 = true;
 
 if ($run_ibis1)
 {
-  // $IBIS = array('ados_module1', 'ados_module2', 'ados_module3', 'ados2_module1','ados2_module2','ados2_module3');
+  //$IBIS = array('vineland_subject');
   $IBIS = array ('ados_module1', 'ados_module2', 'ados_module3', 'ados2_module1','ados2_module2','ados2_module3','adi_r_proband','aosi','csbs','edi','fyi','ibq_r','m_chat_proband','macarthur_words_gestures','mullen','prefrontal_task','rbs_r','scq_proband','scq_subject','seq','vineland_proband','vineland_subject','charge');
   foreach($IBIS as $instrument) {
-      $run = new NDAR_Release_2017 ($instrument, 1);
+      $run = new NDAR_Release_2018 ($instrument, 1);
       $run->run();
   }
 }
@@ -27,10 +27,10 @@ if ($run_ibis1)
 if ($run_ibis2)
 {    //remove charge, head, and ados modules and add other two new
     //run ndar_subject script to get data/files for ndar_subject. Removing from here as it may leads to confusion
-    $IBIS2 = array('charge');
-//  $IBIS2 = array('adi_r_proband','aosi','csbs','ibq_r','macarthur_words_gestures', 'm_chat_proband', 'scq_proband','mullen','rbs_r','vineland_proband','vineland_subject','edi2','height_weight','phys_neuro_exam', 'charge');
+    //$IBIS2 = array('scq_proband');
+     $IBIS2 = array('adi_r_proband','aosi','csbs','ibq_r','macarthur_words_gestures', 'm_chat_proband', 'scq_proband','mullen','rbs_r','vineland_proband','vineland_subject','edi2','height_weight','phys_neuro_exam', 'charge');
   foreach($IBIS2 as $instrument) {
-      $run = new NDAR_Release_2017 ($instrument, 2);
+      $run = new NDAR_Release_2018 ($instrument, 2);
       $run->run();
 
   }
