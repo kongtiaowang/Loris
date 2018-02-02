27cb9fab917c0d2f505ef8ac8099ec60a98005d4
kongtiaowang <kongtiaowangshen@gmail.com>
2018-02-02 15:00:19 -0500

    d

--- test/unittests/NDB_Menu_Filter_Test.php
+++ test/unittests/NDB_Menu_Filter_Test.php
@@ -10,7 +10,7 @@ class NDB_Menu_Filter_Test extends TestCase
      */
     function setUp() {
         global $_SESSION;
-        $this->Session = $this->getMock('stdClass', array('getProperty', 'setProperty'));
+        $this->Session = $this->getMockBuilder(stdClass::class)->setMethods(array('getProperty', 'setProperty'))->getMock(); 
 
         $_SESSION = array(
             'State' => $this->Session
