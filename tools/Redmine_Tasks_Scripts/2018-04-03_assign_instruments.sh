05a468155020a1d9f52f9c6b11c660bd1fc96d59
sruthymathew123 <sruthy.mcin@gmail.com>
2018-04-03 14:42:53 -0400

    Populate certain instruments to candidate timepoints (#801)
    * Assign some instruments to candidated to battery
    
    * one more

---
+++ tools/Redmine_Tasks_Scripts/2018-04-03_assign_instruments.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd /var/www/loris/tools
+
+php fix_timepoint_date_problems.php add_instrument 974665 2443 ECBQ
+php fix_timepoint_date_problems.php add_instrument 230955 2063 ECBQ
+php fix_timepoint_date_problems.php add_instrument 630096 1545 ECBQ
+php fix_timepoint_date_problems.php add_instrument 751872 2752 ECBQ
+php fix_timepoint_date_problems.php add_instrument 811160 1585 ECBQ
+php fix_timepoint_date_problems.php add_instrument 339961 2880 ECBQ
+php fix_timepoint_date_problems.php add_instrument 339961 2880 ECBQ
+php fix_timepoint_date_problems.php add_instrument 160209 2200 ECBQ
+php fix_timepoint_date_problems.php add_instrument 376852 2109 ECBQ
+php fix_timepoint_date_problems.php add_instrument 765425 4727 parental_stress_scale
+php fix_timepoint_date_problems.php add_instrument 673832 5086 m_chat_subject
+php fix_timepoint_date_problems.php add_instrument 490127 5044 m_chat_subject
+php fix_timepoint_date_problems.php add_instrument 350482 5043 m_chat_subject
+php fix_timepoint_date_problems.php add_instrument 246238 5647 m_chat_subject
+php fix_timepoint_date_problems.php add_instrument 391404 4462 med_records_proband
