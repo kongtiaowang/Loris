1f3ed82a822f872316e60a2639f57917d68e0918
sruthymathew123 <sruthy.mcin@gmail.com>
2018-04-03 13:29:47 -0400

    Change IBIS3 to IBISSA (#784)
    * Change IBIS3 to IBISSA
    
    * fix typ0

---
+++ tables_sql/20180223_IBIS3_IBISSA.sql
@@ -0,0 +1,4 @@
+-- IBIS3 to IBISSA changes
+UPDATE `Project` SET `Name`='IBISSA' WHERE `ProjectID`='6';
+UPDATE `subproject` SET `title`='IBISSA LR' WHERE `SubprojectID`='12';
+UPDATE `subproject` SET `title`='IBISSA HR' WHERE `SubprojectID`='13';
