e510f147c0aec2544683992a49e144894f74b4fd
sruthymathew123 <sruthy.mcin@gmail.com>
2018-04-05 11:26:17 -0400

    instr fixes (#803)

--- tools/Redmine_Tasks_Scripts/2018-04-03_assign_instruments.sh
+++ tools/Redmine_Tasks_Scripts/2018-04-03_assign_instruments.sh
@@ -8,9 +8,10 @@ php fix_timepoint_date_problems.php add_instrument 630096 1545 ECBQ
 php fix_timepoint_date_problems.php add_instrument 751872 2752 ECBQ
 php fix_timepoint_date_problems.php add_instrument 811160 1585 ECBQ
 php fix_timepoint_date_problems.php add_instrument 339961 2880 ECBQ
-php fix_timepoint_date_problems.php add_instrument 339961 2880 ECBQ
+php fix_timepoint_date_problems.php add_instrument 327961 2539 ECBQ
 php fix_timepoint_date_problems.php add_instrument 160209 2200 ECBQ
 php fix_timepoint_date_problems.php add_instrument 376852 2109 ECBQ
+php fix_timepoint_date_problems.php add_instrument 215951 2943 ECBQ
 php fix_timepoint_date_problems.php add_instrument 765425 4727 parental_stress_scale
 php fix_timepoint_date_problems.php add_instrument 673832 5086 m_chat_subject
 php fix_timepoint_date_problems.php add_instrument 490127 5044 m_chat_subject
