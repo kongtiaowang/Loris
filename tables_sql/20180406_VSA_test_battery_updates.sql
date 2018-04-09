50c24aecb1d11469164718aeae59cdddc36e467b
sruthymathew123 <sruthy.mcin@gmail.com>
2018-04-09 15:44:06 -0400

    VSA for IBIS1&2 (#813)
    Add VSA for IBIS1 and IBIS2

---
+++ tables_sql/20180406_VSA_test_battery_updates.sql
@@ -0,0 +1,182 @@
+-- Add VSA & related instruments to IBIS 1,2 & SA(for missed)
+-- Adding mri_parameter form to VSA visit for IBIS 1 &2
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('mri_parameter_form',0,0,'Y','Visit',1,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('mri_parameter_form',0,0,'Y','Visit',2,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('mri_parameter_form',0,0,'Y','Visit',3,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('mri_parameter_form',0,0,'Y','Visit',9,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('mri_parameter_form',0,0,'Y','Visit',10,'VSA');
+-- Adding ados2 module 1 instruments to VSA visit for IBIS 1 &2
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ados2_module1',0,0,'Y','Visit',1,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ados2_module1',0,0,'Y','Visit',2,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ados2_module1',0,0,'Y','Visit',3,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ados2_module1',0,0,'Y','Visit',9,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ados2_module1',0,0,'Y','Visit',10,'VSA');
+-- Adding ados2 module 2 instruments to VSA visit for IBIS 1 &2
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ados2_module2',0,0,'Y','Visit',1,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ados2_module2',0,0,'Y','Visit',2,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ados2_module2',0,0,'Y','Visit',3,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ados2_module2',0,0,'Y','Visit',9,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ados2_module2',0,0,'Y','Visit',10,'VSA');
+-- Adding ados2 module 3 instrument to VSA visit for IBIS 1 &2
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ados2_module3',0,0,'Y','Visit',1,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ados2_module3',0,0,'Y','Visit',2,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ados2_module3',0,0,'Y','Visit',3,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ados2_module3',0,0,'Y','Visit',9,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('ados2_module3',0,0,'Y','Visit',10,'VSA');
+-- Adding adir instrument to VSA visit for IBIS 1 &2
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('adi_r_subject',0,0,'Y','Visit',1,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('adi_r_subject',0,0,'Y','Visit',2,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('adi_r_subject',0,0,'Y','Visit',3,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('adi_r_subject',0,0,'Y','Visit',9,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('adi_r_subject',0,0,'Y','Visit',10,'VSA');
+-- Adding adir instrument to VSA visit for IBIS 1 &2
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('adi_r_subject',0,0,'Y','Visit',1,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('adi_r_subject',0,0,'Y','Visit',2,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('adi_r_subject',0,0,'Y','Visit',3,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('adi_r_subject',0,0,'Y','Visit',9,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('adi_r_subject',0,0,'Y','Visit',10,'VSA');
+-- Adding das schoolage to VSA visit for IBIS 1 &2
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('das_schoolage',0,0,'Y','Visit',1,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('das_schoolage',0,0,'Y','Visit',2,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('das_schoolage',0,0,'Y','Visit',3,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('das_schoolage',0,0,'Y','Visit',9,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('das_schoolage',0,0,'Y','Visit',10,'VSA');
+-- Adding wiat part 1 to VSA visit for IBIS 1 & 2
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('wiatIII_part_1',0,0,'Y','Visit',1,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('wiatIII_part_1',0,0,'Y','Visit',2,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('wiatIII_part_1',0,0,'Y','Visit',3,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('wiatIII_part_1',0,0,'Y','Visit',9,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('wiatIII_part_1',0,0,'Y','Visit',10,'VSA');
+-- Adding wiat part 2 to VSA visit for IBIS 1 & 2
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('wiatIII_part_2',0,0,'Y','Visit',1,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('wiatIII_part_2',0,0,'Y','Visit',2,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('wiatIII_part_2',0,0,'Y','Visit',3,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('wiatIII_part_2',0,0,'Y','Visit',9,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('wiatIII_part_2',0,0,'Y','Visit',10,'VSA');
+-- Adding Tanner_boys to VSA visit for IBIS 1 & 2
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('TANNER_Boys',0,0,'Y','Visit',1,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('TANNER_Boys',0,0,'Y','Visit',2,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('TANNER_Boys',0,0,'Y','Visit',3,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('TANNER_Boys',0,0,'Y','Visit',9,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('TANNER_Boys',0,0,'Y','Visit',10,'VSA');
+-- Adding Tanner_girls to VSA visit for IBIS 1 & 2
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('TANNER_Girls',0,0,'Y','Visit',1,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('TANNER_Girls',0,0,'Y','Visit',2,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('TANNER_Girls',0,0,'Y','Visit',3,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('TANNER_Girls',0,0,'Y','Visit',9,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('TANNER_Girls',0,0,'Y','Visit',10,'VSA');
+-- Adding med_records_recruit to VSA visit for IBIS 1 & 2
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('med_records_recruit',0,0,'Y','Visit',1,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('med_records_recruit',0,0,'Y','Visit',2,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('med_records_recruit',0,0,'Y','Visit',3,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('med_records_recruit',0,0,'Y','Visit',9,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('med_records_recruit',0,0,'Y','Visit',10,'VSA');
+-- Adding SCQ to VSA visit for IBIS 1, 2 & SA
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('scq_subject',0,0,'Y','Visit',1,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('scq_subject',0,0,'Y','Visit',2,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('scq_subject',0,0,'Y','Visit',3,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('scq_subject',0,0,'Y','Visit',9,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('scq_subject',0,0,'Y','Visit',10,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('scq_subject',0,0,'Y','Visit',12,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('scq_subject',0,0,'Y','Visit',13,'VSA');
+-- Adding neuroscreen to VSA visit for IBIS 1, 2 & SA
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('neuroscreen',0,0,'Y','Visit',1,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('neuroscreen',0,0,'Y','Visit',2,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('neuroscreen',0,0,'Y','Visit',3,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('neuroscreen',0,0,'Y','Visit',9,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('neuroscreen',0,0,'Y','Visit',10,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('neuroscreen',0,0,'Y','Visit',12,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('neuroscreen',0,0,'Y','Visit',13,'VSA');
+-- Adding DSMV to VSA visit for IBIS 1, 2 & SA
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('DSMV_checklist',0,0,'Y','Visit',1,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('DSMV_checklist',0,0,'Y','Visit',2,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('DSMV_checklist',0,0,'Y','Visit',3,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('DSMV_checklist',0,0,'Y','Visit',9,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('DSMV_checklist',0,0,'Y','Visit',10,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('DSMV_checklist',0,0,'Y','Visit',12,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('DSMV_checklist',0,0,'Y','Visit',13,'VSA');
+
+
+
+
