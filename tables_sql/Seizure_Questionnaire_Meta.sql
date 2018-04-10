3180a24e9aecda6e4e1cb31267b6aa0bebcfe1e5
Liza Levitis <elizabeth.levitis@mail.mcgill.ca>
2018-04-10 15:31:18 -0400

    [Instruments] IBIS Seizure Screener (#812)
    * 1st commit for instrument
    
    * working on page 2
    
    * meta & schema files
    
    * fixed some formatting
    
    * removing meta file from wrong location
    
    * fixed XINRegisterRule issues

---
+++ tables_sql/Seizure_Questionnaire_Meta.sql
@@ -0,0 +1,9 @@
+INSERT INTO test_names(Test_name, Full_name, Sub_Group, isDirectEntry) VALUES('Seizure_Questionnaire', "Seizure Questionnaire", 9, 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Seizure_Questionnaire','Seizure_Questionnaire_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Seizure_Questionnaire','Seizure_Questionnaire_page2', "Page 2", 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Seizure_Questionnaire','Seizure_Questionnaire_page3', "Page 3", 3);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Seizure_Questionnaire', '0', '0', 'N', 'Visit', 1, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Seizure_Questionnaire', '0', '0', 'N', 'Visit', 2, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Seizure_Questionnaire', '0', '0', 'N', 'Visit', 3, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Seizure_Questionnaire', '0', '0', 'N', 'Visit', 9, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Seizure_Questionnaire', '0', '0', 'N', 'Visit', 10, 'VSA', NULL);
