d63a1cb33200aab49dd4b3b10d27157536c2b260
suzanne-lee <suzannelee.mcin@gmail.com>
2018-04-10 15:23:43 -0400

    BRIEF-2 Parent Form Instrument (#807)
    * BRIEF-2 Parent Form Instrument
    
    * removes subprojects from meta
    
    * disables name fields
    
    * removes from VSA

---
+++ tables_sql/BRIEF2_Parent_Meta.sql
@@ -0,0 +1,3 @@
+INSERT INTO test_names(Test_name, Full_name, Sub_Group, isDirectEntry) VALUES('BRIEF2_Parent', "BRIEF2 Parent Form", 1, 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('BRIEF2_Parent','BRIEF2_Parent_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('BRIEF2_Parent','BRIEF2_Parent_page2', "Page 2", 2);
