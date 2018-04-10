5bc953e0d0bc091b2b8ed7bb94449b3c03c86da2
Liza Levitis <elizabeth.levitis@mail.mcgill.ca>
2018-04-10 15:26:16 -0400

    [SQL] Adding TSI to VSA visit  (#810)
    * adding TSI to VSA visit
    
    * adding TSA to VSA for IBIS SA LR & HR

---
+++ tables_sql/2018-04-06_add_TSI_VSA.sql
@@ -0,0 +1,14 @@
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('tsi',0,0,'Y','Visit',1,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('tsi',0,0,'Y','Visit',2,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('tsi',0,0,'Y','Visit',3,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('tsi',0,0,'Y','Visit',9,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('tsi',0,0,'Y','Visit',10,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('tsi',0,0,'Y','Visit',12,'VSA');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('tsi',0,0,'Y','Visit',13,'VSA');
