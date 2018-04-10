58972071c0f79ffa1f27c51ce2c9eaad0602ec18
suzanne-lee <suzannelee.mcin@gmail.com>
2018-04-10 15:24:37 -0400

    BRIEF2 Teacher Form Instrument (#808)
    * BRIEF2 Teacher Form Instrument
    
    * removes subprojects from meta
    
    * disables name fields
    
    * removes from VSA

---
+++ tables_sql/BRIEF2_Teacher_Meta.sql
@@ -0,0 +1,3 @@
+INSERT INTO test_names(Test_name, Full_name, Sub_Group, isDirectEntry) VALUES('BRIEF2_Teacher', "BRIEF2 Teacher Form", 1, 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('BRIEF2_Teacher','BRIEF2_Teacher_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('BRIEF2_Teacher','BRIEF2_Teacher_page2', "Page 2", 2);
