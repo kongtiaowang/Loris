d4fed26cd949f7b6b5ed3767bd76553082ecc64f
Liza Levitis <elizabeth.levitis@mail.mcgill.ca>
2018-04-10 15:29:02 -0400

    [SQL] Add CDI_Words_Sentences to IBIS1 & IBIS2 candidates at V24 (#811)
    * sql patch to add CDI W&S to V24 for IBIS1 & IBIS2
    
    * Delete 2018-04-06_populate_cdi_ws_v24.sql
    
    * Delete 2018-04-06_populate_cdi_ws_v24.sh

---
+++ tables_sql/2018-04-06_populate_cdi_ws_v24.sql
@@ -0,0 +1,11 @@
+--  add CDI_Words_Sentences to IBIS1 & IBIS2 subproject at V24
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('CDI_Words_Sentences',0,0,'Y','Visit',1,'V24');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('CDI_Words_Sentences',0,0,'Y','Visit',2,'V24');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('CDI_Words_Sentences',0,0,'Y','Visit',3,'V24');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('CDI_Words_Sentences',0,0,'Y','Visit',9,'V24');
+INSERT INTO `test_battery`(`Test_name`,`AgeMinDays`, `AgeMaxDays`, `Active`, `Stage`, `SubprojectID`, `Visit_label`)
+VALUES ('CDI_Words_Sentences',0,0,'Y','Visit',10,'V24');
