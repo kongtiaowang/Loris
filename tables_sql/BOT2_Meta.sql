b2ba65eed5c473d29ac188e2b39459049b9c7515
Liza Levitis <elizabeth.levitis@mail.mcgill.ca>
2018-04-26 15:00:34 -0400

    [Instruments] BOT2 (#819)
    * added subtest pages
    
    * scoring needs to be completed
    
    * scale score lookup table in progress
    
    * updating meta file

---
+++ tables_sql/BOT2_Meta.sql
@@ -0,0 +1,19 @@
+INSERT INTO test_names(Test_name, Full_name, Sub_Group) VALUES('BOT2', "BOT 2: Bruininks-Oseretsky Test of Motor Proficiency, <i>Second Edition</i>", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('BOT2','BOT2_page1', "Subtests 1-2", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('BOT2','BOT2_page2', "Subtests 3-4", 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('BOT2','BOT2_page3', "Subtests 5-6", 3);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('BOT2','BOT2_page4', "Subtests 7-8", 4);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('BOT2', '0', '0', 'Y', 'Visit', 1, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('BOT2', '0', '0', 'Y', 'Visit', 2, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('BOT2', '0', '0', 'Y', 'Visit', 3, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('BOT2', '0', '0', 'Y', 'Visit', 9, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('BOT2', '0', '0', 'Y', 'Visit', 10, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('BOT2', '0', '0', 'Y', 'Visit', 12, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('BOT2', '0', '0', 'Y', 'Visit', 13, 'VSA', NULL);
