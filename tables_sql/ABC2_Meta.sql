4647933fc12f368a512ba0238a73866386f91254
suzanne-lee <suzannelee.mcin@gmail.com>
2018-04-26 15:04:36 -0400

    ABC-2 instrument, meta, sql (#841)

---
+++ tables_sql/ABC2_Meta.sql
@@ -0,0 +1,3 @@
+INSERT INTO test_names(Test_name, Full_name, Sub_Group, isDirectEntry) VALUES('ABC2', "ABC-2", 1, 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('ABC2','ABC2_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('ABC2','ABC2_page2', "Page 2", 2);
