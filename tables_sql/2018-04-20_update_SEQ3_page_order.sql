dee31abef7c29173663d2d507aeadcc4db0302e6
Liza Levitis <elizabeth.levitis@mail.mcgill.ca>
2018-04-26 15:24:29 -0400

    [Instruments] updating SEQ3 page order and instruction appearance (#834)
    * updating SEQ3 page order and instruction appearance
    
    * fix
    
    * Update NDB_BVL_Instrument_SEQ3.class.inc
    
    removing commented out lines

---
+++ tables_sql/2018-04-20_update_SEQ3_page_order.sql
@@ -0,0 +1,14 @@
+-- Update SEQ3 Page 2 Order
+UPDATE instrument_subtests SET Order_number = 2 WHERE Subtest_name = "SEQ3_page2";
+-- Update SEQ3 Page 3 Order
+UPDATE instrument_subtests SET Order_number = 3 WHERE Subtest_name = "SEQ3_page3";
+-- Update SEQ3 Page 4 Order
+UPDATE instrument_subtests SET Order_number = 4 WHERE Subtest_name = "SEQ3_page4";
+-- Update SEQ3 Page 5 Order
+UPDATE instrument_subtests SET Order_number = 5 WHERE Subtest_name = "SEQ3_page5";
+-- Update SEQ3 Page 6 Order
+UPDATE instrument_subtests SET Order_number = 6 WHERE Subtest_name = "SEQ3_page6";
+-- Update SEQ3 Page 7 Order
+UPDATE instrument_subtests SET Order_number = 7 WHERE Subtest_name = "SEQ3_page7";
+-- Update SEQ3 Page 8 Order
+UPDATE instrument_subtests SET Order_number = 8 WHERE Subtest_name = "SEQ3_page8";
