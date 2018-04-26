cca41a1e52c089d946155e36c776f96bd67e65b7
suzanne-lee <suzannelee.mcin@gmail.com>
2018-04-26 15:06:17 -0400

    [Instruments] TRF (6-18)  (#824)
    * TRF instrument class, sql, meta
    
    * formatting
    
    * change 0,1,2 options to phrases, fix typo, add italics
    
    * add instruction label on page 3 and 4
    
    * phpcs + phpcbf
    
    * fix typo

---
+++ tables_sql/Teachers_Report_Form_6_To_18_Meta.sql
@@ -0,0 +1,5 @@
+INSERT INTO test_names(Test_name, Full_name, Sub_Group, isDirectEntry) VALUES('Teachers_Report_Form_6_To_18', "Teacher's Report Form (TRF) for Ages 6-18", 1, 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Teachers_Report_Form_6_To_18','Teachers_Report_Form_6_To_18_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Teachers_Report_Form_6_To_18','Teachers_Report_Form_6_To_18_page2', "Page 2", 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Teachers_Report_Form_6_To_18','Teachers_Report_Form_6_To_18_page3', "Page 3", 3);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Teachers_Report_Form_6_To_18','Teachers_Report_Form_6_To_18_page4', "Page 4", 4);
