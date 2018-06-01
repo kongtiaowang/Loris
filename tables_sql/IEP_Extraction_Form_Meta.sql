a1e6204a8330034dbc4e790c243cb783d92146fe
suzanne-lee <suzannelee.mcin@gmail.com>
2018-06-01 10:08:02 -0400

    [Instrument] IEP Extraction Form (VSA) (#891)
    * IEP instrument, sql, meta
    
    * rename meta to match other files
    
    * remove extra file
    
    * insert into more subprojects

---
+++ tables_sql/IEP_Extraction_Form_Meta.sql
@@ -0,0 +1,10 @@
+INSERT INTO test_names (Test_name, Full_name, Sub_group) VALUES ('IEP_Extraction_Form', 'IEP School Extraction Form', '1');
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('IEP_Extraction_Form', 'IEP_Extraction_Form_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('IEP_Extraction_Form', 'IEP_Extraction_Form_page2', "Page 2", 2);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('IEP_Extraction_Form', '0', '0', 'Y', 'Visit', '1', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('IEP_Extraction_Form', '0', '0', 'Y', 'Visit', '2', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('IEP_Extraction_Form', '0', '0', 'Y', 'Visit', '3', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('IEP_Extraction_Form', '0', '0', 'Y', 'Visit', '9', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('IEP_Extraction_Form', '0', '0', 'Y', 'Visit', '10', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('IEP_Extraction_Form', '0', '0', 'Y', 'Visit', '12', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('IEP_Extraction_Form', '0', '0', 'Y', 'Visit', '13', 'VSA', NULL);
