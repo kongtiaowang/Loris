d5eb6d8eab6dada6c7794b90dcfa96529b4ad526
Cole Zweber <colezweber@gmail.com>
2018-06-01 10:14:10 -0400

    [Instrument] SRS-2 School-Age (#902)
    * SRS2 School Age instrument class, sql, meta
    
    * Fixed sql
    
    * Added grade field and corresponding table column
    
    * Fixed sql table
    
    * Final sql fix
    
    * Code sniffer cleanup
    
    * Typos
    
    * Removes commented out line
    
    * Added more descriptive labels for age and grade

---
+++ tables_sql/SRS2_SchoolAge_Meta.sql
@@ -0,0 +1,5 @@
+INSERT INTO test_names(Test_name, Full_name, Sub_Group, isDirectEntry) VALUES('SRS2_SchoolAge', "SRS2 School-Age", 1, 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('SRS2_SchoolAge','SRS2_SchoolAge_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('SRS2_SchoolAge','SRS2_SchoolAge_page2', "Page 2", 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('SRS2_SchoolAge','SRS2_SchoolAge_page3', "Page 3", 3);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('SRS2_SchoolAge','SRS2_SchoolAge_page4', "Page 4", 4);
