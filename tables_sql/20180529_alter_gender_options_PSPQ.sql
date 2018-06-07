68af6a3f8945282198cdb32501ef99217aeb68d1
Mie Beers <mie.beers@gmail.com>
2018-06-07 12:10:04 -0400

    PSPQ - Fixed special validations, changed gender to sex on the front end, cleaned up other small fixes. (#901)
    * Fixed validations for questions with 'other' fields and age fields, made gender of partner field required, fixed some formatting, standardized relationship to child fields to '(biological) mother/father'
    
    * Changed gender to sex on the front end
    
    * Created SQL patch that alters the datatypes in the gender columns to include 'other' option

---
+++ tables_sql/20180529_alter_gender_options_PSPQ.sql
@@ -0,0 +1,4 @@
+ALTER TABLE pspq_1 MODIFY COLUMN respondent_gender enum('male','female','other');
+ALTER TABLE pspq_1 MODIFY COLUMN informant_gender enum('male','female','other');
+ALTER TABLE pspq_2 MODIFY COLUMN respondent_gender enum('male','female','other');
+ALTER TABLE pspq_2 MODIFY COLUMN informant_gender enum('male','female','other');
