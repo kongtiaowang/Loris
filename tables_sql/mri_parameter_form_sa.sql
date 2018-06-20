76db609bbd0ed22681c81f4d2537ca3a3bb49e8e
Cole Zweber <colezweber@gmail.com>
2018-06-20 14:56:29 -0400

    Created meta sql file

--- tables_sql/mri_parameter_form_sa.sql
+++ tables_sql/mri_parameter_form_sa.sql
@@ -112,3 +112,6 @@ CREATE TABLE `mri_parameter_form_sa` (
 PRIMARY KEY  (`CommentID`)
 
               ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO test_names(Test_name, Full_name, Sub_Group, isDirectEntry) VALUES('mri_parameter_form_sa', "MRI Parameter Form School Age", 1, 0);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('mri_parameter_form_sa','mri_parameter_form_sa_page1', "Page 1", 1);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('mri_parameter_form_sa', '0', '0', 'Y', 'Visit', '1', 'VSA', NULL);
