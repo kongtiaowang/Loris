e60ba6ad3db20db623b8bf27258eba97ff362e34
Cole Zweber <colezweber@gmail.com>
2018-07-03 13:09:19 -0400

    Added across all VSA subprojects

--- tables_sql/mri_parameter_form_sa.sql
+++ tables_sql/mri_parameter_form_sa.sql
@@ -115,3 +115,9 @@ PRIMARY KEY  (`CommentID`)
 INSERT INTO test_names(Test_name, Full_name, Sub_Group, isDirectEntry) VALUES('mri_parameter_form_sa', "MRI Parameter Form School Age", 1, 0);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('mri_parameter_form_sa','mri_parameter_form_sa_page1', "Page 1", 1);
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('mri_parameter_form_sa', '0', '0', 'Y', 'Visit', '1', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('mri_parameter_form_sa', '0', '0', 'Y', 'Visit', '2', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('mri_parameter_form_sa', '0', '0', 'Y', 'Visit', '3', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('mri_parameter_form_sa', '0', '0', 'Y', 'Visit', '9', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('mri_parameter_form_sa', '0', '0', 'Y', 'Visit', '10', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('mri_parameter_form_sa', '0', '0', 'Y', 'Visit', '12', 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('mri_parameter_form_sa', '0', '0', 'Y', 'Visit', '13', 'VSA', NULL);
