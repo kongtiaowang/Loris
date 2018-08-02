b90bf63953c83933361f8ada3aefa4642901ed3e
Sruthy Mathew <sruthy.mcin@gmail.com>
2018-08-02 15:52:47 -0400

    more changes

--- modules/survey_accounts/jsx/columnFormatter.js
+++ modules/survey_accounts/jsx/columnFormatter.js
@@ -8,7 +8,12 @@
  * @param {arrray} rowHeaders - array of table headers (column names)
  * @return {*} a formated table cell for a given column
  */
+loris.hiddenHeaders = ['Email'];
 function formatColumn(column, cell, rowData, rowHeaders) {
+    // If a column if set as hidden, don't display it
+    if (loris.hiddenHeaders.indexOf(column) > -1) {
+        return null;
+    }
   // Create the mapping between rowHeaders and rowData in a row object.
   var row = {};
   rowHeaders.forEach(function(header, index) {
@@ -24,13 +29,14 @@ function formatColumn(column, cell, rowData, rowHeaders) {
           return (<td><a href={url}>{cell}</a></td>);
       }
   }
-    if (column === 'ParentID') {
-        if (row.Visit === 'VSA') {
-            var url = loris.BaseURL + "/parent_portal/?id=" + row.ParentID;
-            return (<td><a href={url}>{cell}</a></td>);
+    if (column === 'Parent Portal') {
+        var parentid = row['Parent Portal'];
+        if (parentid !="NA") {
+            var url = loris.BaseURL + "/parent_portal/?id=" + parentid;
+            return (<td><a href={url} target="_blank">Go To Parent Portal</a></td>);
         }
         else{
-            return <td>{cell}</td>;
+            return <td>NA</td>;
         }
     }
   return <td>{cell}</td>;
