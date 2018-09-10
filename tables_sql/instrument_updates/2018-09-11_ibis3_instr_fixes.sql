5898eab03b9460c21b67a90b08a45d053ea156cf
Sruthy Mathew <sruthy.mcin@gmail.com>
2018-09-10 12:34:00 -0400

    first_page_error_fix

---
+++ tables_sql/instrument_updates/2018-09-11_ibis3_instr_fixes.sql
@@ -0,0 +1,9 @@
+-- Fixing Unknown_Columns in field list od SRS2_SchoolAge
+ALTER TABLE SRS2_SchoolAge ADD COLUMN Candidate_Age varchar (100);
+ALTER TABLE SRS2_SchoolAge ADD COLUMN Window_Difference varchar (100);
+-- Fixing Unknown_Columns in field list od SRS2_SchoolAge
+ALTER TABLE CBCL_6_To_18 ADD COLUMN Candidate_Age varchar (100);
+ALTER TABLE CBCL_6_To_18 ADD COLUMN Window_Difference varchar (100);
+
+
+
