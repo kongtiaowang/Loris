eb7d5d9b820a09cee6c3f427d6fc83d7087c6d73
Sruthy Mathew <sruthy.mcin@gmail.com>
2018-09-13 11:59:18 -0400

    Final

--- tables_sql/instrument_updates/2018-09-11_ibis3_instr_fixes.sql
+++ tables_sql/instrument_updates/2018-09-11_ibis3_instr_fixes.sql
@@ -1,9 +1,14 @@
--- Fixing Unknown_Columns in field list od SRS2_SchoolAge
+-- Fixing Unknown_Columns in field list for SRS2_SchoolAge
 ALTER TABLE SRS2_SchoolAge ADD COLUMN Candidate_Age varchar (100);
 ALTER TABLE SRS2_SchoolAge ADD COLUMN Window_Difference varchar (100);
--- Fixing Unknown_Columns in field list od SRS2_SchoolAge
+-- Fixing Unknown_Columns in field list for CBCL
 ALTER TABLE CBCL_6_To_18 ADD COLUMN Candidate_Age varchar (100);
 ALTER TABLE CBCL_6_To_18 ADD COLUMN Window_Difference varchar (100);
+-- Fixing Unknown_Columns in field list for SSIS
+ALTER TABLE SSIS_Parent ADD COLUMN Candidate_Age varchar (100);
+ALTER TABLE SSIS_Parent ADD COLUMN Window_Difference varchar (100);
+ALTER TABLE SSIS_Teacher ADD COLUMN Candidate_Age varchar (100);
+ALTER TABLE SSIS_Teacher ADD COLUMN Window_Difference varchar (100);
 
 
 
