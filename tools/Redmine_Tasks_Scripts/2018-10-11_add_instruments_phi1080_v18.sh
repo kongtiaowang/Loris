a635e50f6475b2117b9625aae6ae5473f7824877
Liza Levitis <llevitis.mcin@gmail.com>
2018-10-11 13:18:34 -0400

    script to populate phi1080 v18 with new instruments

---
+++ tools/Redmine_Tasks_Scripts/2018-10-11_add_instruments_phi1080_v18.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd /var/www/loris/tools
+
+php fix_timepoint_date_problems.php add_instrument 319053 5809 mullen
+php fix_timepoint_date_problems.php add_instrument 319053 5809 ados2_module2
+php fix_timepoint_date_problems.php add_instrument 319053 5809 vineland_subject
+php fix_timepoint_date_problems.php add_instrument 319053 5809 DSMIV_checklist
+php fix_timepoint_date_problems.php add_instrument 319053 5809 DSMV_checklist
+php fix_timepoint_date_problems.php add_instrument 319053 5809 macarthur_words_gestures
+php fix_timepoint_date_problems.php add_instrument 319053 5809 csbs
+php fix_timepoint_date_problems.php add_instrument 319053 5809 m_chat_subject
