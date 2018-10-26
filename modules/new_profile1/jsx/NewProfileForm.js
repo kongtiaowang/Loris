2fbeed7885156be0aa2e03cf671a1873196d0bcf
kongtiaowang <kongtiaowangshen@gmail.com>
2018-10-26 16:01:58 -0400

    day2

--- modules/new_profile1/jsx/NewProfileForm.js
+++ modules/new_profile1/jsx/NewProfileForm.js
@@ -11,21 +11,41 @@ class NewProfileForm extends React.Component {
   constructor(props) {
     super(props);
      this.state = {
-      Data: {},
+      Data: {
+          gender:{
+                 'male': "Male",
+                 'female': "Female"
+          }
+      },
       formData: {},
       isLoaded: false,
      }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.setFormData = this.setFormData.bind(this);
    }
+
+
+  validate() {
+    const data = this.state.formData;
+    let isError = false;
+    if (data.dateTaken !== data.dateTakenConfirm)
+    {
+        isError = true;
+    } 
+      return isError; 
+  }
+
+
   /**
    * Handles form submission
    * @param {event} e - Form submition event
    */
   handleSubmit(e) {
     e.preventDefault();
-    
+    const err = this.validate();    
+    if (!err){
     console.log(this.state.formData);
+    }
   }
 
   /**
@@ -58,8 +78,32 @@ class NewProfileForm extends React.Component {
             minYear="2000"
             maxYear="2017"
             onUserInput={this.setFormData}
-            ref="dateTaken"
+            ref="dateTaken1"
             value={this.state.formData.dateTaken}
+          />
+          <DateElement
+            name="dateTakenConfirm"
+            label="Date of Birth Confirm"
+            minYear="2000"
+            maxYear="2017"
+            onUserInput={this.setFormData}
+            ref="dateTaken2"
+            value={this.state.formData.dateTakenConfirm}
+          />
+          <SelectElement
+            name="gender"
+            label="Gender"
+            options={this.state.Data.gender}
+            onUserInput={this.setFormData}
+            ref="gender"
+            value={this.state.formData.gender}
+          />
+          <TextareaElement
+            name="pscid"
+            label="PSCID"
+            onUserInput={this.setFormData}
+            ref="pscid"
+            value={this.state.formData.pscid}
           />
           <ButtonElement label="Create" />
         </FormElement>
