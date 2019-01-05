616d4a84333f2d9ab1ae65ca980723927ef67815
kongtiaowang <kongtiaowangshen@gmail.com>
2019-01-04 23:14:03 -0500

    d

--- test/integrationtests/Login_Test.php
+++ test/integrationtests/Login_Test.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/LorisIntegrationTest.class.inc';
+require_once __DIR__ . './LorisIntegrationTest.class.inc';
 class LorisLoginTest extends LorisIntegrationTest
 {
     function testLoginFailure()
