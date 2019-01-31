ab223b793e0419f9d3926e4f024f08d058609107
kongtiaowang <kongtiaowangshen@gmail.com>
2019-01-31 13:30:02 -0500

    fdas

--- test/RBfiles/RB_bids_mri_scan_type_rel.sql
+++ test/RBfiles/RB_bids_mri_scan_type_rel.sql
@@ -1,3 +1,4 @@
+SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE TABLE `bids_mri_scan_type_rel`;
 LOCK TABLES `bids_mri_scan_type_rel` WRITE;
 INSERT INTO `bids_mri_scan_type_rel` (`MRIScanTypeID`, `BIDSCategoryID`, `BIDSScanTypeSubCategoryID`, `BIDSScanTypeID`, `BIDSEchoNumber`) VALUES (40,2,1,1,NULL);
@@ -6,3 +7,4 @@ INSERT INTO `bids_mri_scan_type_rel` (`MRIScanTypeID`, `BIDSCategoryID`, `BIDSSc
 INSERT INTO `bids_mri_scan_type_rel` (`MRIScanTypeID`, `BIDSCategoryID`, `BIDSScanTypeSubCategoryID`, `BIDSScanTypeID`, `BIDSEchoNumber`) VALUES (45,1,NULL,4,NULL);
 INSERT INTO `bids_mri_scan_type_rel` (`MRIScanTypeID`, `BIDSCategoryID`, `BIDSScanTypeSubCategoryID`, `BIDSScanTypeID`, `BIDSEchoNumber`) VALUES (48,3,NULL,5,NULL);
 UNLOCK TABLES;
+SET FOREIGN_KEY_CHECKS = 1;
