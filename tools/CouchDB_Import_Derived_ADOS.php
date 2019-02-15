af0e5475249ec6ec22bf3970ec9f9c03c95ae975
Liza Levitis <llevitis.mcin@gmail.com>
2019-02-15 13:45:35 -0500

    updating the couchdb import script for derived ADOS

--- tools/CouchDB_Import_Derived_ADOS.php
+++ tools/CouchDB_Import_Derived_ADOS.php
@@ -226,8 +226,10 @@ print "ADOSModule: $ADOSModule\n";
         }
         $PSCID=$row['PSCID'];
         $Visit_label  = $row['Visit_label'];
-        $ADOS_SA_CSS  = NDB_BVL_Instrument_IBIS::ADOS_SA_CSS( $ADOSModule, $row['a1'], $row['social_affect_total']);
-        $ADOS_RRB_CSS = NDB_BVL_Instrument_IBIS::ADOS_RRB_CSS($ADOSModule, $row['a1'], $row['restricted_repetitive_behavior_total']);
+        $ados_age = round($row['Candidate_Age']/12);
+
+        $ADOS_SA_CSS  = NDB_BVL_Instrument_IBIS::ADOS_SA_CSS( $ADOSModule, $row['a1'], $row['social_affect_total'], $ados_age);
+        $ADOS_RRB_CSS = NDB_BVL_Instrument_IBIS::ADOS_RRB_CSS($ADOSModule, $row['a1'], $row['restricted_repetitive_behavior_total'], $ados_age);
 
         print $this->CouchDB->replaceDoc(
             "ADOS_Derived:$PSCID" . "_" . $Visit_label,
