e57191ff233b4d646499e094e8cbf82cfd011f7e
Sruthy Mathew <sruthy.mcin@gmail.com>
2019-04-04 12:20:52 -0400

    Initial

---
+++ tables_sql/IBIS_EP_DS_Infant/2019_04_04_add_instruments_IBIS_EP_DS.sql
@@ -0,0 +1,89 @@
+-- QRS to IBIS EP HR, IBIS EP LR, DS Infant at V06, V12, V24
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('QRS', '0', '0', 'Y', 'Visit', 18, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('QRS', '0', '0', 'Y', 'Visit', 18, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('QRS', '0', '0', 'Y', 'Visit', 18, 'V24', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('QRS', '0', '0', 'Y', 'Visit', 19, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('QRS', '0', '0', 'Y', 'Visit', 19, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('QRS', '0', '0', 'Y', 'Visit', 19, 'V24', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('QRS', '0', '0', 'Y', 'Visit', 20, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('QRS', '0', '0', 'Y', 'Visit', 20, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('QRS', '0', '0', 'Y', 'Visit', 20, 'V24', NULL);
+
+-- FYI to IBIS EP HR, IBIS EP LR, DS Infant at V06, V12, V24
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('fyi', '0', '0', 'Y', 'Visit', 18, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('fyi', '0', '0', 'Y', 'Visit', 18, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('fyi', '0', '0', 'Y', 'Visit', 18, 'V24', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('fyi', '0', '0', 'Y', 'Visit', 19, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('fyi', '0', '0', 'Y', 'Visit', 19, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('fyi', '0', '0', 'Y', 'Visit', 19, 'V24', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('fyi', '0', '0', 'Y', 'Visit', 20, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('fyi', '0', '0', 'Y', 'Visit', 20, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('fyi', '0', '0', 'Y', 'Visit', 20, 'V24', NULL);
+
+-- csbs to IBIS EP HR, IBIS EP LR, DS Infant at V06, V12, V24
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('csbs', '0', '0', 'Y', 'Visit', 18, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('csbs', '0', '0', 'Y', 'Visit', 18, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('csbs', '0', '0', 'Y', 'Visit', 18, 'V24', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('csbs', '0', '0', 'Y', 'Visit', 19, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('csbs', '0', '0', 'Y', 'Visit', 19, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('csbs', '0', '0', 'Y', 'Visit', 19, 'V24', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('csbs', '0', '0', 'Y', 'Visit', 20, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('csbs', '0', '0', 'Y', 'Visit', 20, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('csbs', '0', '0', 'Y', 'Visit', 20, 'V24', NULL);
+
+-- aosi to IBIS EP HR, IBIS EP LR, DS Infant at V06, V12, V24
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('aosi', '0', '0', 'Y', 'Visit', 18, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('aosi', '0', '0', 'Y', 'Visit', 18, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('aosi', '0', '0', 'Y', 'Visit', 18, 'V24', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('aosi', '0', '0', 'Y', 'Visit', 19, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('aosi', '0', '0', 'Y', 'Visit', 19, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('aosi', '0', '0', 'Y', 'Visit', 19, 'V24', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('aosi', '0', '0', 'Y', 'Visit', 20, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('aosi', '0', '0', 'Y', 'Visit', 20, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('aosi', '0', '0', 'Y', 'Visit', 20, 'V24', NULL);
+
+--ados instruments; talk to Leigh
