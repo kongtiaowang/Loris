f943f7228610adb3ae80557768d54b002378ed6f
Sruthy Mathew <sruthy.mcin@gmail.com>
2019-04-11 12:56:18 -0400

    adding mullen

--- tables_sql/IBIS_EP_DS_Infant/2019_04_04_add_instruments_IBIS_EP_DS.sql
+++ tables_sql/IBIS_EP_DS_Infant/2019_04_04_add_instruments_IBIS_EP_DS.sql
@@ -86,6 +86,28 @@ VALUES ('aosi', '0', '0', 'Y', 'Visit', 20, 'V12', NULL);
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
 VALUES ('aosi', '0', '0', 'Y', 'Visit', 20, 'V24', NULL);
 
+-- mullen to IBIS EP HR, IBIS EP LR, DS Infant at V06, V12, V24
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('mullen', '0', '0', 'Y', 'Visit', 18, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('mullen', '0', '0', 'Y', 'Visit', 18, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('mullen', '0', '0', 'Y', 'Visit', 18, 'V24', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('mullen', '0', '0', 'Y', 'Visit', 19, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('mullen', '0', '0', 'Y', 'Visit', 19, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('mullen', '0', '0', 'Y', 'Visit', 19, 'V24', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('mullen', '0', '0', 'Y', 'Visit', 20, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('mullen', '0', '0', 'Y', 'Visit', 20, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('mullen', '0', '0', 'Y', 'Visit', 20, 'V24', NULL);
+
 -- ados2 module1 instruments to IBIS EP HR, IBIS EP LR, DS Infant at V06, V12, V24
 
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
