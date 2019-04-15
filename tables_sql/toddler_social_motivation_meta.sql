67050473cf9ce77a7305eaf284d16b46cc8584fc
Sruthy Mathew <sruthy.mcin@gmail.com>
2019-04-15 12:58:27 -0400

    toddler age

--- tables_sql/toddler_social_motivation_meta.sql
+++ tables_sql/toddler_social_motivation_meta.sql
@@ -3,23 +3,17 @@ INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_num
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('toddler_social_motivation','toddler_social_motivation_page2', "Page 2", 2);
 
 
--- toddler_social_motivation to IBIS EP HR, IBIS EP LR, DS Infant at V06, V12, V24
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-VALUES ('toddler_social_motivation', '0', '0', 'Y', 'Visit', 18, 'V06', NULL);
+-- toddler_social_motivation to IBIS EP HR, IBIS EP LR, DS Infant at  V12, V24
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
 VALUES ('toddler_social_motivation', '0', '0', 'Y', 'Visit', 18, 'V12', NULL);
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
 VALUES ('toddler_social_motivation', '0', '0', 'Y', 'Visit', 18, 'V24', NULL);
 
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-VALUES ('toddler_social_motivation', '0', '0', 'Y', 'Visit', 19, 'V06', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
 VALUES ('toddler_social_motivation', '0', '0', 'Y', 'Visit', 19, 'V12', NULL);
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
 VALUES ('toddler_social_motivation', '0', '0', 'Y', 'Visit', 19, 'V24', NULL);
 
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-VALUES ('toddler_social_motivation', '0', '0', 'Y', 'Visit', 20, 'V06', NULL);
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
 VALUES ('toddler_social_motivation', '0', '0', 'Y', 'Visit', 20, 'V12', NULL);
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
