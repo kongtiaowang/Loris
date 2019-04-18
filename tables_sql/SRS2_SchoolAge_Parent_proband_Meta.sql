a5f7a57fecf575387dd8a3798273bf05175f907c
Sruthy Mathew <sruthy.mcin@gmail.com>
2019-04-18 16:32:57 -0400

    SRS2 School Age Proband

---
+++ tables_sql/SRS2_SchoolAge_Parent_proband_Meta.sql
@@ -0,0 +1,5 @@
+INSERT INTO test_names(Test_name, Full_name, Sub_Group, isDirectEntry) VALUES('SRS2_SchoolAge_Parent_proband', "SRS-2 School Age Proband (Send to Mom OR Dad)", 1, 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('SRS2_SchoolAge_Parent_proband','SRS2_SchoolAge_Parent_proband_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('SRS2_SchoolAge_Parent_proband','SRS2_SchoolAge_Parent_proband_page2', "Page 2", 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('SRS2_SchoolAge_Parent_proband','SRS2_SchoolAge_Parent_proband_page3', "Page 3", 3);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('SRS2_SchoolAge_Parent_proband','SRS2_SchoolAge_Parent_proband_page4', "Page 4", 4);
