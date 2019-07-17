f9b42000ef3d25366cae2d1342c36057d2e0b1e9
Sruthy Mathew <sruthy.mcin@gmail.com>
2019-07-17 12:40:36 -0400

    scoring completed

--- tables_sql/instrument_updates/Conners_Parent_addScores.sql
+++ tables_sql/instrument_updates/Conners_Parent_addScores.sql
@@ -16,5 +16,14 @@ ALTER TABLE conners_parent ADD COLUMN PR_T_score varchar(50) default NULL;
 ALTER TABLE conners_parent ADD COLUMN PI_T_score varchar(50) default NULL;
 ALTER TABLE conners_parent ADD COLUMN NI_T_score varchar(50) default NULL;
 
+ALTER TABLE conners_parent ADD COLUMN IN_validity varchar(50) default NULL;
+ALTER TABLE conners_parent ADD COLUMN HY_validity varchar(50) default NULL;
+ALTER TABLE conners_parent ADD COLUMN LP_validity varchar(50) default NULL;
+ALTER TABLE conners_parent ADD COLUMN EF_validity varchar(50) default NULL;
+ALTER TABLE conners_parent ADD COLUMN AG_validity varchar(50) default NULL;
+ALTER TABLE conners_parent ADD COLUMN PR_validity varchar(50) default NULL;
+ALTER TABLE conners_parent ADD COLUMN PI_validity varchar(150) default NULL;
+ALTER TABLE conners_parent ADD COLUMN NI_validity varchar(150) default NULL;
+
 
 
