aca1509f3fabce9371ebc1f3e714999786d905a1
Sruthy Mathew <sruthy.mcin@gmail.com>
2019-07-23 15:23:34 -0400

    Minor Fixes

--- tables_sql/instrument_updates/Conners_Parent_addScores.sql
+++ tables_sql/instrument_updates/Conners_Parent_addScores.sql
@@ -16,14 +16,14 @@ ALTER TABLE conners_parent ADD COLUMN PR_T_score varchar(50) default NULL;
 ALTER TABLE conners_parent ADD COLUMN PI_T_score varchar(50) default NULL;
 ALTER TABLE conners_parent ADD COLUMN NI_T_score varchar(50) default NULL;
 
-ALTER TABLE conners_parent ADD COLUMN IN_validity varchar(50) default NULL;
-ALTER TABLE conners_parent ADD COLUMN HY_validity varchar(50) default NULL;
-ALTER TABLE conners_parent ADD COLUMN LP_validity varchar(50) default NULL;
-ALTER TABLE conners_parent ADD COLUMN EF_validity varchar(50) default NULL;
-ALTER TABLE conners_parent ADD COLUMN AG_validity varchar(50) default NULL;
-ALTER TABLE conners_parent ADD COLUMN PR_validity varchar(50) default NULL;
-ALTER TABLE conners_parent ADD COLUMN PI_validity varchar(150) default NULL;
-ALTER TABLE conners_parent ADD COLUMN NI_validity varchar(150) default NULL;
+ALTER TABLE conners_parent ADD COLUMN IN_response_style varchar(50) default NULL;
+ALTER TABLE conners_parent ADD COLUMN HY_response_style varchar(50) default NULL;
+ALTER TABLE conners_parent ADD COLUMN LP_response_style varchar(50) default NULL;
+ALTER TABLE conners_parent ADD COLUMN EF_response_style varchar(50) default NULL;
+ALTER TABLE conners_parent ADD COLUMN AG_response_style varchar(50) default NULL;
+ALTER TABLE conners_parent ADD COLUMN PR_response_style varchar(50) default NULL;
+ALTER TABLE conners_parent ADD COLUMN PI_response_style varchar(150) default NULL;
+ALTER TABLE conners_parent ADD COLUMN NI_response_style varchar(150) default NULL;
 
 
 
