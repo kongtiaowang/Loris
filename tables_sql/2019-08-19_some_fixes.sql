ee60d0fba2021d6f5b17894b4a7d4e9e2c452c32
Sruthy Mathew <sruthy.mcin@gmail.com>
2019-08-19 13:55:25 -0400

    Instr arranging

--- tables_sql/2019-08-19_some_fixes.sql
+++ tables_sql/2019-08-19_some_fixes.sql
@@ -3,3 +3,7 @@ UPDATE test_battery  SET Active ='N' where Visit_label IS NULL and SubprojectID 
 UPDATE participant_accounts SET Email ='lorideleon@live.com' where SessionID ='6005'and Test_name='ABC2';
 -- Redmine 16782
 UPDATE candidate SET Active ='N' where PSCID ='PHI3000';
+
+UPDATE test_names tn
+SET tn.Sub_group = (SELECT ID FROM test_subgroups where Subgroup_name='Miscellaneous (Data no more collected)')
+WHERE tn.test_name IN ('das_schoolage','ADULT_SELF_REPORT','ABCL','SRS2_InformantReport','SRS2_SelfReport')
