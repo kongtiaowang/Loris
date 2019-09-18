94d50c92426333acfa9ab4494ae63070fa9fd1b3
Sruthy Mathew <sruthy.mcin@gmail.com>
2019-09-18 10:56:30 -0400

    spaces

--- tables_sql/2019-09-18_add_to_test_battery.sql
+++ tables_sql/2019-09-18_add_to_test_battery.sql
@@ -1,4 +1,3 @@
-
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
 VALUES ('adi_r_proband', '0', '0', 'Y', 'Visit', 18, 'V06', NULL);
 
