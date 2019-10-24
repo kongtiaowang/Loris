02dffdac1ece45a262ff32830cd88593a730c6cf
Sruthy Mathew <sruthy.mcin@gmail.com>
2019-10-24 15:03:02 -0400

    SRS2 Scoring and Minor Scoring Fixes

---
+++ tables_sql/instrument_updates/SRS2_SA_addScores.sql
@@ -0,0 +1,64 @@
+ALTER TABLE SRS2_SchoolAge_Parent ADD COLUMN total_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent ADD COLUMN DSM5_SCI_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent ADD COLUMN DSM5_RRB_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent ADD COLUMN Awr_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent ADD COLUMN Cog_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent ADD COLUMN Com_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent ADD COLUMN Mot_score TEXT default NULL;
+
+
+ALTER TABLE SRS2_SchoolAge_Parent ADD COLUMN total_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent ADD COLUMN DSM5_SCI_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent ADD COLUMN DSM5_RRB_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent ADD COLUMN Awr_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent ADD COLUMN Cog_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent ADD COLUMN Com_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent ADD COLUMN Mot_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent ADD COLUMN score_validity TEXT default NULL;
+
+
+ALTER TABLE SRS2_SchoolAge_Parent_proband ADD COLUMN total_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent_proband ADD COLUMN DSM5_SCI_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent_proband ADD COLUMN DSM5_RRB_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent_proband ADD COLUMN Awr_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent_proband ADD COLUMN Cog_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent_proband ADD COLUMN Com_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent_proband ADD COLUMN Mot_score TEXT default NULL;
+
+
+ALTER TABLE SRS2_SchoolAge_Parent_proband ADD COLUMN total_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent_proband ADD COLUMN DSM5_SCI_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent_proband ADD COLUMN DSM5_RRB_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent_proband ADD COLUMN Awr_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent_proband ADD COLUMN Cog_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent_proband ADD COLUMN Com_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent_proband ADD COLUMN Mot_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge_Parent_proband ADD COLUMN score_validity TEXT default NULL;
+
+ALTER TABLE SRS2_SchoolAge ADD COLUMN total_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge ADD COLUMN DSM5_SCI_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge ADD COLUMN DSM5_RRB_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge ADD COLUMN Awr_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge ADD COLUMN Cog_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge ADD COLUMN Com_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge ADD COLUMN Mot_score TEXT default NULL;
+
+
+ALTER TABLE SRS2_SchoolAge ADD COLUMN total_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge ADD COLUMN DSM5_SCI_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge ADD COLUMN DSM5_RRB_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge ADD COLUMN Awr_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge ADD COLUMN Cog_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge ADD COLUMN Com_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge ADD COLUMN Mot_T_score TEXT default NULL;
+ALTER TABLE SRS2_SchoolAge ADD COLUMN score_validity TEXT default NULL;
+
+
+
+
+
+
+
+
+
+
