1a51aa595f70374d46655e0570725d44b408e3c8
Sruthy Mathew <sruthy.mcin@gmail.com>
2019-11-13 09:28:00 -0500

    Minor patch fix

--- tables_sql/radiological_review_vsa_Meta.sql
+++ tables_sql/radiological_review_vsa_Meta.sql
@@ -3,23 +3,29 @@ INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_num
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Radiology_Review_VSA','Radiology_Review_VSA_page2', "Page 2", 2);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Radiology_Review_VSA','Radiology_Review_VSA_page3', "Page 3", 3);
 
---  IBISSA LR & IBISSA HR get the Rad review SA form across ALL visits
+--  IBIS1 & 2(subprojects) VSA visit lables
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 12, NULL, NULL);
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 1, 'VSA', NULL);
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 13, NULL, NULL);
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 2, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 3, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 9, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 10, 'VSA', NULL);
+
+--  DS SA VSA visit label
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 21, 'VSA', NULL);
 
 --  DS Infant gets the Rad review SA form across ALL visits
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
     VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 20, NULL, NULL);
-
---  All VSA visits get the Rad review SA form 
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', NULL, 'VSA', NULL);
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 23, NULL, NULL);
 
---  DS SA gets the Rad review SA form across ALL visits
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 21, NULL, NULL);
+
 
 --  IBIS EP High Risk & IBIS EP Low Risk get the Rad review SA form across ALL visits
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
