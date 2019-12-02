c6aa55c0f9bf286f697dcb048992477ed6ced3dc
Sruthy Mathew <sruthy.mcin@gmail.com>
2019-12-02 10:44:43 -0500

    adding test battery patches

--- tables_sql/EEG_Meta.sql
+++ tables_sql/EEG_Meta.sql
@@ -1,7 +1,53 @@
-INSERT INTO test_names(Test_name, Full_name, Sub_Group,isDirectEntry) VALUES('EEG', "EEG", 1 , 1);
+INSERT INTO test_names(Test_name, Full_name, Sub_Group) VALUES('EEG', "EEG", 8);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page1', "Page 1", 1);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page2', "Page 2", 2);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page3', "Page 3", 3);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page4', "Page 4", 4);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page5', "Page 5", 5);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page6', "Page 6", 6);
+
+--  IBIS1 & 2(subprojects) VSA visit lables
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 1, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 2, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 3, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 9, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 10, 'VSA', NULL);
+
+--  DS SA VSA visit label
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 21, 'VSA', NULL);
+
+--  DS Infant project gets the Rad review SA form across ALL visits
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 20, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 20, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 20, 'V24', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 23, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 23, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 23, 'V24', NULL);
+
+
+
+--  IBIS EP High Risk & IBIS EP Low Risk get the Rad review SA form across ALL visits
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 18, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 18, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 18, 'V24', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 19, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 19, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 19, 'V24', NULL);
