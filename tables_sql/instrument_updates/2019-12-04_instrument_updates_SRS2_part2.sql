36ce757261e9a4ec022336924dca5a56416d037b
Sruthy Mathew <sruthy.mcin@gmail.com>
2019-12-04 12:10:46 -0500

    spacing

--- tables_sql/instrument_updates/2019-12-04_instrument_updates_SRS2_part2.sql
+++ tables_sql/instrument_updates/2019-12-04_instrument_updates_SRS2_part2.sql
@@ -2,4 +2,3 @@ ALTER TABLE SRS2_InformantReport_Dad_On_Mom ADD COLUMN `informant_rel_to_child` 
 ALTER TABLE SRS2_InformantReport_Mom_On_Dad ADD COLUMN `informant_rel_to_child` enum('biological_mother','biological_father','','other') DEFAULT NULL;
 ALTER TABLE SRS2_InformantReport_Dad_On_Mom ADD COLUMN `informant_rel_to_child_other` TEXT DEFAULT NULL;
 ALTER TABLE SRS2_InformantReport_Mom_On_Dad ADD COLUMN `informant_rel_to_child_other` TEXT DEFAULT NULL;
-
