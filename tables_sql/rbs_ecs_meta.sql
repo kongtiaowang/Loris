6c3bfa80a5a3a7032997caa00108b4d9ea217aab
Ling Ma <ianma2000@hotmail.com>
2020-02-14 10:34:08 -0500

    RBS-ECS modifications

--- tables_sql/rbs_ecs_meta.sql
+++ tables_sql/rbs_ecs_meta.sql
@@ -1,36 +1,7 @@
 INSERT INTO test_names (test_name, full_name, sub_group, isdirectentry) VALUES
-('rbs_ecs', 'Repetitive Behavior Scales Early Childhood Supplement (RBS-ECS)', 1, 0);
+('rbs_ecs', 'Repetitive Behavior Scales Early Childhood Supplement (RBS-ECS)', 1, 1);
 
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('rbs_ecs', 'rbs_ecs_page1', "Page 1", 1);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('rbs_ecs', 'rbs_ecs_page2', "Page 2", 1);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('rbs_ecs', 'rbs_ecs_page3', "Page 3", 1);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('rbs_ecs', 'rbs_ecs_page4', "Page 4", 1);
-
---  DS Infant project gets the rbs_ecs form across ALL visits
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-    VALUES ('rbs_ecs', '0', '0', 'Y', 'Visit', 20, 'V06', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-    VALUES ('rbs_ecs', '0', '0', 'Y', 'Visit', 20, 'V12', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-    VALUES ('rbs_ecs', '0', '0', 'Y', 'Visit', 20, 'V24', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-    VALUES ('rbs_ecs', '0', '0', 'Y', 'Visit', 23, 'V06', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-    VALUES ('rbs_ecs', '0', '0', 'Y', 'Visit', 23, 'V12', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-    VALUES ('rbs_ecs', '0', '0', 'Y', 'Visit', 23, 'V24', NULL);
-
---  IBIS EP High Risk & IBIS EP Low Risk get the rbs_ecs form across ALL visits
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-    VALUES ('rbs_ecs', '0', '0', 'Y', 'Visit', 18, 'V06', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-    VALUES ('rbs_ecs', '0', '0', 'Y', 'Visit', 18, 'V12', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-    VALUES ('rbs_ecs', '0', '0', 'Y', 'Visit', 18, 'V24', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-    VALUES ('rbs_ecs', '0', '0', 'Y', 'Visit', 19, 'V06', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-    VALUES ('rbs_ecs', '0', '0', 'Y', 'Visit', 19, 'V12', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-    VALUES ('rbs_ecs', '0', '0', 'Y', 'Visit', 19, 'V24', NULL);
-
