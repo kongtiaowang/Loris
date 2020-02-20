1f4a3d06dcb9a31a3186515ceaf957798de3cb2a
Sruthy Mathew <sruthy.mcin@gmail.com>
2020-02-20 14:29:55 -0500

    Adding ACEFamilyMedHx to all missing subprojects

---
+++ tables_sql/instrument_updates/2020-02-20_add_ACE_Family_med_test_battery.sql
@@ -0,0 +1,45 @@
+--  IBIS1 & 2(subprojects) VSA visit label
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 1, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 2, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 3, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 9, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 10, 'VSA', NULL);
+
+--  DS SA VSA visit label
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 21, 'VSA', NULL);
+
+--  DS Infant(across all sub projects & visit labels)
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 20, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 20, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 20, 'V24', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 23, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 23, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 23, 'V24', NULL);
+
+
+
+--  IBIS EP High Risk & IBIS EP (across all sub projects & visit labels)
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 18, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 18, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 18, 'V24', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 19, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 19, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('ACEFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 19, 'V24', NULL);
