13216c7377be78a71b0c27ce007e6284cc21728d
Ling Ma <lingyun.ma@mcin.ca>
2020-03-26 11:37:09 -0400

    Survey Version of APSI (Autism Parent Screen for Infants)

---
+++ tables_sql/apsi_survey_meta.sql
@@ -0,0 +1,2 @@
+INSERT INTO test_names(Test_name, Full_name, Sub_Group, IsDirectEntry) VALUES('apsi_survey', 'Autism Parent Screen for Infants Survey (APSI Survey)', 1, 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('apsi_survey','apsi_survey_page1', 'Page 1', 1);
