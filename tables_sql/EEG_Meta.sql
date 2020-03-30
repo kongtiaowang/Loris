eff1b87245779926f7a379b40d1200974364ac64
Ling Ma <lingyun.ma@mcin.ca>
2020-03-30 14:14:13 -0400

    redmine 17618 for EEG modification

--- tables_sql/EEG_Meta.sql
+++ tables_sql/EEG_Meta.sql
@@ -1,11 +1,10 @@
 INSERT INTO test_names(Test_name, Full_name, Sub_Group) VALUES('EEG', "EEG", 8);
-INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page1', "EEG Net", 1);
-INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page2', "Paradigm 1: Resting Bubbles", 2);
-INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page3', "Paradigm 2: VEP", 3);
-INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page4', "Paradigm 3: AEP", 4);
-INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page5', "Paradigm 4: Social/Non-social", 5);
-INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page6', "Overall EEG Session Information", 6);
-
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page1', 'PARADIGM 1: Resting', 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page2', 'PARADIGM 2: Visual', 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page3', 'PARADIGM 3: Resting', 3);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page4', 'PARADIGM 4: Auditory', 4);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page5', 'PARADIGM 5: Resting', 5);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('EEG','EEG_page6', 'General Session Notes', 6);
 
 --  DS Infant project gets the Rad review SA form across ALL visits
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
@@ -20,8 +19,6 @@ INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, Subp
     VALUES ('EEG', '0', '0', 'Y', 'Visit', 23, 'V12', NULL);
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
     VALUES ('EEG', '0', '0', 'Y', 'Visit', 23, 'V24', NULL);
-
-
 
 --  IBIS EP High Risk & IBIS EP Low Risk get the Rad review SA form across ALL visits
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
