de9637ddb8b773f9e4c07f00416d8417120247b6
Ling Ma <lingyun.ma@mcin.ca>
2020-04-01 09:53:31 -0400

    IBQ-R (Infant Behavior Questionnaire - Revised) survey

---
+++ tables_sql/ibq_r_survey_meta.sql
@@ -0,0 +1,9 @@
+INSERT INTO test_names(Test_name, Full_name, Sub_Group, IsDirectEntry) VALUES('ibq_r_survey', 'IBQ-R (Infant Behavior Questionnaire) Survey', 10, 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('ibq_r_survey','ibq_r_survey_page1', 'Page 1', 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('ibq_r_survey','ibq_r_survey_page2', 'Page 2', 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('ibq_r_survey','ibq_r_survey_page3', 'Page 3', 3);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('ibq_r_survey','ibq_r_survey_page4', 'Page 4', 4);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('ibq_r_survey','ibq_r_survey_page5', 'Page 5', 5);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('ibq_r_survey','ibq_r_survey_page6', 'Page 6', 6);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('ibq_r_survey','ibq_r_survey_page7', 'Page 7', 7);
+
