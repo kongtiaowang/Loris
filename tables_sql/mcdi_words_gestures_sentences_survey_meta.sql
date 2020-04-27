237d576905e515723e766283ea78fada46ea119a
Ling Ma <lingyun.ma@mcin.ca>
2020-04-27 12:19:14 -0400

    MCDI Word & Sentences & Gestures Survey

---
+++ tables_sql/mcdi_words_gestures_sentences_survey_meta.sql
@@ -0,0 +1,12 @@
+-- Meta
+INSERT INTO test_names(Test_name, Full_name, Sub_Group, IsDirectEntry) VALUES('mcdi_words_gestures_sentences_survey', 'MCDI - Words, Gestures & Sentences Survey', 13, 1);
+
+INSERT INTO instrument_subtests  (Test_name, Subtest_name, Description, Order_number) VALUES('mcdi_words_gestures_sentences_survey', 'mcdi_words_gestures_sentences_survey_page1', 'Part I: A1-3', 1);
+INSERT INTO instrument_subtests  (Test_name, Subtest_name, Description, Order_number) VALUES('mcdi_words_gestures_sentences_survey', 'mcdi_words_gestures_sentences_survey_page2', 'Part I: A4-5', 2);
+INSERT INTO instrument_subtests  (Test_name, Subtest_name, Description, Order_number)VALUES( 'mcdi_words_gestures_sentences_survey', 'mcdi_words_gestures_sentences_survey_page3', 'Part I: A6-8', 3);
+INSERT INTO instrument_subtests  (Test_name, Subtest_name, Description, Order_number) VALUES('mcdi_words_gestures_sentences_survey', 'mcdi_words_gestures_sentences_survey_page4', 'Part I: A9-11', 4);
+INSERT INTO instrument_subtests  (Test_name, Subtest_name, Description, Order_number) VALUES('mcdi_words_gestures_sentences_survey', 'mcdi_words_gestures_sentences_survey_page5', 'Part I: A12-14', 5);
+INSERT INTO instrument_subtests  (Test_name, Subtest_name, Description, Order_number) VALUES('mcdi_words_gestures_sentences_survey', 'mcdi_words_gestures_sentences_survey_page6', 'Part I: A15-18', 6);
+INSERT INTO instrument_subtests  (Test_name, Subtest_name, Description, Order_number) VALUES('mcdi_words_gestures_sentences_survey', 'mcdi_words_gestures_sentences_survey_page7', 'Part I: A19-22, B', 7);
+INSERT INTO instrument_subtests  (Test_name, Subtest_name, Description, Order_number) VALUES( 'mcdi_words_gestures_sentences_survey', 'mcdi_words_gestures_sentences_survey_page8', 'Part II', 8);
+INSERT INTO instrument_subtests  (Test_name, Subtest_name, Description, Order_number) VALUES( 'mcdi_words_gestures_sentences_survey', 'mcdi_words_gestures_sentences_survey_page9', 'Part III', 9);
