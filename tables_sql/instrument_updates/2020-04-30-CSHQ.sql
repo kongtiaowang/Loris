1cb6f8b818b126a4c262607f43e499a00d0b6fbe
Ling Ma <lingyun.ma@mcin.ca>
2020-04-30 22:09:54 -0400

    add fine restriction

---
+++ tables_sql/instrument_updates/2020-04-30-CSHQ.sql
@@ -0,0 +1,9 @@
+ALTER TABLE CSHQ ADD COLUMN bed_time_hours varchar(10) AFTER bed_time;
+ALTER TABLE CSHQ ADD COLUMN bed_time_mins varchar(10) AFTER bed_time_hours;
+ALTER TABLE CSHQ ADD COLUMN bed_time_status enum('dnk','refusal','not_answered') AFTER bed_time_mins;
+ALTER TABLE CSHQ ADD COLUMN sleep_behavior_duration_hours varchar(10) AFTER sleep_behavior_duration;
+ALTER TABLE CSHQ ADD COLUMN sleep_behavior_duration_mins varchar(10) AFTER sleep_behavior_duration_hours;
+ALTER TABLE CSHQ ADD COLUMN morning_waking_time_hours varchar(10) AFTER morning_waking_time;
+ALTER TABLE CSHQ ADD COLUMN morning_waking_time_mins varchar(10) AFTER morning_waking_time_hours;
+ALTER TABLE CSHQ MODIFY COLUMN `sleep_behavior_duration_status` enum('dnk','refusal','not_answered');
+ALTER TABLE CSHQ MODIFY COLUMN `morning_waking_time_status` enum('dnk','refusal','not_answered');
