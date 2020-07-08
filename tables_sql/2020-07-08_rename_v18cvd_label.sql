6a99991fbd94b88906b0c7dde26c309dfcf93b23
Suzanne Lee <suzannelee.mcin@gmail.com>
2020-07-08 15:20:59 -0400

    update patch

---
+++ tables_sql/2020-07-08_rename_v18cvd_label.sql
@@ -0,0 +1,2 @@
+UPDATE `Visit_Windows` SET Visit_label = 'V18-CVD' WHERE Visit_label = 'V18-CVD-VinelandSubject';
+UPDATE `session` SET Visit_label = 'V18-CVD' WHERE Visit_label = 'V18-CVD-VinelandSubject';
