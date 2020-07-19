8dfd3840c02075e667e7f9de57d13465a9d0e5ae
Suzanne Lee <suzannelee.mcin@gmail.com>
2020-07-18 20:01:42 -0400

    add survey to survey test battery

---
+++ tables_sql/2020-07-20_add_rbs_ec_general_timepoints.sql
@@ -0,0 +1,8 @@
+INSERT INTO survey_test_battery (
+    Test_name,
+    Visit_label
+)
+VALUES
+    ('rbs_ec_general_survey','V12'),
+    ('rbs_ec_general_survey','V12-CVD'),
+    ('rbs_ec_general_survey','V24');
