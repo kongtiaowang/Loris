1a19ddc664ec5a7e821fd01ca99abd65ddcfe5b3
Suzanne Lee <suzannelee.mcin@gmail.com>
2020-07-22 11:14:52 -0400

    insert examiner 324 into examiners_psc_rel

---
+++ tables_sql/2020-07-22_add_kristen_hammond_to_examiner_dropdown.sql
@@ -0,0 +1,12 @@
+-- Add Kristen Hammond to examiners_psc_rel for all CenterIDs
+INSERT INTO `examiners_psc_rel` (`examinerID`, `centerID`, `active`, `pending_approval`) VALUES ('324', '1', 'Y', 'N');
+INSERT INTO `examiners_psc_rel` (`examinerID`, `centerID`, `active`, `pending_approval`) VALUES ('324', '2', 'Y', 'N');
+INSERT INTO `examiners_psc_rel` (`examinerID`, `centerID`, `active`, `pending_approval`) VALUES ('324', '3', 'Y', 'N');
+INSERT INTO `examiners_psc_rel` (`examinerID`, `centerID`, `active`, `pending_approval`) VALUES ('324', '4', 'Y', 'N');
+INSERT INTO `examiners_psc_rel` (`examinerID`, `centerID`, `active`, `pending_approval`) VALUES ('324', '5', 'Y', 'N');
+INSERT INTO `examiners_psc_rel` (`examinerID`, `centerID`, `active`, `pending_approval`) VALUES ('324', '6', 'Y', 'N');
+INSERT INTO `examiners_psc_rel` (`examinerID`, `centerID`, `active`, `pending_approval`) VALUES ('324', '7', 'Y', 'N');
+INSERT INTO `examiners_psc_rel` (`examinerID`, `centerID`, `active`, `pending_approval`) VALUES ('324', '8', 'Y', 'N');
+INSERT INTO `examiners_psc_rel` (`examinerID`, `centerID`, `active`, `pending_approval`) VALUES ('324', '9', 'Y', 'N');
+INSERT INTO `examiners_psc_rel` (`examinerID`, `centerID`, `active`, `pending_approval`) VALUES ('324', '10', 'Y', 'N');
+INSERT INTO `examiners_psc_rel` (`examinerID`, `centerID`, `active`, `pending_approval`) VALUES ('324', '11', 'Y', 'N');
