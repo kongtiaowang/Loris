3c0d68225f50241fdf9fb28496fbf2a1355a40d2
Suzanne Lee <suzannelee.mcin@gmail.com>
2020-07-28 20:16:47 -0400

    med psyc history survey class, create table, meta

---
+++ tables_sql/Med_Psych_History_survey_Meta.sql
@@ -0,0 +1,9 @@
+INSERT INTO test_names (Test_name, Full_name, Sub_Group, isDirectEntry) VALUES ('Med_Psych_History_survey', "Medical and Psychiatric History - Survey", 1, 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Med_Psych_History_survey','Med_Psych_History_survey_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Med_Psych_History_survey','Med_Psych_History_survey_page2', "Page 2", 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Med_Psych_History_survey','Med_Psych_History_survey_page3', "Page 3", 3);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Med_Psych_History_survey','Med_Psych_History_survey_page4', "Page 4", 4);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Med_Psych_History_survey','Med_Psych_History_survey_page5', "Page 5", 5);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Med_Psych_History_survey','Med_Psych_History_survey_page6', "Page 6", 6);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Med_Psych_History_survey','Med_Psych_History_survey_page7', "Page 7", 7);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Med_Psych_History_survey','Med_Psych_History_survey_page8', "Page 8", 8);
