57f80684ebea2a3e664e27dfe2971763ffa29047
Suzanne Lee <suzannelee.mcin@gmail.com>
2020-08-12 16:01:57 -0400

    fix IPSC_consent_data typo

--- tables_sql/IPSC_tracking_form.sql
+++ tables_sql/IPSC_tracking_form.sql
@@ -1,25 +1,20 @@
 CREATE TABLE `IPSC_tracking_form` (
-`CommentID` varchar(255) NOT NULL default '',
-
-                            `UserID` varchar(255) default NULL,
-
-                            `Examiner` varchar(255) default NULL,
-
-                            `Testdate` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-
-                            `Data_entry_completion_status` enum('Incomplete','Complete') NOT NULL default 'Incomplete',
-`Date_taken` date default NULL,
-`Candidate_Age` varchar(255) default NULL,
-`Window_Difference` varchar(255) default NULL,
-`recruiter_asked_blood_draw` enum('not_yet_called','yes','no') default NULL,
-`IPSC_consent_data` date default NULL,
-`blood_draw_location` enum('site','take_home_kit_sent') default NULL,
-`fedex_tracking_number` text default NULL,
-`blood_draw_date` date default NULL,
-`ipsc_notes` text default NULL,
-PRIMARY KEY  (`CommentID`)
-
-              ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    `CommentID` varchar(255) NOT NULL default '',
+    `UserID` varchar(255) default NULL,
+    `Examiner` varchar(255) default NULL,
+    `Testdate` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `Data_entry_completion_status` enum('Incomplete','Complete') NOT NULL default 'Incomplete',
+    `Date_taken` date default NULL,
+    `Candidate_Age` varchar(255) default NULL,
+    `Window_Difference` varchar(255) default NULL,
+    `recruiter_asked_blood_draw` enum('not_yet_called','yes','no') default NULL,
+    `IPSC_consent_date` date default NULL,
+    `blood_draw_location` enum('site','take_home_kit_sent') default NULL,
+    `fedex_tracking_number` text default NULL,
+    `blood_draw_date` date default NULL,
+    `ipsc_notes` text default NULL,
+    PRIMARY KEY  (`CommentID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Meta Files
 INSERT INTO test_names(Test_name, Full_name, Sub_Group) VALUES('IPSC_tracking_form', 'IPSC Tracking Form', 6);
