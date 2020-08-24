9385882dcfa3e3a48a8dfa971dfe0353267a7201
Suzanne Lee <suzannelee.mcin@gmail.com>
2020-08-24 13:00:22 -0400

    covid child self report survey class + sql files

---
+++ tables_sql/COVID_Child_Self_Report_survey_Meta.sql
@@ -0,0 +1,7 @@
+INSERT INTO test_names (Test_name, Full_name, Sub_Group, isDirectEntry) VALUES ('COVID_Child_Self_Report_survey', "COVID - Child Self Report - Survey", 1, 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('COVID_Child_Self_Report_survey','COVID_Child_Self_Report_survey_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('COVID_Child_Self_Report_survey','COVID_Child_Self_Report_survey_page2', "Page 2", 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('COVID_Child_Self_Report_survey','COVID_Child_Self_Report_survey_page3', "Page 3", 3);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('COVID_Child_Self_Report_survey','COVID_Child_Self_Report_survey_page4', "Page 4", 4);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('COVID_Child_Self_Report_survey','COVID_Child_Self_Report_survey_page5', "Page 5", 5);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('COVID_Child_Self_Report_survey','COVID_Child_Self_Report_survey_page6', "Page 6", 6);
