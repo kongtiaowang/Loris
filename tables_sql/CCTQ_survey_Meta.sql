6bb562a3115e6316e09a2bc850a12519155dadcd
Suzanne Lee <suzannelee.mcin@gmail.com>
2020-08-26 11:41:00 -0400

    CCTQ survey class + sql files

---
+++ tables_sql/CCTQ_survey_Meta.sql
@@ -0,0 +1,6 @@
+INSERT INTO test_names (Test_name, Full_name, Sub_Group, isDirectEntry) VALUES ('CCTQ_survey', "CCTQ (Children's ChronoType Questionnaire) - Survey", 1, 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CCTQ_survey','CCTQ_survey_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CCTQ_survey','CCTQ_survey_page2', "Page 2", 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CCTQ_survey','CCTQ_survey_page3', "Page 3", 3);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CCTQ_survey','CCTQ_survey_page4', "Page 4", 4);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CCTQ_survey','CCTQ_survey_page5', "Page 5", 5);
