3aaf76dc27b3603463616048a1916c148ecc1096
suzanne-lee <suzannebhlee@gmail.com>
2020-08-27 11:44:20 -0400

    remove vsa and vsa-cvd

--- tables_sql/CCTQ_survey_Meta.sql
+++ tables_sql/CCTQ_survey_Meta.sql
@@ -4,6 +4,4 @@ INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_num
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CCTQ_survey','CCTQ_survey_page3', "Page 3", 3);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CCTQ_survey','CCTQ_survey_page4', "Page 4", 4);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('CCTQ_survey','CCTQ_survey_page5', "Page 5", 5);
-INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('CCTQ_survey','VSA');
-INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('CCTQ_survey','VSA-CVD');
 INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('CCTQ_survey','VSA-Sleep');
