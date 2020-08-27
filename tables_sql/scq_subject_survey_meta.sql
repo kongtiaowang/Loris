521f3e0ad9d1cf2824fe4f0399154a3e31643b3e
Sruthy Mathew <sruthy.mcin@gmail.com>
2020-08-27 09:12:37 -0400

    typo

--- tables_sql/scq_subject_survey_meta.sql
+++ tables_sql/scq_subject_survey_meta.sql
@@ -1,2 +1,2 @@
-INSERT INTO test_names(Test_name, Full_name, Sub_Group, isDirectEntry) VALUES('scq_subject_survey', "SCQ - Subject (Social Communication Questionnaire - Subject) - Survey", 9, 1);
+INSERT INTO test_names(Test_name, Full_name, Sub_Group, isDirectEntry) VALUES('scq_subject_survey', "SCQ Subject (Social Communication Questionnaire - Subject) - Survey", 9, 1);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('scq_subject_survey','scq_subject_survey_page1', "Page 1", 1);
