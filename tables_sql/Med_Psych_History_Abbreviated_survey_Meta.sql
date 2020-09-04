9eacd1705247a7e3954cdd580c8fc125fd4db34d
Suzanne Lee <suzannelee.mcin@gmail.com>
2020-09-04 12:01:12 -0400

    med psych history abbreviated survey class + sql

---
+++ tables_sql/Med_Psych_History_Abbreviated_survey_Meta.sql
@@ -0,0 +1,9 @@
+INSERT INTO test_names (Test_name, Full_name, Sub_Group, isDirectEntry) VALUES ('Med_Psych_History_Abbreviated_survey', "Medical and Psychiatry Form - Abbreviated - Survey", 1, 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Med_Psych_History_Abbreviated_survey','Med_Psych_History_Abbreviated_survey_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Med_Psych_History_Abbreviated_survey','Med_Psych_History_Abbreviated_survey_page2', "Page 2", 2);
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('Med_Psych_History_Abbreviated_survey','V06');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('Med_Psych_History_Abbreviated_survey','V6-CVD');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('Med_Psych_History_Abbreviated_survey','V12');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('Med_Psych_History_Abbreviated_survey','V12-CVD');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('Med_Psych_History_Abbreviated_survey','V24');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('Med_Psych_History_Abbreviated_survey','V24-CVD');
