615214ff267505bf5aac1f46a48c1474d1fd0297
nicolasbrossard <nicolasbrossard.mni@gmail.com>
2020-09-16 14:03:00 -0400

    Fixed incorrect path of delete SQL script.

--- tools/single_use/normalize_mri_protocol_range_data.php
+++ tools/single_use/normalize_mri_protocol_range_data.php
@@ -126,14 +126,14 @@ $tables_to_normalize = array("mri_protocol", "mri_protocol_checks");
 foreach ($tables_to_normalize as $table) {
     split_ranges($table, $printToSQL, $output);
 }
-$filename = __DIR__ . "/../../project/tables_sql/DELETE_mri_protocol_range_columns.sql";
+$filename = __DIR__ . "/../../tables_sql/DELETE_mri_protocol_range_columns.sql";
 if ($printToSQL) {
     echo("Please run the SQL patch found in: " . $filename . "\n");
 }
 
 function _exportSQL ($output) {
     //export file
-    $filename = __DIR__ . "/../../project/tables_sql/DELETE_mri_protocol_range_columns.sql";
+    $filename = __DIR__ . "/../../tables_sql/DELETE_mri_protocol_range_columns.sql";
     $fp       = fopen($filename, "a");
     fwrite($fp, $output);
     fclose($fp);
