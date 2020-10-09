7d63544d483f457a4f533b3efb1fb381a29e9add
Sruthy Mathew <sruthy.mcin@gmail.com>
2020-10-09 12:21:23 -0400

    Survey Test battery updates

---
+++ tables_sql/2020-10-09_Redmine_18372.sql
@@ -0,0 +1,8 @@
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('QRS_Proband','V06');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('QRS_Proband','V6-CVD');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('SRS2_SchoolAge_Parent_proband','V06');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('SRS2_SchoolAge_Parent_proband','V6-CVD');
+-- Delete list of surveys added to Screening vist label as per Santiago
+DELETE * from survey_test_battery where Visit_label='Screening';
+
+
