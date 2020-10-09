e38881dc0bfb1be11c697e2f468d9a39ba0c454c
Sruthy Mathew <sruthy.mcin@gmail.com>
2020-10-09 12:23:05 -0400

    spaces

--- tables_sql/2020-10-09_Redmine_18372.sql
+++ tables_sql/2020-10-09_Redmine_18372.sql
@@ -4,5 +4,3 @@ INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('SRS2_SchoolAge
 INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('SRS2_SchoolAge_Parent_proband','V6-CVD');
 -- Delete list of surveys added to Screening vist label as per Santiago
 DELETE * from survey_test_battery where Visit_label='Screening';
-
-
