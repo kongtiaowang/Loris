d2880344c99fb69c32c9bbfe7302c54624f00b49
Sruthy Mathew <sruthy.mcin@gmail.com>
2020-10-16 08:31:41 -0400

    Redmine 18398

---
+++ tables_sql/test_battery_updates/2020-10-16_test_battery_updates.sql
@@ -0,0 +1,24 @@
+-- Redmine 18398
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('vrrsb', '0', '0', 'Y', 'Visit', 18, 'V18-CVD', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('vrrsb', '0', '0', 'Y', 'Visit', 19, 'V18-CVD', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('vrrsb', '0', '0', 'Y', 'Visit', 18, 'V24-CVD', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('vrrsb', '0', '0', 'Y', 'Visit', 19, 'V24-CVD', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('BSRC', '0', '0', 'Y', 'Visit', 18, 'V18-CVD', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('BSRC', '0', '0', 'Y', 'Visit', 19, 'V18-CVD', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('BSRC', '0', '0', 'Y', 'Visit', 18, 'V24-CVD', NULL);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('BSRC', '0', '0', 'Y', 'Visit', 19, 'V24-CVD', NULL);
