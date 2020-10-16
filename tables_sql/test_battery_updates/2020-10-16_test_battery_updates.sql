c031d38bd6fa90fe7058373decbc00428c8d4f71
Sruthy Mathew <sruthy.mcin@gmail.com>
2020-10-16 14:42:12 -0400

    Redmine 18396

--- tables_sql/test_battery_updates/2020-10-16_test_battery_updates.sql
+++ tables_sql/test_battery_updates/2020-10-16_test_battery_updates.sql
@@ -12,12 +12,6 @@ INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, Subp
 VALUES ('vrrsb', '0', '0', 'Y', 'Visit', 19, 'V24-CVD', NULL);
 
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-VALUES ('BSRC', '0', '0', 'Y', 'Visit', 18, 'V18-CVD', NULL);
-
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
-VALUES ('BSRC', '0', '0', 'Y', 'Visit', 19, 'V18-CVD', NULL);
-
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
 VALUES ('BSRC', '0', '0', 'Y', 'Visit', 18, 'V24-CVD', NULL);
 
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
