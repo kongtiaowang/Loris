f4d23a918f68111b2f29e098b771245e04a80351
SantiagoTG <54332302+SantiagoTG@users.noreply.github.com>
2020-10-20 14:22:35 -0400

    Add files via upload

---
+++ tables_sql/2020-10-20_Add_ADI-R_Proband_to_V12_and_V12-CVD.sql
@@ -0,0 +1,5 @@
+-- Addresses Redmine 18426: Add ADI-R Proband to V12 & V12-CVD for IBIS EP
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('adi_r_proband', '0', '0', 'Y', 'Visit', 18, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('adi_r_proband', '0', '0', 'Y', 'Visit', 19, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('adi_r_proband', '0', '0', 'Y', 'Visit', 18, 'V12-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('adi_r_proband', '0', '0', 'Y', 'Visit', 19, 'V12-CVD', NULL);
