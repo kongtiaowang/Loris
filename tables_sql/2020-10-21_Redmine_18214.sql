d60925e07c96ca89df3bb75b72500c1c19473203
suzanne-lee <suzannebhlee@gmail.com>
2020-10-22 19:55:31 -0400

    sql patch updates

--- tables_sql/2020-10-21_Redmine_18214.sql
+++ tables_sql/2020-10-21_Redmine_18214.sql
@@ -1,4 +1,1 @@
-DELETE FROM test_names WHERE Test_name='tsi_ds';
 DELETE FROM test_battery WHERE Test_name='tsi_ds';
-DELETE FROM instrument_subtests WHERE Test_name='tsi_ds';
-DELETE FROM flag WHERE Test_name='tsi_ds';
