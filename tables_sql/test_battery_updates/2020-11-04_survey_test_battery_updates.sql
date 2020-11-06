d9b40f845e4c0f180a869dab45904f2202167c2e
Sruthy Mathew <sruthy.mcin@gmail.com>
2020-11-06 13:12:51 -0500

    Add SRS2_Parent

--- tables_sql/test_battery_updates/2020-11-04_survey_test_battery_updates.sql
+++ tables_sql/test_battery_updates/2020-11-04_survey_test_battery_updates.sql
@@ -2,13 +2,18 @@ DELETE from survey_test_battery where test_name IN('acesubjectmedical_survey',
 'Med_Psych_History_Abbreviated_survey',
 'rbs_r') and Visit_label IN ('V06','V6-CVD');
 
--- V9 & V9-CVD, V12, V12-CVD
---  SRS-2 SA (Social Responsiveness Scale – Schoo Age) - Survey (Add) *
-
 INSERT INTO survey_test_battery (Test_name, Visit_label)
 VALUES ('SRS2_Preschool_survey','V09');
 INSERT INTO survey_test_battery (Test_name, Visit_label)
 VALUES ('SRS2_Preschool_survey','V9-CVD');
+INSERT INTO survey_test_battery (Test_name, Visit_label)
+VALUES ('SRS2_SchoolAge_Parent_proband','V09');
+INSERT INTO survey_test_battery (Test_name, Visit_label)
+VALUES ('SSRS2_SchoolAge_Parent_proband','V9-CVD');
+INSERT INTO survey_test_battery (Test_name, Visit_label)
+VALUES ('SRS2_SchoolAge_Parent_proband','V12');
+INSERT INTO survey_test_battery (Test_name, Visit_label)
+VALUES ('SSRS2_SchoolAge_Parent_proband','V12-CVD');
 
 DELETE from survey_test_battery where test_name IN('acesubjectmedical_survey',
 'mcdi_words_gestures_sentences_survey',
