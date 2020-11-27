62aa5e59893ab52aa9e25ed16ea4529fc5c4fab4
Sruthy Mathew <sruthy.mcin@gmail.com>
2020-11-27 13:48:12 -0500

    More updates

--- tables_sql/2020-11-26-test_battery_updates.sql
+++ tables_sql/2020-11-26-test_battery_updates.sql
@@ -13,3 +13,10 @@ INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, Subp
 VALUES ('adi_r_subject', '0', '0', 'Y', 'Visit', 9, 'VSA-CVD', NULL);
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
 VALUES ('adi_r_subject', '0', '0', 'Y', 'Visit', 10, 'VSA-CVD', NULL);
+
+
+-- Add neuroscreen and head measurement subject to IBIS EP HR V6-CVD
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('neuro_screen', '0', '0', 'Y', 'Visit', 18, 'V6-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('head_measurements_subject', '0', '0', 'Y', 'Visit', 18, 'V6-CVD', NULL);
