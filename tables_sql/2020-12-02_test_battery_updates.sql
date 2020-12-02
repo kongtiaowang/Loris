af9f3778a3541a37b40177856f3bfe04ae53ee48
Sruthy Mathew <sruthy.mcin@gmail.com>
2020-12-02 17:37:47 -0500

    Battery Updates (Regular & Survey)

---
+++ tables_sql/2020-12-02_test_battery_updates.sql
@@ -0,0 +1,20 @@
+-- Add MRI instruments to IBIS EP
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('EEG', '0', '0', 'Y', 'Visit', 18, 'V6-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('EEG', '0', '0', 'Y', 'Visit', 18, 'V12-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('mri_parameter_ep', '0', '0', 'Y', 'Visit', 18, 'V6-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('mri_parameter_ep', '0', '0', 'Y', 'Visit', 18, 'V12-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('mri_screening_questionnaire', '0', '0', 'Y', 'Visit', 18, 'V6-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('mri_screening_questionnaire', '0', '0', 'Y', 'Visit', 18, 'V12-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('Radiology_Review_VSA', '0', '0', 'Y', 'Visit', 18, 'V6-CVD', NULL);
+
+DELETE FROM survey_test_battery where Test_name IN ('rbs_r_proband','SRS2_SchoolAge_Parent_proband');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('rbs_r_proband','All');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('SRS2_SchoolAge_Parent_proband','All');
+
