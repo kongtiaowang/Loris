5ee1371b5088c1ef3f4f6a852aae92185d6ad3fe
nicolasbrossard <nicolasbrossard.mni@gmail.com>
2021-01-19 13:40:16 -0500

    Instruments radiology_review_infant and mock_scanner_training_form_infant not in DQT.

---
+++ tables_sql/2021-01-14_imaging_forms_ep_and_dsi_part2.sql
@@ -0,0 +1,27 @@
+-- ===============================================================
+--
+-- Creates a table for instrument radiology_review_infant.
+-- Creates a table for instrument mock_scanner_training_form_infant.
+--
+-- ===============================================================
+
+
+
+
+-- #####################################
+--
+--     radiology_review_infant
+--
+-- #####################################
+
+CREATE TABLE radiology_review_infant           LIKE Radiology_Review_VSA;
+
+
+-- #####################################
+--
+-- mock_scanner_training_form_infant
+--
+-- #####################################
+
+CREATE TABLE mock_scanner_training_form_infant LIKE Mock_Scanner_Training_Version_2;
+
