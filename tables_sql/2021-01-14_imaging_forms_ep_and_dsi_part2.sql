e2b8feb455f3ab10efc46355b44773f3cc01402e
nicolasbrossard <nicolasbrossard.mni@gmail.com>
2021-01-19 14:08:30 -0500

    SQL patch for deletion of unwanted instruments.

--- tables_sql/2021-01-14_imaging_forms_ep_and_dsi_part2.sql
+++ tables_sql/2021-01-14_imaging_forms_ep_and_dsi_part2.sql
@@ -16,6 +16,15 @@
 
 CREATE TABLE radiology_review_infant           LIKE Radiology_Review_VSA;
 
+DELETE FROM Radiology_Review_VSA
+WHERE CommentID IN (
+    SELECT f.Commentid
+    FROM flag f
+    JOIN session s ON (f.sessionid=s.id)
+    WHERE s.visit_label IN ('V06-CVD', 'V12-CVD', 'V24-CVD')
+    AND s.subprojectid IN (18,20,23)
+    AND f.test_name = 'Radiology_Review_VSA'
+);
 
 -- #####################################
 --
@@ -25,3 +34,12 @@ CREATE TABLE radiology_review_infant           LIKE Radiology_Review_VSA;
 
 CREATE TABLE mock_scanner_training_form_infant LIKE Mock_Scanner_Training_Version_2;
 
+DELETE FROM Mock_Scanner_Training_Version_2
+WHERE CommentID IN (
+    SELECT f.Commentid
+    FROM flag f
+    JOIN session s ON (f.sessionid=s.id)
+    WHERE s.visit_label IN ('V06-CVD', 'V12-CVD', 'V24-CVD')
+    AND s.subprojectid IN (18,20,23)
+    AND f.test_name = 'Mock_Scanner_Training_Version_2'
+);
