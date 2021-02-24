99699b42a016745195e7fba7b5ed71ec6c495085
Suzanne Lee <suzannelee.mcin@gmail.com>
2021-02-24 16:42:48 -0500

    visit label information SA instrument class + sql

---
+++ tables_sql/Visit_Level_Information_SA_Meta.sql
@@ -0,0 +1,12 @@
+INSERT INTO test_names (Test_name, Full_name, Sub_Group, isDirectEntry) VALUES ('Visit_Level_Information_SA', "Visit Level Information - SA", 1, 0);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Visit_Level_Information_SA','Visit_Level_Information_SA_page1', "Page 1", 1);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 18, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 18, 'VSA-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 9, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 9, 'VSA-CVD', NULL);
+
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 18, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 18, 'VSA-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 18, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 18, 'VSA-CVD', NULL);
