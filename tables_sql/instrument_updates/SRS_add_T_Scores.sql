30e52ce8bbb6fa418f11afe8074b5629fc928e35
Sruthy Mathew <sruthy.mcin@gmail.com>
2021-02-24 20:08:51 -0500

    SRS Preschool Scoring Refsctoring

---
+++ tables_sql/instrument_updates/SRS_add_T_Scores.sql
@@ -0,0 +1,7 @@
+ALTER TABLE SRS ADD COLUMN srs_t_score TEXT default NULL;
+ALTER TABLE SRS ADD COLUMN awareness_t_score TEXT default NULL;
+ALTER TABLE SRS ADD COLUMN cognition_t_score TEXT default NULL;
+ALTER TABLE SRS ADD COLUMN communication_t_score TEXT default NULL;
+ALTER TABLE SRS ADD COLUMN motivation_t_score TEXT default NULL;
+ALTER TABLE SRS ADD COLUMN mannerisms_t_score TEXT default NULL;
+ALTER TABLE SRS ADD COLUMN score_range TEXT default NULL;
