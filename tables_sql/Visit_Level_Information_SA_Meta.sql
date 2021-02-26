cf7456090998c6c53148475c024e3a6443f6d72d
suzanne-lee <suzannebhlee@gmail.com>
2021-02-26 11:21:58 -0500

    update meta

--- tables_sql/Visit_Level_Information_SA_Meta.sql
+++ tables_sql/Visit_Level_Information_SA_Meta.sql
@@ -1,12 +1,34 @@
 INSERT INTO test_names (Test_name, Full_name, Sub_Group, isDirectEntry) VALUES ('Visit_Level_Information_SA', "Visit Level Information - SA", 1, 0);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Visit_Level_Information_SA','Visit_Level_Information_SA_page1', "Page 1", 1);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 18, 'VSA', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 18, 'VSA-CVD', NULL);
+
+-- 6 month recruit 
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 1, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 1, 'VSA-CVD', NULL);
+
+-- 12 month recruit
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 2, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 2, 'VSA-CVD', NULL);
+
+-- Control
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 3, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 3, 'VSA-CVD', NULL);
+
+-- Relative
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 7, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 7, 'VSA-CVD', NULL);
+
+-- IBIS2 High Risk 
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 9, 'VSA', NULL);
 INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 9, 'VSA-CVD', NULL);
 
+-- IBIS2 Low Risk
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 10, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 10, 'VSA-CVD', NULL);
 
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 18, 'VSA', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 18, 'VSA-CVD', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 18, 'VSA', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 18, 'VSA-CVD', NULL);
+-- IBISSA LR
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 12, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 12, 'VSA-CVD', NULL);
+
+-- IBISSA HR
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 13, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_SA', '0', '0', 'Y', 'Visit', 13, 'VSA-CVD', NULL);
