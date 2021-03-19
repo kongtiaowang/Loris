d1c32650013ec0f25c0c0fdac8a1869ad0e53143
Sruthy Mathew <sruthy.mcin@gmail.com>
2021-03-19 12:26:33 -0400

    Redmine SQL Update Tasks

---
+++ tables_sql/2021-03-19_Redmine_tasks.sql
@@ -0,0 +1,20 @@
+-- Redmine 18965
+UPDATE participant_accounts
+SET status = 'Sent'
+WHERE sessionid = (
+    SELECT id
+    FROM session
+    WHERE candid=815769
+    AND visit_label='V06-CVD'
+)
+AND test_name = 'pspq_InformantReport_mom_on_dad';
+
+
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('CBCL_6_To_18','VSA-Sleep');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('MASC2_Parent','VSA-Sleep');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('MASC2_Self_Report','VSA-Sleep');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('QRS','VSA-Sleep');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('rbs_r','VSA-Sleep');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('SRS2_SchoolAge_Parent','VSA-Sleep');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('tanner_boys_survey','VSA-Sleep');
+INSERT INTO survey_test_battery (Test_name, Visit_label) VALUES ('tanner_girls_survey','VSA-Sleep');
