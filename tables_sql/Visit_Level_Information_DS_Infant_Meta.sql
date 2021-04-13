3a503fc7415c84d1849624597037b9183a1a984b
Suzanne Lee <suzannelee.mcin@gmail.com>
2021-04-13 16:46:19 -0400

    Visit level information - DS-Infant instrument class + sql files

---
+++ tables_sql/Visit_Level_Information_DS_Infant_Meta.sql
@@ -0,0 +1,18 @@
+INSERT INTO test_names (Test_name, Full_name, Sub_Group, isDirectEntry) VALUES ('Visit_Level_Information_DS_Infant', "Visit Level Information - DS-Infant", 1, 0);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('Visit_Level_Information_DS_Infant','Visit_Level_Information_DS_Infant_page1', "Page 1", 1);
+
+-- DS Infant, 20
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_DS_Infant', '0', '0', 'Y', 'Visit', 20, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_DS_Infant', '0', '0', 'Y', 'Visit', 20, 'V06-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_DS_Infant', '0', '0', 'Y', 'Visit', 20, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_DS_Infant', '0', '0', 'Y', 'Visit', 20, 'V12-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_DS_Infant', '0', '0', 'Y', 'Visit', 20, 'V24', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_DS_Infant', '0', '0', 'Y', 'Visit', 20, 'V24-CVD', NULL);
+
+-- Control DS Infant, 23
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_DS_Infant', '0', '0', 'Y', 'Visit', 23, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_DS_Infant', '0', '0', 'Y', 'Visit', 23, 'V06-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_DS_Infant', '0', '0', 'Y', 'Visit', 23, 'V12', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_DS_Infant', '0', '0', 'Y', 'Visit', 23, 'V12-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_DS_Infant', '0', '0', 'Y', 'Visit', 23, 'V24', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('Visit_Level_Information_DS_Infant', '0', '0', 'Y', 'Visit', 23, 'V24-CVD', NULL);
