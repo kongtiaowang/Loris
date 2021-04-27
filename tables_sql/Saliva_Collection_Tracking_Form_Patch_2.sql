0b524685a1b5ec40ab8f5e82921243d1604db73b
Wang Shen <kongtiaowangshen@gmail.com>
2021-04-26 21:59:26 -0400

    remove question b and rename question 6

---
+++ tables_sql/Saliva_Collection_Tracking_Form_Patch_2.sql
@@ -0,0 +1,9 @@
+alter table `Saliva_Collection_Tracking_Form` DROP COLUMN `sq4_3`;
+alter table `Saliva_Collection_Tracking_Form` DROP COLUMN `sq5_2`;
+alter table `Saliva_Collection_Tracking_Form` DROP COLUMN `pq8_3`;
+alter table `Saliva_Collection_Tracking_Form` DROP COLUMN `pq9_2`;
+alter table `Saliva_Collection_Tracking_Form` DROP COLUMN `mq4_3`;
+alter table `Saliva_Collection_Tracking_Form` DROP COLUMN `mq5_2`;
+alter table `Saliva_Collection_Tracking_Form` DROP COLUMN `fq4_3`;
+alter table `Saliva_Collection_Tracking_Form` DROP COLUMN `fq5_2`;
+
