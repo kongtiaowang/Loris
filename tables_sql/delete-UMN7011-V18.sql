cb1b2a2d3a388aa5a03fdb94a9ac253df7375ed2
Shen <kongtiaowangshen@gmail.com>
2021-04-30 15:20:49 -0400

    Update delete-UMN7011-V18.sql

--- tables_sql/delete-UMN7011-V18.sql
+++ tables_sql/delete-UMN7011-V18.sql
@@ -1,3 +1,3 @@
-DELETE FROM issues WHERE sessionID='6365';
+DELETE FROM issues WHERE sessionID = (SELECT ID FROM session WHERE CandID='856337');
 
 DELETE FROM session WHERE CandID='856337' and Visit_label='V18';
