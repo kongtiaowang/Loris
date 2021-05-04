1e0840bf0c368cbc98ea982ac8259ac6760643ea
Suzanne Lee <suzannelee.mcin@gmail.com>
2021-05-04 11:40:46 -0400

    pspq self mom scoring

--- tables_sql/2021-05-04_Redmine_18738.sql
+++ tables_sql/2021-05-04_Redmine_18738.sql
@@ -6,3 +6,12 @@ ALTER TABLE pspq_SelfReport_dad ADD `total_score` varchar(255) default NULL AFTE
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_SelfReport_dad', 'pspq_SelfReport_dad_page1', 'Page 1', 1);
 UPDATE instrument_subtests SET Description = 'Page 2', Order_number = 2 WHERE Test_name = 'pspq_SelfReport_dad' AND Subtest_name = 'pspq_SelfReport_dad_page2';
 UPDATE instrument_subtests SET Description = 'Page 3', Order_number = 3 WHERE Test_name = 'pspq_SelfReport_dad' AND Subtest_name = 'pspq_SelfReport_dad_page3';
+
+ALTER TABLE pspq_SelfReport_mom ADD `aloof_score` varchar(255) default NULL AFTER `respondent_gender`;
+ALTER TABLE pspq_SelfReport_mom ADD `prs_score` varchar(255) default NULL AFTER `aloof_score`;
+ALTER TABLE pspq_SelfReport_mom ADD `rigid_score` varchar(255) default NULL AFTER `prs_score`;
+ALTER TABLE pspq_SelfReport_mom ADD `total_score` varchar(255) default NULL AFTER `rigid_score`;
+
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_SelfReport_mom', 'pspq_SelfReport_mom_page1', 'Page 1', 1);
+UPDATE instrument_subtests SET Description = 'Page 2', Order_number = 2 WHERE Test_name = 'pspq_SelfReport_mom' AND Subtest_name = 'pspq_SelfReport_mom_page2';
+UPDATE instrument_subtests SET Description = 'Page 3', Order_number = 3 WHERE Test_name = 'pspq_SelfReport_mom' AND Subtest_name = 'pspq_SelfReport_mom_page3';
--- tables_sql/PSPQ_instruments_Meta.sql
+++ tables_sql/PSPQ_instruments_Meta.sql
@@ -4,8 +4,9 @@ INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_num
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_SelfReport_dad', 'pspq_SelfReport_dad_page3', 'Page 3', 3);
 
 INSERT INTO test_names (Test_name, Full_name, Sub_group,IsDirectEntry) VALUES ('pspq_SelfReport_mom', 'PSPQ Mom Self Report (Send to Mom)', '13','1');
-INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_SelfReport_mom', 'pspq_SelfReport_mom_page2', 'PSPQ Self Report Mom Page 2', 1);
-INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_SelfReport_mom', 'pspq_SelfReport_mom_page3', 'PSPQ Self Report Mom Page 3', 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_SelfReport_mom', 'pspq_SelfReport_mom_page1', 'Page 1', 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_SelfReport_mom', 'pspq_SelfReport_mom_page2', 'Page 2', 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_SelfReport_mom', 'pspq_SelfReport_mom_page3', 'Page 3', 3);
 
 INSERT INTO test_names (Test_name, Full_name, Sub_group,IsDirectEntry) VALUES ('pspq_InformantReport_dad_on_mom', "PSPQ Dad's Report on Mom (Send to Dad)", '13','1');
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_InformantReport_dad_on_mom', 'pspq_InformantReport_dad_on_mom_page2', "PSPQ Dad's Report on Mom Page 2", 1);
