908d85127d8718f6f4e4daba6de4c23b1cad2187
Suzanne Lee <suzannelee.mcin@gmail.com>
2021-05-04 15:59:22 -0400

    scaared adult survey

---
+++ tables_sql/SCAARED_Adult_Meta.sql
@@ -0,0 +1,7 @@
+INSERT INTO test_names (Test_name, Full_name, Sub_Group, isDirectEntry) VALUES ('SCAARED_Adult', "SCAARED (Screen for Adult Anxiety Related Disorders) - Survey", 1, 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('SCAARED_Adult','SCAARED_Adult_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('SCAARED_Adult','SCAARED_Adult_page2', "Page 2", 2);
+
+-- Fragile X at VSA
+INSERT INTO survey_test_battery (Test_name, Active, SubprojectID, Visit_label) VALUES ('SCAARED_Adult', DEFAULT, 4, 'VSA');
+INSERT INTO survey_test_battery (Test_name, Active, SubprojectID, Visit_label) VALUES ('SCAARED_Adult', DEFAULT, 5, 'VSA');
