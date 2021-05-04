88f03ae777aa5f0a1731f9a7f755b59d7b33686b
Suzanne Lee <suzannelee.mcin@gmail.com>
2021-05-04 13:47:27 -0400

    pspq informant dad on mom scoring

--- tables_sql/PSPQ_instruments_Meta.sql
+++ tables_sql/PSPQ_instruments_Meta.sql
@@ -9,9 +9,9 @@ INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_num
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_SelfReport_mom', 'pspq_SelfReport_mom_page3', 'Page 3', 3);
 
 INSERT INTO test_names (Test_name, Full_name, Sub_group,IsDirectEntry) VALUES ('pspq_InformantReport_dad_on_mom', "PSPQ Dad's Report on Mom (Send to Dad)", '13','1');
-INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_InformantReport_dad_on_mom', 'pspq_InformantReport_dad_on_mom_page2', "PSPQ Dad's Report on Mom Page 2", 1);
-INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_InformantReport_dad_on_mom', 'pspq_InformantReport_dad_on_mom_page3', "PSPQ Dad's Report on Mom Page 3", 2);
-INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_InformantReport_dad_on_mom', 'pspq_InformantReport_dad_on_mom_page4', "PSPQ Dad's Report on Mom Page 4", 3);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_InformantReport_dad_on_mom', 'pspq_InformantReport_dad_on_mom_page1', "PSPQ Dad's Report on Mom Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_InformantReport_dad_on_mom', 'pspq_InformantReport_dad_on_mom_page2', "PSPQ Dad's Report on Mom Page 2", 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_InformantReport_dad_on_mom', 'pspq_InformantReport_dad_on_mom_page3', "PSPQ Dad's Report on Mom Page 3", 3);
 
 INSERT INTO test_names (Test_name, Full_name, Sub_group,IsDirectEntry) VALUES ('pspq_InformantReport_mom_on_dad', "PSPQ Mom's Report on Dad (Send to Mom)", '13','1');
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_InformantReport_mom_on_dad', 'pspq_InformantReport_mom_on_dad_page1', "PSPQ Mom's Report on Dad Page 1", 1);
