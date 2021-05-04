fe3824d372cdefb34ee51076d712758c26f970d4
Suzanne Lee <suzannelee.mcin@gmail.com>
2021-05-04 12:39:22 -0400

    pspq informant mom on dad scoring

--- tables_sql/2021-05-04_Redmine_18738.sql
+++ tables_sql/2021-05-04_Redmine_18738.sql
@@ -15,3 +15,13 @@ ALTER TABLE pspq_SelfReport_mom ADD `total_score` varchar(255) default NULL AFTE
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_SelfReport_mom', 'pspq_SelfReport_mom_page1', 'Page 1', 1);
 UPDATE instrument_subtests SET Description = 'Page 2', Order_number = 2 WHERE Test_name = 'pspq_SelfReport_mom' AND Subtest_name = 'pspq_SelfReport_mom_page2';
 UPDATE instrument_subtests SET Description = 'Page 3', Order_number = 3 WHERE Test_name = 'pspq_SelfReport_mom' AND Subtest_name = 'pspq_SelfReport_mom_page3';
+
+ALTER TABLE pspq_InformantReport_mom_on_dad ADD `aloof_score` varchar(255) default NULL AFTER `respondent_gender`;
+ALTER TABLE pspq_InformantReport_mom_on_dad ADD `prs_score` varchar(255) default NULL AFTER `aloof_score`;
+ALTER TABLE pspq_InformantReport_mom_on_dad ADD `rigid_score` varchar(255) default NULL AFTER `prs_score`;
+ALTER TABLE pspq_InformantReport_mom_on_dad ADD `total_score` varchar(255) default NULL AFTER `rigid_score`;
+
+UPDATE test_names SET Full_name = "PSPQ Mom's Report on Dad (Send to Mom)" WHERE Test_name = 'pspq_InformantReport_mom_on_dad';
+UPDATE instrument_subtests SET Description = 'Page 1', Subtest_name = 'pspq_InformantReport_mom_on_dad_page1' WHERE Test_name = 'pspq_InformantReport_mom_on_dad' AND Order_number = 1;
+UPDATE instrument_subtests SET Description = 'Page 2', Subtest_name = 'pspq_InformantReport_mom_on_dad_page2' WHERE Test_name = 'pspq_InformantReport_mom_on_dad' AND Order_number = 2;
+UPDATE instrument_subtests SET Description = 'Page 3', Subtest_name = 'pspq_InformantReport_mom_on_dad_page3' WHERE Test_name = 'pspq_InformantReport_mom_on_dad' AND Order_number = 3;
--- tables_sql/PSPQ_instruments_Meta.sql
+++ tables_sql/PSPQ_instruments_Meta.sql
@@ -13,9 +13,9 @@ INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_num
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_InformantReport_dad_on_mom', 'pspq_InformantReport_dad_on_mom_page3', "PSPQ Dad's Report on Mom Page 3", 2);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_InformantReport_dad_on_mom', 'pspq_InformantReport_dad_on_mom_page4', "PSPQ Dad's Report on Mom Page 4", 3);
 
-INSERT INTO test_names (Test_name, Full_name, Sub_group,IsDirectEntry) VALUES ('pspq_InformantReport_mom_on_dad', "PSPQ Mom's Report on Dad(Send to Mom)", '13','1');
-INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_InformantReport_mom_on_dad', 'pspq_InformantReport_mom_on_dad_page2', "PSPQ Mom's Report on Dad Page 2", 1);
-INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_InformantReport_mom_on_dad', 'pspq_InformantReport_mom_on_dad_page3', "PSPQ Mom's Report on Dad Page 3", 2);
-INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_InformantReport_mom_on_dad', 'pspq_InformantReport_mom_on_dad_page4', "PSPQ Mom's Report on Dad Page 4", 3);
+INSERT INTO test_names (Test_name, Full_name, Sub_group,IsDirectEntry) VALUES ('pspq_InformantReport_mom_on_dad', "PSPQ Mom's Report on Dad (Send to Mom)", '13','1');
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_InformantReport_mom_on_dad', 'pspq_InformantReport_mom_on_dad_page1', "PSPQ Mom's Report on Dad Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_InformantReport_mom_on_dad', 'pspq_InformantReport_mom_on_dad_page2', "PSPQ Mom's Report on Dad Page 2", 2);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('pspq_InformantReport_mom_on_dad', 'pspq_InformantReport_mom_on_dad_page3', "PSPQ Mom's Report on Dad Page 3", 3);
 
 UPDATE test_names SET IsDirectEntry=0 where Test_name IN ('pspq_1','pspq_2');
