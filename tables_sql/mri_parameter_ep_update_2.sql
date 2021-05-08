a0669062f2985951020e565b8c3311633fb202fd
Shen <kongtiaowangshen@gmail.com>
2021-05-08 09:38:00 -0400

    Update mri_parameter_ep_update_2.sql

--- tables_sql/mri_parameter_ep_update_2.sql
+++ tables_sql/mri_parameter_ep_update_2.sql
@@ -2,4 +2,4 @@ alter table `mri_parameter_ep` MODIFY COLUMN `face_mask` varchar(255)  default N
 
 alter table `mri_parameter_ep` MODIFY COLUMN `face_shield` varchar(255)  default NULL;
 
-alter table `mri_parameter_ep` ADD COLUMN `record_medications_other` varchar(255)  default NULL;
+alter table `mri_parameter_ep` ADD COLUMN `record_medications_other` text default NULL;
