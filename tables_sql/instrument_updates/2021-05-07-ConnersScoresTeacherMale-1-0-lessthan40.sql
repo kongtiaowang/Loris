6ae3ecbf2bcee66cdd68243ee9c9af3d8f2b3b9d
Shen <kongtiaowangshen@gmail.com>
2021-05-08 11:58:49 -0400

    Update 2021-05-07-ConnersScoresTeacherMale-1-0-lessthan40.sql

--- tables_sql/instrument_updates/2021-05-07-ConnersScoresTeacherMale-1-0-lessthan40.sql
+++ tables_sql/instrument_updates/2021-05-07-ConnersScoresTeacherMale-1-0-lessthan40.sql
@@ -1,2 +1,2 @@
-UPDATE conners_teacher_lookup SET scale_T_score='>=40' WHERE scale='AG' and sex='M' and scale_raw_score IN ('0','1');
+UPDATE conners_teacher_lookup SET scale_T_score='<=40' WHERE scale='AG' and sex='M' and scale_raw_score IN ('0','1');
 
