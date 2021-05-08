f83fead86ab387c816b4151b38022260b86d9a97
Shen <kongtiaowangshen@gmail.com>
2021-05-08 10:09:23 -0400

    Update mri_parameter_ep_update_2.sql

--- tables_sql/mri_parameter_ep_update_2.sql
+++ tables_sql/mri_parameter_ep_update_2.sql
@@ -1,5 +1,14 @@
+alter table `mri_parameter_ep` Modify COLUMN `record_orientation_1` enum('','supine','leftlat','rightlat')  default NULL;
+alter table `mri_parameter_ep` Modify COLUMN `record_orientation_2` enum('','supine','leftlat','rightlat')  default NULL;
+alter table `mri_parameter_ep` Modify COLUMN `record_orientation_3` enum('','supine','leftlat','rightlat')  default NULL;
+alter table `mri_parameter_ep` Modify COLUMN `record_orientation_4` enum('','supine','leftlat','rightlat')  default NULL;
+
+alter table `mri_parameter_ep` Modify COLUMN `record_vitamine_1` enum('','left','right') default NULL;
+alter table `mri_parameter_ep` Modify COLUMN `record_vitamine_2` enum('','left','right') default NULL;
+alter table `mri_parameter_ep` Modify COLUMN `record_vitamine_3` enum('','left','right') default NULL;
+alter table `mri_parameter_ep` Modify COLUMN `record_vitamine_4` enum('','left','right') default NULL;
+
+
 alter table `mri_parameter_ep` MODIFY COLUMN `face_mask` varchar(255)  default NULL;
-
 alter table `mri_parameter_ep` MODIFY COLUMN `face_shield` varchar(255)  default NULL;
-
 alter table `mri_parameter_ep` ADD COLUMN `record_medications_other` text default NULL;
