6d781c5fe222d786c130fd23681ac53af13c8941
Suzanne Lee <suzannelee.mcin@gmail.com>
2021-05-11 13:05:38 -0400

    SCARED/SCAARED survey updates

--- tables_sql/SCARED_Child_Meta.sql
+++ tables_sql/SCARED_Child_Meta.sql
@@ -2,6 +2,10 @@ INSERT INTO test_names (Test_name, Full_name, Sub_Group, isDirectEntry) VALUES (
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('SCARED_Child','SCARED_Child_page1', "Page 1", 1);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('SCARED_Child','SCARED_Child_page2', "Page 2", 2);
 
--- Fragile X at VSA
+-- Fragile X at VSA and VSA-CVD
 INSERT INTO survey_test_battery (Test_name, Active, SubprojectID, Visit_label) VALUES ('SCARED_Child', DEFAULT, 4, 'VSA');
 INSERT INTO survey_test_battery (Test_name, Active, SubprojectID, Visit_label) VALUES ('SCARED_Child', DEFAULT, 5, 'VSA');
+INSERT INTO survey_test_battery (Test_name, Active, SubprojectID, Visit_label) VALUES ('SCARED_Child', DEFAULT, 7, 'VSA');
+INSERT INTO survey_test_battery (Test_name, Active, SubprojectID, Visit_label) VALUES ('SCARED_Child', DEFAULT, 4, 'VSA-CVD');
+INSERT INTO survey_test_battery (Test_name, Active, SubprojectID, Visit_label) VALUES ('SCARED_Child', DEFAULT, 5, 'VSA-CVD');
+INSERT INTO survey_test_battery (Test_name, Active, SubprojectID, Visit_label) VALUES ('SCARED_Child', DEFAULT, 7, 'VSA-CVD');
