7ceb0c55a5017ff0638eea89000c1d438023e364
Jordan Stirling <jstirling91@gmail.com>
2021-05-18 15:47:44 -0400

    Redmine-18581 Update all candidates' participant status to withdrawn - can not use data where their study consent is no

--- tables_sql/2021_05_18_Redmine_18581.sql
+++ tables_sql/2021_05_18_Redmine_18581.sql
@@ -5,3 +5,9 @@
 INSERT INTO `participant_status_options` (`ID`, `Description`, `Required`) VALUES
         ('14', 'Withdrawn - can use data', '0'),
         ('15', 'Withdrawn - can not use data', '0');
+
+-- Update all candidates' participant status to withdrawn - can not use data where their study consent is no.
+UPDATE candidate_consent_rel c
+    JOIN participant_status p ON (c.CandidateID=p.CandID)
+    SET p.participant_status=15
+    WHERE c.ConsentID=1 AND c.Status='no';
