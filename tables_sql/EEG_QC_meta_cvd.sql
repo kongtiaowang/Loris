03277798f1f7104e0580a65b5f3bed78608703ac
Wang Shen <kongtiaowangshen@gmail.com>
2021-05-18 18:06:55 -0400

    add cvd

---
+++ tables_sql/EEG_QC_meta_cvd.sql
@@ -0,0 +1,37 @@
+--  DS Infant(across all sub projects & visit labels)
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 20, 'V06-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 20, 'V12-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 20, 'V24-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 20, 'CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 23, 'V06-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 23, 'V12-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 23, 'V24-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 23, 'CVD', NULL);
+
+
+
+--  IBIS EP High Risk & IBIS EP (across all sub projects & visit labels)
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 18, 'V06-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 18, 'V12-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 18, 'V24-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 18, 'CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 19, 'V06-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 19, 'V12-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 19, 'V24-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 19, 'CVD', NULL);
