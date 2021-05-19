91f736c2ab018b47f627827ffdc92ea6e9a4b043
Wang Shen <kongtiaowangshen@gmail.com>
2021-05-18 22:43:20 -0400

    add dropdown for each bold section

---
+++ tables_sql/2021-05-18-mri_parameter_form_sa_update.sql
@@ -0,0 +1,4 @@
+ALTER TABLE mri_parameter_form_sa  ADD COLUMN `bold_subject_wakefulness` enum('awake','sleepy','asleep') default NULL;
+ALTER TABLE mri_parameter_form_sa  ADD COLUMN `bold_subject_wakefulness_2` enum('awake','sleepy','asleep') default NULL;
+ALTER TABLE mri_parameter_form_sa  ADD COLUMN `bold_subject_wakefulness_3` enum('awake','sleepy','asleep') default NULL;
+ALTER TABLE mri_parameter_form_sa  ADD COLUMN `bold_subject_wakefulness_4` enum('awake','sleepy','asleep') default NULL;
