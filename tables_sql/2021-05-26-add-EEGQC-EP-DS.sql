4209f7a2f8564b3a727618fa75811c6860b6a55d
Wang Shen <kongtiaowangshen@gmail.com>
2021-05-25 16:13:41 -0400

    ds and ep

---
+++ tables_sql/2021-05-26-add-EEGQC-EP-DS.sql
@@ -0,0 +1,16 @@
+--  DS Infant(across all sub projects & visit labels) And IBIS EP High Risk & IBIS EP (across all sub projects & visit labels)
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+    VALUES ('EEG_QC', '0', '0', 'Y', 'Visit', 20, 'V06-CVD', NULL),
+           ('EEG_QC', '0', '0', 'Y', 'Visit', 20, 'V12-CVD', NULL),
+           ('EEG_QC', '0', '0', 'Y', 'Visit', 20, 'V24-CVD', NULL),
+           ('EEG_QC', '0', '0', 'Y', 'Visit', 23, 'V06-CVD', NULL),
+           ('EEG_QC', '0', '0', 'Y', 'Visit', 23, 'V12-CVD', NULL),
+           ('EEG_QC', '0', '0', 'Y', 'Visit', 23, 'V24-CVD', NULL),
+           ('EEG_QC', '0', '0', 'Y', 'Visit', 23, 'VSA', NULL),
+           ('EEG_QC', '0', '0', 'Y', 'Visit', 18, 'V06-CVD', NULL),
+           ('EEG_QC', '0', '0', 'Y', 'Visit', 18, 'V12-CVD', NULL),
+           ('EEG_QC', '0', '0', 'Y', 'Visit', 18, 'V24-CVD', NULL),
+           ('EEG_QC', '0', '0', 'Y', 'Visit', 18, 'V09', NULL),
+           ('EEG_QC', '0', '0', 'Y', 'Visit', 18, 'V9-CVD', NULL),
+           ('EEG_QC', '0', '0', 'Y', 'Visit', 18, 'V18', NULL),
+           ('EEG_QC', '0', '0', 'Y', 'Visit', 18, 'V18-CVD', NULL);   
