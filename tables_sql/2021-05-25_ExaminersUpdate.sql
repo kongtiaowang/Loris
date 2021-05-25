129dbff10082dbc9ff578a23e227330230688b18
nicolasbrossard <nicolasbrossard.mni@gmail.com>
2021-05-25 18:13:32 -0400

    Certify three examiners for the

--- tables_sql/2021-05-25_ExaminersUpdate.sql
+++ tables_sql/2021-05-25_ExaminersUpdate.sql
@@ -11,3 +11,10 @@ INSERT INTO `examiners` (`full_name`, `radiologist`, `active`, `pending_approval
 
 INSERT INTO `examiners_psc_rel` (`examinerID`, `centerID`, `active`, `pending_approval`) SELECT `examinerID`, 11, 'Y', 'N' FROM `examiners` WHERE `full_name` = "Jaclyn Gunderson";
 INSERT INTO `examiners_psc_rel` (`examinerID`, `centerID`, `active`, `pending_approval`) SELECT `examinerID`, 11, 'Y', 'N' FROM `examiners` WHERE `full_name` = "Pang Chaxiong";
+
+INSERT INTO certification (examinerID                                                           , date_cert   , testID                                                           , pass)
+ VALUES                   ((SELECT examinerid FROM examiners WHERE full_name='Jaclyn Gunderson'), '2021-05-01', (SELECT ID FROM test_names WHERE test_name='MC_HOME_UMN_REVISED'), 'certified');
+INSERT INTO certification (examinerID                                                           , date_cert   , testID                                                           , pass)
+ VALUES                   ((SELECT examinerid FROM examiners WHERE full_name='Pang Chaxiong')   , '2021-05-01', (SELECT ID FROM test_names WHERE test_name='MC_HOME_UMN_REVISED'), 'certified');
+INSERT INTO certification (examinerID                                                           , date_cert   , testID                                                           , pass)
+ VALUES                   ((SELECT examinerid FROM examiners WHERE full_name='Jessica Simacek') , '2021-05-01', (SELECT ID FROM test_names WHERE test_name='MC_HOME_UMN_REVISED'), 'certified');
