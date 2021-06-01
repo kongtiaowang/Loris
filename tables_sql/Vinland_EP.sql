6c0aead6b40934b6e8c51593729f2738b322a70d
root <root@cbrain.mcgill.ca>
2021-06-01 17:16:14 -0400

    fix visit for EP and DS-Infant

---
+++ tables_sql/Vinland_EP.sql
@@ -0,0 +1,45 @@
+-- ------------------------------------------------------------------------------------
+--
+-- SQL for the Vineland_EP instrument.
+--                
+-- This new instrument is to be administered for DS-Infant at visits V6 and V6-CVD
+--
+-- ------------------------------------------------------------------------------------
+
+-- ---------------------------------------------
+-- Table structure identical to Vineland_SA
+-- --------------------------------------------
+CREATE TABLE Vineland_EP LIKE Vineland_SA;
+
+INSERT INTO test_names (Test_name, Full_name, Sub_group, TestExpert, IsDirectEntry)
+    SELECT 'Vineland_EP', 'Vineland 3 - EP (Vineland Adaptive Behavior Scales-3rd Edition - EP)', tn.Sub_group, tn.TestExpert, tn.IsDirectEntry
+    FROM test_names tn
+    WHERE tn.test_name = 'Vineland_SA';
+
+-- ---------------------------------------------------------
+-- No inserts in intrument_subtests, just like Vineland_SA
+-- ---------------------------------------------------------
+
+-- ---------------------------------------------------------------
+-- Add the Vineland_3_EP instrument to the V6 visit for
+-- subproject IDS 20 and 23
+-- ---------------------------------------------------------------
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('Vineland_EP', '0', '0', 'Y', 'Visit', 18, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('Vineland_EP', '0', '0', 'Y', 'Visit', 20, 'V06', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('Vineland_EP', '0', '0', 'Y', 'Visit', 23, 'V06', NULL);
+
+-- ------------------------------------------------------------------
+-- Add the Vineland_3_EP instrument to the V6-CVD visit for
+-- subproject IDS 20 and 23
+-- ------------------------------------------------------------------
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('Vineland_EP', '0', '0', 'Y', 'Visit', 18, 'V6-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('Vineland_EP', '0', '0', 'Y', 'Visit', 20, 'V6-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('Vineland_EP', '0', '0', 'Y', 'Visit', 23, 'V6-CVD', NULL);
