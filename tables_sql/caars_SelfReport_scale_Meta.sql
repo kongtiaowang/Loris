2ac91f6686d7f67ecb728f771700f5ca375e69af
root <root@cbrain.mcgill.ca>
2021-06-01 10:41:24 -0400

    create CAARS-SelfReport Instrument as VSA under Project=Fragile X

---
+++ tables_sql/caars_SelfReport_scale_Meta.sql
@@ -0,0 +1,8 @@
+INSERT INTO test_names (Test_name, Full_name, Sub_Group, isDirectEntry) VALUES ('caars_SelfReport_scale', "CAARS–Self-Report Scale: Short Version (CAARS–S:S)", 1, 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('caars_SelfReport_scale','caars_SelfReport_scale_page1', "Page 1", 1);
+INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('caars_SelfReport_scale','caars_SelfReport_scale_page2', "Page 2", 2);
+
+-- Fragile X at VSA
+INSERT INTO survey_test_battery (Test_name, Active, SubprojectID, Visit_label) VALUES ('caars_SelfReport_scale', DEFAULT, 4, 'VSA');
+INSERT INTO survey_test_battery (Test_name, Active, SubprojectID, Visit_label) VALUES ('caars_SelfReport_scale', DEFAULT, 5, 'VSA');
+INSERT INTO survey_test_battery (Test_name, Active, SubprojectID, Visit_label) VALUES ('caars_SelfReport_scale', DEFAULT, 7, 'VSA');
