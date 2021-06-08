d64bf840c84292bd2d0e896b3e9949992c3fbf1e
Jordan Stirling <jstirling91@gmail.com>
2021-06-08 15:46:39 -0400

    UPGRADE: bvl_feedback module overrides

--- modules/bvl_feedback/ajax/react_get_bvl_threads.php
+++ modules/bvl_feedback/ajax/react_get_bvl_threads.php
@@ -2,7 +2,7 @@
 /**
  * Used by react on initial load to load the thread state.
  *
- * PHP version 5
+ * PHP version 7
  *
  * @category Behavioural
  * @package  Main
@@ -10,6 +10,8 @@
  * @license  GPLv3 <http://www.gnu.org/licenses/gpl-3.0.en.html>
  * @link     https://www.github.com/aces/Loris-Trunk/
  */
+namespace LORIS\bvl_feedback;
+use \LORIS\StudyEntities\Candidate\CandID;
 header("content-type:application/json");
 ini_set('default_charset', 'utf-8');
 
@@ -17,34 +19,36 @@ set_include_path(
     __DIR__ . "/../../../project/libraries:" .
     __DIR__ . "/../../../php/libraries:"
 );
-//IBIS OVERRIDE CODE starts here
-require_once __DIR__ . '/../../../../vendor/autoload.php';
-//IBIS OVERRIDE CODE ends here
 
-$user     =& User::singleton();
-$username = $user->getUsername();
+require_once __DIR__ . "/../../../vendor/autoload.php";
+
+$username = \User::singleton()->getUsername();
+
 
 if (isset($_POST['candID']) && !(isset($_POST['sessionID']))) {
-    $feedbackThread =& NDB_BVL_Feedback::Singleton($username, $_POST['candID']);
+    $candID         = new CandID($_POST['candID']);
+    $feedbackThread =& \NDB_BVL_Feedback::Singleton($username, $candID);
 } elseif (isset($_POST['candID']) && isset($_POST['sessionID'])
     && !(isset($_POST['commentID']))
 ) {
+    $candID         = new CandID($_POST['candID']);
     $feedbackThread =&
-         NDB_BVL_Feedback::Singleton(
-             $username,
-             $_POST['candID'],
-             $_POST['sessionID']
-         );
+        \NDB_BVL_Feedback::Singleton(
+            $username,
+            $candID,
+            $_POST['sessionID']
+        );
 } elseif (isset($_POST['candID']) && isset($_POST['sessionID'])
     && isset($_POST['commentID'])
 ) {
+    $candID         = new CandID($_POST['candID']);
     $feedbackThread =&
-            NDB_BVL_Feedback::Singleton(
-                $username,
-                $_POST['candID'],
-                $_POST['sessionID'],
-                $_POST['commentID']
-            );
+        \NDB_BVL_Feedback::Singleton(
+            $username,
+            $candID,
+            $_POST['sessionID'],
+            $_POST['commentID']
+        );
 }
 
 $feedbackThreadList = $feedbackThread->getThreadList();
