480b6100da1dc1bf6c0033c10352e6c552d89e93
Jordan Stirling <jstirling91@gmail.com>
2021-06-09 14:51:45 -0400

    Add missing changes in upgrade

--- modules/bvl_feedback/js/bvl_feedback_panel_jquery.js
+++ modules/bvl_feedback/js/bvl_feedback_panel_jquery.js
@@ -1,23 +1,48 @@
-
 /**
  * Created by evanmcilroy on 15-06-09.
  */
 $(document).ready(function() {
-  var candID = $('meta[itemprop="candID"]').attr("context");
-  var sessionID = $('meta[itemprop="sessionID"]').attr("context");
-  var commentID = $('meta[itemprop="commentID"]').attr("context");
+  // IBIS SPECIFIC OVERRIDE CODE IN IF CASE (ELSE CASE HAS THE DEFAULT LORIS CODE
+  if (window.location.href.indexOf("instrument_list") > -1) {
 
-  $('.navbar-toggle').on('click',function(event){
+    var candID = $('meta[itemprop="candID"]').attr("context");
+    var sessionID = $('meta[itemprop="sessionID"]').attr("context");
+    var commentID = $('meta[itemprop="commentID"]').attr("context");
     $("#bvl_feedback_menu").toggleClass("active_panel");
     $("#bvl_panel_wrapper").toggleClass("bvl_panel");
+    $("#sidebar-wrapper").toggle("#sidebar-wrapper hide_sidebar");
+    $("#page_wrapper_sidebar").toggleClass("wrapper");
 
-	  //We check if a sidebar exists on the page and toggle it if such.
-	  if ($("#page_wrapper_sidebar").length){
-	    $("#sidebar-wrapper").toggle("#sidebar-wrapper hide_sidebar");
-	    $("#page_wrapper_sidebar").toggleClass("wrapper");
-	  }
-    $('.dynamictable').DynamicTable();
-  });
+    $('.navbar-toggle').on('click', function (event) {
+      $("#bvl_feedback_menu").toggleClass("active_panel");
+      $("#bvl_panel_wrapper").toggleClass("bvl_panel");
+
+
+
+      //We check if a sidebar exists on the page and toggle it if such.
+      if ($("#page_wrapper_sidebar").length) {
+        $("#sidebar-wrapper").toggle("#sidebar-wrapper hide_sidebar");
+        $("#page_wrapper_sidebar").toggleClass("wrapper");
+      }
+
+    });
+  }
+  //IBIS SPECIFIC OVERRIDE CODE ENDS HERE
+  else {
+    var candID = $('meta[itemprop="candID"]').attr("context");
+    var sessionID = $('meta[itemprop="sessionID"]').attr("context");
+    var commentID = $('meta[itemprop="commentID"]').attr("context");
+
+    $('.navbar-toggle').on('click', function (event) {
+      $("#bvl_feedback_menu").toggleClass("active_panel");
+      $("#bvl_panel_wrapper").toggleClass("bvl_panel");
+
+      //We check if a sidebar exists on the page and toggle it if such.
+      if ($("#page_wrapper_sidebar").length) {
+        $("#sidebar-wrapper").toggle("#sidebar-wrapper hide_sidebar");
+        $("#page_wrapper_sidebar").toggleClass("wrapper");
+      }
+      $('.dynamictable').DynamicTable();
+    });
+  }
 }); // end of document
-
-
