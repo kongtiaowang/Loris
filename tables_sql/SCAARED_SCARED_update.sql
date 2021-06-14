4cdc8c3b989eb36edb5847220499a6b81b94bf63
root <root@cbrain.mcgill.ca>
2021-06-14 13:13:21 -0400

    fix form naming

---
+++ tables_sql/SCAARED_SCARED_update.sql
@@ -0,0 +1,8 @@
+
+-- UPDATE TABLE NAME
+
+UPDATE test_names SET Full_name="SCAARED (Screen for Adult Anxiety Related Disorders) - Dad - Survey" WHERE Test_name = "SCAARED_Dad";
+UPDATE test_names SET Full_name="SCAARED (Screen for Adult Anxiety Related Disorders) - Mom - Survey" WHERE Test_name = "SCAARED_Mom";
+UPDATE test_names SET Full_name="SCAARED (Screen for Anxiety Related Disorders) - Child - Sibling - Survey" WHERE Test_name = "SCARED_Sibling";
+UPDATE test_names SET Full_name="SCAARED (Screen for Anxiety Related Disorders) - Child - Subject - Survey" WHERE Test_name = "SCARED_Subject";
+
