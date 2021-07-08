c8242d29e4238e18115a3446a52413135d6efc88
root <root@cbrain.mcgill.ca>
2021-07-08 16:42:11 -0400

    code scoring algorithm

---
+++ tables_sql/CCTQ_survey_alter.sql
@@ -0,0 +1,3 @@
+ALTER TABLE CCTQ_survey ADD COLUMN midsleep_point_total_derived_score varchar(255) default NULL;
+ALTER TABLE CCTQ_survey ADD COLUMN mornings_evenings_total_derived_score varchar(255) default NULL;
+ALTER TABLE CCTQ_survey ADD COLUMN chronotype_total_derived_score varchar(255) default NULL;
