de24bc43b659e242fbdbb89bd6586cf8fa683df2
Wang Shen <kongtiaowangshen@gmail.com>
2021-07-13 22:42:48 -0400

    SQL

---
+++ tables_sql/tsi_ds_infant-2021-07-13.sql
@@ -0,0 +1,7 @@
+alter table `TSI_DS_Infant` ADD COLUMN `not_finish0` varchar(10)  default NULL;
+alter table `TSI_DS_Infant` ADD COLUMN `not_finish1` varchar(10)  default NULL;
+alter table `TSI_DS_Infant` ADD COLUMN `not_finish2` varchar(10)  default NULL;
+alter table `TSI_DS_Infant` ADD COLUMN `not_finish3` varchar(10)  default NULL;
+alter table `TSI_DS_Infant` ADD COLUMN `not_finish4` varchar(10)  default NULL;
+alter table `TSI_DS_Infant` ADD COLUMN `not_finish5` varchar(10)  default NULL;
+
