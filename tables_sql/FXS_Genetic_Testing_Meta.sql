54d17d42daa21d6538f60789a7ee59097a91faa0
nicolasbrossard <nicolasbrossard.mni@gmail.com>
2021-07-19 17:02:05 -0400

    Desactivate FXS genetic testing instrument in the test battery

--- tables_sql/FXS_Genetic_Testing_Meta.sql
+++ tables_sql/FXS_Genetic_Testing_Meta.sql
@@ -2,9 +2,9 @@ INSERT INTO test_names(Test_name, Full_name, Sub_Group, isDirectEntry) VALUES('F
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('FXS_Genetic_Testing','FXS_Genetic_Testing_page1', "Page 1", 1);
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('FXS_Genetic_Testing','FXS_Genetic_Testing_page2', "Page 2", 2);
 
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FXS_Genetic_Testing', '0', '0', 'Y', 'Visit', 4, 'VSA', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FXS_Genetic_Testing', '0', '0', 'Y', 'Visit', 5, 'VSA', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FXS_Genetic_Testing', '0', '0', 'Y', 'Visit', 7, 'VSA', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FXS_Genetic_Testing', '0', '0', 'Y', 'Visit', 4, 'VSA-CVD', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FXS_Genetic_Testing', '0', '0', 'Y', 'Visit', 5, 'VSA-CVD', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FXS_Genetic_Testing', '0', '0', 'Y', 'Visit', 7, 'VSA-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FXS_Genetic_Testing', '0', '0', 'N', 'Visit', 4, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FXS_Genetic_Testing', '0', '0', 'N', 'Visit', 5, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FXS_Genetic_Testing', '0', '0', 'N', 'Visit', 7, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FXS_Genetic_Testing', '0', '0', 'N', 'Visit', 4, 'VSA-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FXS_Genetic_Testing', '0', '0', 'N', 'Visit', 5, 'VSA-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FXS_Genetic_Testing', '0', '0', 'N', 'Visit', 7, 'VSA-CVD', NULL);
