6715f46e8fd799306797766e06f7525e8aac355e
root <root@cbrain.mcgill.ca>
2021-07-20 14:03:11 -0400

    new instruments FOH

---
+++ tables_sql/FOHFamilyMedicalHistory_Meta.sql
@@ -0,0 +1,8 @@
+INSERT INTO test_names(Test_name, Full_name, Sub_Group, isDirectEntry) VALUES('FOHFamilyMedicalHistory', "FOH - Family Medical History", 1, 0);
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FOHFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 4, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FOHFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 5, 'VSA', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FOHFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 4, 'VSA-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FOHFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 5, 'VSA-CVD', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('FOHFamilyMedicalHistory', '0', '0', 'Y', 'Visit', 20, 'iDS-FOH', NULL);
+
