8dfc1a783816358beb1551cf097b78d8196e81dd
shonibare <47093379+shonibare@users.noreply.github.com>
2021-08-19 14:20:11 -0400

    2021 08 18 GitHub issue (#1931)
    * edit instruments
    
    * update form
    
    * update field
    
    * add missing fields
    
    * update instrument
    
    * restore file
    
    * update instrument
    
    Co-authored-by: root <root@cbrain.mcgill.ca>

---
+++ tables_sql/BISQ_survey_alter1.sql
@@ -0,0 +1,5 @@
+ALTER TABLE BISQ_survey ADD COLUMN while_feeding varchar(255) default NULL;
+ALTER TABLE BISQ_survey ADD COLUMN being_rocked varchar(255) default NULL;
+ALTER TABLE BISQ_survey ADD COLUMN bed_alone varchar(255) default NULL;
+ALTER TABLE BISQ_survey ADD COLUMN bed_near_parent varchar(255) default NULL;
+ALTER TABLE BISQ_survey ADD COLUMN not_answered varchar(255) default NULL;
---
+++ tables_sql/SIHI_alter1.sql
@@ -0,0 +1,6 @@
+UPDATE instrument_subtests SET Description = "I. First year of life" WHERE Test_name="SIHI" AND Order_number = 1;
+UPDATE instrument_subtests SET Description = "II. Toddler years (age 2-3)" WHERE Test_name="SIHI" AND Order_number = 2;
+UPDATE instrument_subtests SET Description = "III. Preschool/kindergarten (age 4-6)" WHERE Test_name="SIHI" AND Order_number = 3;
+UPDATE instrument_subtests SET Description = "IV. Early elementary school (grade 1-3)" WHERE Test_name="SIHI" AND Order_number = 4;
+UPDATE instrument_subtests SET Description = "V. Late elementary school (grade 4-5)" WHERE Test_name="SIHI" AND Order_number = 5;
+UPDATE instrument_subtests SET Description = "VI. Middle School" WHERE Test_name="SIHI" AND Order_number = 6;
