f0a663d899d1c896f98c1e508257e920da3e115b
shonibare <47093379+shonibare@users.noreply.github.com>
2021-08-26 11:29:01 -0400

    fix lookup table (#1939)
    * fix lookup table
    
    * change dropdown field to multiselect
    
    * patch file
    
    Co-authored-by: root <root@cbrain.mcgill.ca>

---
+++ tables_sql/BISQ_alter.sql
@@ -0,0 +1,1 @@
+ALTER TABLE BISQ CHANGE COLUMN sleep_opts sleep_opts varchar(255) default NULL;
---
+++ tables_sql/MASC2_Parent_lookup_alter.sql
@@ -0,0 +1,1 @@
+UPDATE MASC2_Parent_lookup SET scale_raw_score='<=2' WHERE scale_T_score='40' AND scale='SP' AND sex='F' AND age_years='12 to 15';
