a1cd3e07451f5271c9be28487ea288e3336489d4
Shen <kongtiaowangshen@gmail.com>
2021-09-10 16:09:49 -0400

    Update 2021-09-10-CDI_WS_percentiles_lookup.sql

--- tables_sql/2021-09-10-CDI_WS_percentiles_lookup.sql
+++ tables_sql/2021-09-10-CDI_WS_percentiles_lookup.sql
@@ -1,5 +1,5 @@
 INSERT INTO CDI_WS_percentiles_lookup VALUES
 ('27', 'female', 'word_forms', '0', '5'),
-('28', 'female', 'word_forms', '0', '5'),
-('29', 'female', 'word_forms', '0', '5'),
-('30', 'female', 'word_forms', '0', '5');
+('28', 'female', 'word_forms', '0', '0'),
+('29', 'female', 'word_forms', '0', '0'),
+('30', 'female', 'word_forms', '0', '0');
