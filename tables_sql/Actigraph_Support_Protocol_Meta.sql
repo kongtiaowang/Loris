4d4389a9dceaf3de14bae966710fbb886e9ba2ce
root <root@cbrain.mcgill.ca>
2021-09-14 14:38:12 -0400

    meta file

---
+++ tables_sql/Actigraph_Support_Protocol_Meta.sql
@@ -0,0 +1,14 @@
+INSERT INTO `test_names` (`Test_name`, `Full_name`, `Sub_group`) VALUES ('Actigraph_Support_Protocol', 'Actigraph Support Protocol', '1');
+
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('Actigraph_Support_Protocol', '0', '0', 'Y', 'Visit', 1, 'VSA-Sleep', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('Actigraph_Support_Protocol', '0', '0', 'Y', 'Visit', 2, 'VSA-Sleep', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('Actigraph_Support_Protocol', '0', '0', 'Y', 'Visit', 3, 'VSA-Sleep', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('Actigraph_Support_Protocol', '0', '0', 'Y', 'Visit', 9, 'VSA-Sleep', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('Actigraph_Support_Protocol', '0', '0', 'Y', 'Visit', 10, 'VSA-Sleep', NULL);
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID)
+VALUES ('Actigraph_Support_Protocol', '0', '0', 'Y', 'Visit', 21, 'VSA-Sleep', NULL);
