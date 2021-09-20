7832e565a9f7cc8197b32dd91541f43880277965
jstirling91 <jstirling91@gmail.com>
2021-09-20 09:51:27 -0400

    Add SQL patches for IBIS specific upgrades from 21.0 to 23.0 (#1998)

---
+++ tables_sql/Release_Patches/IBIS_22_to_23_post_upgrade.sql
@@ -0,0 +1,10 @@
+INSERT INTO permissions (code,description,categoryID) VALUES ('server_processes_manager','View and manage server processes','2');
+INSERT INTO modules (Name, Active) VALUES ('reliability', 'Y');
+INSERT INTO modules (Name, Active) VALUES ('environment_residential_history', 'Y');
+INSERT INTO modules (Name, Active) VALUES ('schedule_module', 'Y');
+INSERT INTO modules (Name, Active) VALUES ('videos', 'Y');
+INSERT INTO modules (Name, Active) VALUES ('final_radiological_review', 'Y');
+INSERT INTO modules (Name, Active) VALUES ('bobdule', 'Y');
+INSERT INTO modules (Name, Active) VALUES ('quality_control', 'Y');
+INSERT INTO modules (Name, Active) VALUES ('dqt', 'Y');
+UPDATE modules SET Active='N' WHERE Name='imaging_qc';
