113285ec630fb29265c697d5ae9d69ec567881fe
Jordan Stirling <jstirling91@gmail.com>
2021-10-06 15:07:33 -0400

    [23.0] Add patch submitted in PR-2062 to master upgrade patch

--- tables_sql/Release_Patches/IBIS_22_to_23_post_upgrade.sql
+++ tables_sql/Release_Patches/IBIS_22_to_23_post_upgrade.sql
@@ -69,3 +69,15 @@ WHERE mst.Scan_type LIKE '%EP';
 -- Activate all lines of table mri_protocol
 -- ------------------------------------------
 UPDATE mri_protocol SET active='Y';
+
+-- -------------------------------------------------------------------------------------------
+-- All instruments listed in table test_names shold have column UserID. This patch adds that
+-- column to the instrument tables that do not already have it
+-- -------------------------------------------------------------------------------------------
+
+ALTER TABLE acesubjectmedical_survey ADD COLUMN UserID VARCHAR(255) DEFAULT NULL AFTER CommentID;
+ALTER TABLE apsi_survey ADD COLUMN UserID VARCHAR(255) DEFAULT NULL AFTER CommentID;
+ALTER TABLE mcdi_words_gestures_sentences_survey ADD COLUMN UserID VARCHAR(255) DEFAULT NULL AFTER CommentID;
+ALTER TABLE rbs_ec_survey ADD COLUMN UserID VARCHAR(255) DEFAULT NULL AFTER CommentID;
+ALTER TABLE tanner_boys_survey ADD COLUMN UserID VARCHAR(255) DEFAULT NULL AFTER CommentID;
+ALTER TABLE tanner_girls_survey ADD COLUMN UserID VARCHAR(255) DEFAULT NULL AFTER CommentID;
