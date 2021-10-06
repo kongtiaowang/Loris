9fd3997346122488b99a332f1bea04e349a9d51d
Jordan Stirling <jstirling91@gmail.com>
2021-10-06 17:02:51 -0400

    [23.0] Add patch submitted in PR-2068 to master upgrade patch

--- tables_sql/Release_Patches/IBIS_22_to_23_post_upgrade.sql
+++ tables_sql/Release_Patches/IBIS_22_to_23_post_upgrade.sql
@@ -81,3 +81,17 @@ ALTER TABLE mcdi_words_gestures_sentences_survey ADD COLUMN UserID VARCHAR(255) 
 ALTER TABLE rbs_ec_survey ADD COLUMN UserID VARCHAR(255) DEFAULT NULL AFTER CommentID;
 ALTER TABLE tanner_boys_survey ADD COLUMN UserID VARCHAR(255) DEFAULT NULL AFTER CommentID;
 ALTER TABLE tanner_girls_survey ADD COLUMN UserID VARCHAR(255) DEFAULT NULL AFTER CommentID;
+
+
+-- ---------------------------------------------------------------
+-- Removes all duplicate entries in table CBCL_1_to_5_yrs_lookup
+-- ---------------------------------------------------------------
+
+DELETE FROM CBCL_1_to_5_yrs_lookup WHERE Raw=62 AND Type='Total';
+INSERT INTO CBCL_1_to_5_yrs_lookup VALUES(62,64,'Total',NULL,'Clinical Range');
+
+DELETE FROM CBCL_1_to_5_yrs_lookup WHERE Raw=63 AND Type='Total';
+INSERT INTO CBCL_1_to_5_yrs_lookup VALUES(63,65,'Total',NULL,'Clinical Range');
+
+DELETE FROM CBCL_1_to_5_yrs_lookup WHERE Raw=128 AND Type='Total';
+INSERT INTO CBCL_1_to_5_yrs_lookup VALUES(128,88,'Total',NULL,'Clinical Range');
