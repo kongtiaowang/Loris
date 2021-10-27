6d946a73990461be339e4b31968752e9c5e47eac
root <root@cbrain.mcgill.ca>
2021-10-27 10:42:04 -0400

    update scoring algorithm

---
+++ tables_sql/CSHQ_alter.sql
@@ -0,0 +1,8 @@
+ALTER TABLE CSHQ ADD COLUMN `score_bedtime_resistance_avg` varchar(10) default NULL;
+ALTER TABLE CSHQ ADD COLUMN `score_sleep_onset_delay_avg` varchar(10) default NULL;
+ALTER TABLE CSHQ ADD COLUMN `score_sleep_duration_avg` varchar(10) default NULL;
+ALTER TABLE CSHQ ADD COLUMN `score_sleep_anxiety_avg` varchar(10) default NULL;
+ALTER TABLE CSHQ ADD COLUMN `score_night_wakings_avg` varchar(10) default NULL;
+ALTER TABLE CSHQ ADD COLUMN `score_parasomnias_avg` varchar(10) default NULL;
+ALTER TABLE CSHQ ADD COLUMN `score_sleep_disordered_breathing_avg` varchar(10) default NULL;
+ALTER TABLE CSHQ ADD COLUMN `score_daytime_sleepiness_avg` varchar(10) default NULL;
