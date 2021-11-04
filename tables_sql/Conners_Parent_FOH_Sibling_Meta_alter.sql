8a469b5f90279732580a8c7536b18df1608fbc74
root <root@cbrain.mcgill.ca>
2021-11-04 12:15:45 -0400

    edit validation rule, add checkbox,remove survey from visit

---
+++ tables_sql/Conners_Parent_FOH_Sibling_Meta_alter.sql
@@ -0,0 +1,3 @@
+INSERT INTO survey_test_battery (Test_name, Active, SubprojectID, Visit_label) VALUES ('Conners_Parent_FOH_Sibling', DEFAULT, 4, 'VSA');
+INSERT INTO survey_test_battery (Test_name, Active, SubprojectID, Visit_label) VALUES ('Conners_Parent_FOH_Sibling', DEFAULT, 5, 'VSA');
+
--- tables_sql/SIHI_alter2.sql
+++ tables_sql/SIHI_alter2.sql
@@ -1,1 +1,2 @@
-ALTER TABLE SIHI ADD COLUMN not_in_middle_school varchar(20) default NULL;
+-- ALTER TABLE SIHI ADD COLUMN not_in_middle_school varchar(20) default NULL;
+ALTER TABLE SIHI ADD COLUMN not_in_elementary_school varchar(20) default NULL;
