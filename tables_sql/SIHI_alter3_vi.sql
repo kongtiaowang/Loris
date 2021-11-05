153a0b665ebe36fa15ac2752a66bfd8f83f0691e
root <root@cbrain.mcgill.ca>
2021-11-05 15:58:00 -0400

    add missing fields in page 6

---
+++ tables_sql/SIHI_alter3_vi.sql
@@ -0,0 +1,3 @@
+ALTER TABLE SIHI ADD COLUMN vi_q2_please_describe_concern varchar(100) default NULL;
+ALTER TABLE SIHI ADD COLUMN vi_q5_if_yes_select_other varchar(100) default NULL;
+ALTER TABLE SIHI ADD COLUMN vi_q5_additional_comment varchar(250) default NULL;
