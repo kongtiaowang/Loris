4948fafb4f70539f11d2d0798af264def1bbcbd1
root <root@cbrain.mcgill.ca>
2021-11-25 17:02:23 -0500

    add missing fields

---
+++ tables_sql/CDI_Words_Sentences_alter.sql
@@ -0,0 +1,5 @@
+ALTER TABLE CDI_Words_Sentences ADD part_2_40 varchar(1) NOT NULL default '';
+ALTER TABLE CDI_Words_Sentences ADD part_2_41 varchar(1) NOT NULL default '';
+ALTER TABLE CDI_Words_Sentences ADD part_2_42 varchar(1) NOT NULL default '';
+ALTER TABLE CDI_Words_Sentences ADD part_2_43 varchar(1) NOT NULL default '';
+ALTER TABLE CDI_Words_Sentences ADD part_5_70 varchar(1) NOT NULL default '';
