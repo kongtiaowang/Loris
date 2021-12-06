971772ff32dcb2c032a6899968a40c47baaf0870
Suzanne Lee <suzannelee.mcin@gmail.com>
2021-12-06 14:05:40 -0500

    add surveys to survey_test_battery for ids-foh visit

---
+++ tables_sql/2021-12-06_Github_2228.sql
@@ -0,0 +1,17 @@
+-- Github #2228
+
+INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('ADULT_SELF_REPORT_DAD','iDS-FOH', NULL);
+INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('ADULT_SELF_REPORT_MOM','iDS-FOH', NULL);
+INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('ABCL_MOM_ON_DAD','iDS-FOH', NULL);
+INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('ABCL_DAD_ON_MOM','iDS-FOH', NULL);
+INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('caars_SelfReport_dad','iDS-FOH', NULL);
+INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('caars_SelfReport_mom','iDS-FOH', NULL);
+INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('CBCL_1_To_5_survey_sibling','iDS-FOH', NULL);
+INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('CBCL_6_To_18_survey_sibling','iDS-FOH', NULL);
+INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('Conners_Parent_FOH_Sibling','iDS-FOH', NULL);
+INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('SCARED_Sibling','iDS-FOH', NULL);
+INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('SCARED_Parent_Sibling','iDS-FOH', NULL);
+INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('SCAARED_Dad','iDS-FOH', NULL);
+INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('SCAARED_Mom','iDS-FOH', NULL);
+INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('SRS2_Preschool_Sibling_survey','iDS-FOH', NULL);
+INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('SRS2_SchoolAge_Sibling','iDS-FOH', NULL);
