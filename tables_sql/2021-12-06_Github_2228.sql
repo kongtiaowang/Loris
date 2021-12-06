c9a3950263251b795ea7e437fb94c7d54dd018b2
Suzanne Lee <suzannelee.mcin@gmail.com>
2021-12-06 14:21:39 -0500

    remove duplicate

--- tables_sql/2021-12-06_Github_2228.sql
+++ tables_sql/2021-12-06_Github_2228.sql
@@ -8,7 +8,6 @@ INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('
 INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('caars_SelfReport_mom','iDS-FOH', NULL);
 INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('CBCL_1_To_5_survey_sibling','iDS-FOH', NULL);
 INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('CBCL_6_To_18_survey_sibling','iDS-FOH', NULL);
-INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('Conners_Parent_FOH_Sibling','iDS-FOH', NULL);
 INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('SCARED_Sibling','iDS-FOH', NULL);
 INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('SCARED_Parent_Sibling','iDS-FOH', NULL);
 INSERT INTO survey_test_battery (Test_name, Visit_label, SubprojectID) VALUES ('SCAARED_Dad','iDS-FOH', NULL);
