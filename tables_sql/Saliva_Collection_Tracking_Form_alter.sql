9aeeb1844845242600816b7634cfabf032c32e0c
root <root@cbrain.mcgill.ca>
2021-12-31 10:21:51 -0500

    add new fields

---
+++ tables_sql/Saliva_Collection_Tracking_Form_alter.sql
@@ -0,0 +1,8 @@
+ALTER TABLE Saliva_Collection_Tracking_Form ADD `sq3a` varchar(100) default NULL;
+ALTER TABLE Saliva_Collection_Tracking_Form ADD `sq8` varchar(100) default NULL;
+ALTER TABLE Saliva_Collection_Tracking_Form ADD `pq3a` varchar(100) default NULL;
+ALTER TABLE Saliva_Collection_Tracking_Form ADD `pq9_status` varchar(100) default NULL;
+ALTER TABLE Saliva_Collection_Tracking_Form ADD `mq4_status` varchar(100) default NULL;
+ALTER TABLE Saliva_Collection_Tracking_Form ADD `fq4_status` varchar(100) default NULL;
+ALTER TABLE Saliva_Collection_Tracking_Form ADD `other_date_delivered` varchar(100) default NULL;
+ALTER TABLE Saliva_Collection_Tracking_Form ADD `other_staff_delivered` varchar(100) default NULL;
