a0ab22e087358c40f2cca7f9f3d5849deda047d4
Suzanne Lee <suzannelee.mcin@gmail.com>
2022-01-13 17:26:12 -0500

    update patch

--- tables_sql/2022-01-10_Github_2229.sql
+++ tables_sql/2022-01-10_Github_2229.sql
@@ -1,8 +1,24 @@
 -- Github 2229
 -- Remove IBISSA and TEST IBISSA Projects
 
+UPDATE
+    session
+SET
+    ProjectID = (SELECT RegistrationProjectID FROM candidate where CandID='856269')
+WHERE
+    CandID='856269' AND
+    ProjectID = 6;
+
+UPDATE
+    session
+SET
+    ProjectID = (SELECT RegistrationProjectID FROM candidate where CandID='247315')
+WHERE
+    CandID='247315' AND
+    ProjectID = 6;
+
 -- Deletes session where ProjectID = 7 (TEST IBISSA)
-DELETE FROM session WHERE SessionID = 5788;
+DELETE FROM session WHERE ProjectID = 7;
 
 -- Deletes candidates with RegistrationProjectID = 7 (TEST IBISSA)
 DELETE FROM candidate WHERE ID IN (
