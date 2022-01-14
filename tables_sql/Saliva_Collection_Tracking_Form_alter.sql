ab7d8d73c5b92b2fd26717a611944f53aacd8666
nicolasbrossard <nicolasbrossard.mni@gmail.com>
2022-01-14 16:13:32 -0500

    Fix table definition and invalid patches for table Saliva_Collection_Tracking_Form

--- tables_sql/Saliva_Collection_Tracking_Form_alter.sql
+++ tables_sql/Saliva_Collection_Tracking_Form_alter.sql
@@ -1,5 +1,5 @@
 ALTER TABLE Saliva_Collection_Tracking_Form ADD `sq3a` varchar(100) default NULL;
-ALTER TABLE Saliva_Collection_Tracking_Form ADD `sq8` varchar(100) default NULL;
+ALTER TABLE Saliva_Collection_Tracking_Form ADD `sq8` varchar(255) default NULL;
 ALTER TABLE Saliva_Collection_Tracking_Form ADD `pq3a` varchar(100) default NULL;
 ALTER TABLE Saliva_Collection_Tracking_Form ADD `pq9_status` varchar(100) default NULL;
 ALTER TABLE Saliva_Collection_Tracking_Form ADD `mq4_status` varchar(100) default NULL;
