98af737e9c497614ab5fb7fb45ccaefbc3877bd9
Suzanne Lee <suzannelee.mcin@gmail.com>
2022-01-14 11:25:55 -0500

    remove ibissa and test ibissa project

---
+++ tables_sql/2022-01-14_Github_2229.sql
@@ -0,0 +1,17 @@
+-- Github 2229
+-- Remove IBISSA and TEST IBISSA Projects
+
+DELETE FROM candidate_consent_rel WHERE CandidateID IN (603080, 897213, 143683, 852976);
+
+DELETE FROM candidate WHERE CandID IN (
+    603080,
+    897213,
+    143683,
+    852976
+);
+
+DELETE FROM project_subproject_rel WHERE ProjectSubprojectRelID = 14;
+DELETE FROM project_subproject_rel WHERE ProjectSubprojectRelID = 15;
+
+DELETE FROM Project WHERE ProjectID = 6;
+DELETE FROM Project WHERE ProjectID = 7;
