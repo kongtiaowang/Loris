87600cd9e96a100dd35e6fd15a1289368e38d682
nicolasbrossard <nicolasbrossard.mni@gmail.com>
2022-01-14 16:25:35 -0500

    Fiux broken patch tables_sql/2022-01-10_Github_2229.sql

--- tables_sql/2022-01-10_Github_2229.sql
+++ tables_sql/2022-01-10_Github_2229.sql
@@ -20,18 +20,3 @@ WHERE
 -- Deletes session where ProjectID = 7 (TEST IBISSA)
 DELETE FROM session WHERE ProjectID = 7;
 
--- Deletes candidates with RegistrationProjectID = 7 (TEST IBISSA)
-DELETE FROM candidate WHERE ID IN (
-    1742,
-    1736,
-    2251,
-    1741
-);
-
--- Deletes rel between IBISSA Project and IBISSA HR and IBISSA LR subprojects
-DELETE FROM project_subproject_rel WHERE ID = 14;
-DELETE FROM project_subproject_rel WHERE ID = 15;
-
--- Deletes IBISSA and TEST IBISSA Projects
-DELETE FROM Project WHERE ID = 6;
-DELETE FROM Project WHERE ID = 7;
