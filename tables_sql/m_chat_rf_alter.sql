84643e137c98e5d8f60aabdc75a66f44f5b117d2
nicolasbrossard <nicolasbrossard.mni@gmail.com>
2022-01-14 16:47:33 -0500

    Fix patch tables_sql/m_chat_rf_alter.sql

--- tables_sql/m_chat_rf_alter.sql
+++ tables_sql/m_chat_rf_alter.sql
@@ -1,13 +1,10 @@
 -- remove instrument from V09 & V12 Visit Label 
-DELETE FROM test_battery WHERE Test_name='m_chat_rf' AND Visit_label='V09';
-DELETE FROM test_battery WHERE Test_name='m_chat_rf' AND Visit_label='V12';
+DELETE FROM test_battery WHERE Test_name='m_chat_rf' AND Visit_label IN ('V09', 'V9-CVD', 'V12', 'V12-CVD');
 
+DELETE FROM test_battery WHERE Test_name='m_chat_rf' AND Visit_label IN ('V06', 'V06-CVD') AND SubprojectID=20;
+
+DELETE FROM test_battery WHERE Test_name='m_chat_rf' AND Visit_label IN ('V18', 'V18-CVD') AND SubprojectID IN (20,23);
+
+DELETE FROM test_battery WHERE Test_name='m_chat_rf' AND Visit_label IN ('V24', 'V24-CVD') AND SubprojectID=18;
 
 ALTER TABLE m_chat_rf ADD `specify_recipient` enum('subject','proband') default NULL;
-
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('m_chat_rf', '0', '0', 'Y', 'Visit', 18, 'V06', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('m_chat_rf', '0', '0', 'Y', 'Visit', 23, 'V06', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('m_chat_rf', '0', '0', 'Y', 'Visit', 18, 'V18', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('m_chat_rf', '0', '0', 'Y', 'Visit', 20, 'V24', NULL);
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, SubprojectID, Visit_label, CenterID) VALUES ('m_chat_rf', '0', '0', 'Y', 'Visit', 23, 'V24', NULL);
-
