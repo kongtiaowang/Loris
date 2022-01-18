9d5855f0b7f06668bac93b3eafe7f3223f28538e
shonibare <47093379+shonibare@users.noreply.github.com>
2022-01-18 12:37:37 -0500

    patch to update mri_parameter_ep table

---
+++ tables_sql/add_puls_fields_mri_parameter_ep_alter.sql
@@ -0,0 +1,4 @@
+ALTER TABLE mri_parameter_ep ADD `puls_min` varchar(10) default NULL;
+ALTER TABLE mri_parameter_ep ADD `puls_max` varchar(10) default NULL;
+ALTER TABLE mri_parameter_ep ADD `puls_avg` varchar(10) default NULL;
+ALTER TABLE mri_parameter_ep ADD `puls_stddiff` varchar(10) default NULL;
