f1ebba5a77e376ea54015baae42126b1120324e7
Wang Shen <kongtiaowangshen@gmail.com>
2022-01-25 20:30:10 -0500

    try

--- tables_sql/2022-01-26-cbcl-6-18-patch.sql
+++
@@ -1,4 +0,0 @@
-
-ALTER TABLE CBCL_6_To_18_survey_sibling ADD COLUMN score_validity varchar(100) default NULL;
-
-ALTER TABLE CBCL_6_To_18_survey_sibling ADD COLUMN other_prob_score varchar(100) default NULL;
