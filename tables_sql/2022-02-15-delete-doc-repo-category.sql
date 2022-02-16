01456bcda3f7f371a2980cfa5e93749982796573
Wang Shen <kongtiaowangshen@gmail.com>
2022-02-15 22:10:56 -0500

    delete query

---
+++ tables_sql/2022-02-15-delete-doc-repo-category.sql
@@ -0,0 +1,8 @@
+-- Root >Conference Calls >Imaging Committee >Agendas >2021 >2022 >
+ 
+delete from document_repository_categories where id='367' and category_name='2022';
+
+-- Root >Conference Calls >Imaging Committee >Minutes >2021 >2022 >
+
+delete from document_repository_categories where id='368' and category_name='2022';
+
