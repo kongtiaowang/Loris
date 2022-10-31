45681fcdd25336501d3e382c0e99e6a613cab43b
Shen <kongtiaowangshen@gmail.com>
2022-10-31 11:56:48 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -312,7 +312,7 @@ const lorisModules = {
   server_processes_manager: ['server_processes_managerIndex'],
   instruments: ['CandidateInstrumentList'],
   candidate_profile: ['CandidateInfo'],
-  schedule_module': ['scheduleIndex'],
+  schedule_module: ['scheduleIndex'],
   api_docs: ['swagger-ui_custom'],
 };
 for (const [key] of Object.entries(lorisModules)) {
