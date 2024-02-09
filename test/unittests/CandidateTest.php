17d673fc64680c833b46584ed8e8dafd91791fcb
kongtiaowang <kongtiaowangshen@gmail.com>
2024-02-08 21:46:42 -0500

    fdsa

--- test/unittests/CandidateTest.php
+++ test/unittests/CandidateTest.php
@@ -219,10 +219,10 @@ $this->_utilityMock->expects($this->any())
         $this->_dbMock->expects($this->once())
             ->method('pselectRow')
             ->willReturn($this->_candidateInfo);
-        $this->_dbMock->expects($this->any())
+/*        $this->_dbMock->expects($this->any())
             ->method('pselectCol')
 	    ->willReturn(['Male','Female','Other']);
-
+ */
         $this->_candidate->select($this->_candidateInfo['CandID']);
 
         //validate _candidate Info
