68565832b7ba003527e2ebaddab435144c8022d6
kongtiaowang <kongtiaowangshen@gmail.com>
2024-10-16 13:36:38 -0400

    fdsa

--- test/wait-for-services.sh
+++ test/wait-for-services.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 
 cmd="$@"
 
-max_attempts=10
+max_attempts=100
 attempt=0
 
 echo "Waiting for mysqld..."
@@ -18,8 +18,10 @@ while ! mysqladmin ping -h db -u SQLTestUser --password="TestPassword" --silent;
   
   # Check if maximum attempts reached
   if [ "$attempt" -ge "$max_attempts" ]; then
-    echo "MySQL did not respond after $max_attempts attempts. Starting MySQL container..."
-    docker compose up -d db
+    echo "MySQL did not respond after $max_attempts attempts. Creating a flag for GitHub Actions..."
+    
+    # Create a flag file to signal GitHub Actions
+    echo "MySQL not started" > /tmp/mysql_error_flag
     break
   fi
 done
