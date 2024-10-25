39c4165543a2b3279d8eadffe9b46c921dd415ec
kongtiaowang <kongtiaowangshen@gmail.com>
2024-10-25 10:08:30 -0400

    test

--- modules/acknowledgements/test/AcknowledgementsTest.php
+++ modules/acknowledgements/test/AcknowledgementsTest.php
@@ -12,8 +12,8 @@
  * @link     https://github.com/aces/Loris
  */
 use Facebook\WebDriver\WebDriverBy;
- require_once __DIR__
-    . "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
+require_once __DIR__ . "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
+
 /**
  * AcknowledgementsIntegrationTest
  *
@@ -25,210 +25,162 @@ use Facebook\WebDriver\WebDriverBy;
  */
 class AcknowledgementsIntegrationTest extends LorisIntegrationTest
 {
-    //filter location
-    static $fullname     = 'input[name="fullName"]';
-    static $citationName = 'input[name="citationName"]';
-    static $startDate    = 'input[name="startDate"]';
-    static $endDate      = 'input[name="endDate"]';
-    static $present      = 'select[name="present"]';
-    static $display      = '.table-header > .row > div > div:nth-child(1)';
-    static $clearFilter  = 'a[name="reset"]';
+    // Filter locations
+    private static array $selectors = [
+        'fullname' => 'input[name="fullName"]',
+        'citationName' => 'input[name="citationName"]',
+        'startDate' => 'input[name="startDate"]',
+        'endDate' => 'input[name="endDate"]',
+        'present' => 'select[name="present"]',
+        'display' => '.table-header > .row > div > div:nth-child(1)',
+        'clearFilter' => 'a[name="reset"]',
+    ];
 
     // Initial array data
-
-    static $testData = [
-        'ID'            => '999',
-        'ordering'      => '999',
-        'full_name'     => 'Demo Test',
+    private static array $testData = [
+        'ID' => '999',
+        'ordering' => '999',
+        'full_name' => 'Demo Test',
         'citation_name' => "Demo's Citation",
-        'affiliations'  => 'mcgill',
-        'degrees'       => 'bachelors',
-        'roles'         => 'investigators',
-        'start_date'    => '2015-01-01',
-        'end_date'      => '2016-01-01',
-        'present'       => 'Yes',
+        'affiliations' => 'mcgill',
+        'degrees' => 'bachelors',
+        'roles' => 'investigators',
+        'start_date' => '2015-01-01',
+        'end_date' => '2016-01-01',
+        'present' => 'Yes',
     ];
-    static $newData  = [
-        'ordering'      => '9999',
-        'full_name'     => 'Test Test',
+
+    private static array $newData = [
+        'ordering' => '9999',
+        'full_name' => 'Test Test',
         'citation_name' => "Test's Citation",
-        'affiliations'  => 'McGill',
-        'degrees'       => 'Bachelors',
-        'roles'         => 'Investigators',
-        'start_date'    => '2016-11-11',
-        'end_date'      => '2017-11-11',
-        'present'       => 'Yes',
+        'affiliations' => 'McGill',
+        'degrees' => 'Bachelors',
+        'roles' => 'Investigators',
+        'start_date' => '2016-11-11',
+        'end_date' => '2017-11-11',
+        'present' => 'Yes',
     ];
+
     /**
-     * Insert testing data into the database
-     * author: Wang Shen
+     * Insert testing data into the database.
      *
      * @return void
      */
-    function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
-
         $this->setUpConfigSetting("citation_policy", "citation policy test text");
-
-        $this->DB->insert(
-            "acknowledgements",
-            self::$testData
-        );
-
+        $this->DB->insert("acknowledgements", self::$testData);
     }
+
     /**
-     * Delete testing data from database
-     * author: Wang Shen
+     * Delete testing data from database.
      *
      * @return void
      */
-    function tearDown(): void
+    protected function tearDown(): void
     {
         $this->restoreConfigSetting("citation_policy");
         $this->DB->delete("acknowledgements", ['ID' => '999']);
         $this->DB->delete("acknowledgements", ['full_name' => 'Test Test']);
         parent::tearDown();
     }
+
     /**
-     * Tests that, when loading the Acknowledgements module, some
-     * text appears in the body.
+     * Tests that the Acknowledgements module loads correctly with view permission.
      *
      * @return void
      */
-    function testAcknowledgementsDoespageLoadWithView()
+    function testAcknowledgementsPageLoadWithView(): void
     {
-        $this->setupPermissions(["acknowledgements_view"]);
-        $this->safeGet($this->url . "/acknowledgements/");
-        $bodyText = $this->safeFindElement(
-            WebDriverBy::cssSelector("body")
-        )->getText();
-        $this->assertStringContainsString("Acknowledgements", $bodyText);
-        $this->assertStringNotContainsString(
-            "You do not have access to this page.",
-            $bodyText
-        );
-        $this->assertStringNotContainsString(
-            "An error occured while loading the page.",
-            $bodyText
-        );
+        $this->checkPageLoad('acknowledgements_view', 'Acknowledgements');
     }
+
     /**
-     * Tests that, when loading the Acknowledgements module, some
-     * text appears in the body.
+     * Tests that the Acknowledgements module loads correctly with edit permission.
      *
      * @return void
      */
-    function testAcknowledgementsDoespageLoadwithEdit()
+    function testAcknowledgementsPageLoadWithEdit(): void
     {
-        $this->setupPermissions(["acknowledgements_edit"]);
-        $this->safeGet($this->url . "/acknowledgements/");
-        $bodyText = $this->safeFindElement(
-            WebDriverBy::cssSelector("body")
-        )->getText();
-        $this->assertStringContainsString("Acknowledgements", $bodyText);
-        $this->assertStringNotContainsString(
-            "You do not have access to this page.",
-            $bodyText
-        );
-        $this->assertStringNotContainsString(
-            "An error occured while loading the page.",
-            $bodyText
-        );
+        $this->checkPageLoad('acknowledgements_edit', 'Acknowledgements');
     }
+
     /**
-     * Ensures that the module loads if and only if the user has one of the
-     * module permissions codes.
+     * Helper function to check page load.
+     *
+     * @param string $permission
+     * @param string $expectedText
+     * @return void
+     */
+    private function checkPageLoad(string $permission, string $expectedText): void
+    {
+        $this->setupPermissions([$permission]);
+        $this->safeGet($this->url . "/acknowledgements/");
+        $bodyText = $this->safeFindElement(WebDriverBy::cssSelector("body"))->getText();
+
+        $this->assertStringContainsString($expectedText, $bodyText);
+        $this->assertStringNotContainsString("You do not have access to this page.", $bodyText);
+        $this->assertStringNotContainsString("An error occurred while loading the page.", $bodyText);
+    }
+
+    /**
+     * Ensures that the module loads if the user has one of the permissions.
      *
      * @return void
      */
     public function testPermissions(): void
     {
-        $this->checkPagePermissions(
-            '/acknowledgements/',
-            [
-                'acknowledgements_view',
-                'acknowledgements_edit'
-            ],
-            "Acknowledgements"
-        );
+        $this->checkPagePermissions('/acknowledgements/', ['acknowledgements_view', 'acknowledgements_edit'], "Acknowledgements");
     }
 
     /**
-     * Tests filter and clearfilter function
+     * Tests filter and clear filter functionality.
      *
      * @return void
      */
-    function testFilters()
+    function testFilters(): void
     {
         $this->safeGet($this->url . "/acknowledgements/");
-        $this->_filterTest(
-            self::$fullname,
-            self::$display,
-            self::$clearFilter,
-            self::$testData['full_name'],
-            "1 row"
-        );
 
-        $this->_filterTest(
-            self::$citationName,
-            self::$display,
-            self::$clearFilter,
-            self::$testData['citation_name'],
-            "1 row"
-        );
-        $this->_filterTest(
-            self::$startDate,
-            self::$display,
-            self::$clearFilter,
-            self::$testData['start_date'],
-            "1 row"
-        );
-        $this->_filterTest(
-            self::$endDate,
-            self::$display,
-            self::$clearFilter,
-            self::$testData['end_date'],
-            "1 row"
-        );
-        $this->_filterTest(
-            self::$present,
-            self::$display,
-            self::$clearFilter,
-            self::$testData['present'],
-            "30"
-        );
+        foreach (self::$selectors as $key => $selector) {
+            if (in_array($key, ['display', 'clearFilter'])) {
+                continue; // Skip display and clearFilter selectors.
+            }
+            $this->_filterTest(
+                $selector,
+                self::$selectors['display'],
+                self::$selectors['clearFilter'],
+                self::$testData[$key],
+                $key === 'present' ? "30" : "1 row"
+            );
+        }
     }
 
     /**
-     * Tests that, can't find Add Acknowledgement button on the page if
-     * user doesn't have acknowledgements_edit permission
+     * Tests that the Add Acknowledgement button is not visible without edit permission.
      *
      * @return void
      */
-    function testCantAddNewRecord()
+    function testCantAddNewRecord(): void
     {
         $this->setupPermissions(["acknowledgements_view"]);
         $this->safeGet($this->url . "/acknowledgements/");
-        $pagetext = $this->safeFindElement(
-            WebDriverBy::cssSelector("body")
-        )->getText();
-        $this->assertStringNotContainsString("Add Acknowledgement", $pagetext);
-
+        $bodyText = $this->safeFindElement(WebDriverBy::cssSelector("body"))->getText();
+        $this->assertStringNotContainsString("Add Acknowledgement", $bodyText);
     }
+
     /**
-     * Tests that, can't find Add Acknowledgement button on the page if
-     * user doesn't have acknowledgements_edit permission
+     * Tests that the citation policy setting is displayed on the page.
      *
      * @return void
      */
-    function testConfigSettingPolicy()
+    function testConfigSettingPolicy(): void
     {
         $this->safeGet($this->url . "/acknowledgements/");
-        $pagetext = $this->safeFindElement(
-            WebDriverBy::cssSelector("#citationPolicy")
-        )->getText();
-        $this->assertStringContainsString("citation policy test text", $pagetext);
+        $bodyText = $this->safeFindElement(WebDriverBy::cssSelector("#citationPolicy"))->getText();
+        $this->assertStringContainsString("citation policy test text", $bodyText);
     }
 }
 
