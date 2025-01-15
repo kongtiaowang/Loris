2c1682847cdeaf0d7792884ee8b11dbae9ec7e1a
kongtiaowang <kongtiaowangshen@gmail.com>
2025-01-15 12:44:15 -0500

    tyr

--- raisinbread/instruments/instrument_sql/Meta/mri_parameter_form_meta.sql
+++ raisinbread/instruments/instrument_sql/Meta/mri_parameter_form_meta.sql
@@ -1,3 +1,2 @@
-INSERT INTO test_names (Test_name, Full_name, Sub_group) SELECT 'mri_parameter_form', 'MRI Parameter Form', ID FROM test_subgroups WHERE Subgroup_name = 'Imaging';
-
+INSERT INTO test_names (Test_name, Full_name, Sub_group) SELECT 'mri_parameter_form', 'MRI Parameter Form', ID FROM test_subgroups WHERE ID='1';
 
