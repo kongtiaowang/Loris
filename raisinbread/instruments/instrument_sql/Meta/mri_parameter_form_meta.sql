9ebe3183ea0fcee60b417d21a94df321ad43878a
kongtiaowang <kongtiaowangshen@gmail.com>
2025-01-15 12:05:18 -0500

    try

--- raisinbread/instruments/instrument_sql/Meta/mri_parameter_form_meta.sql
+++ raisinbread/instruments/instrument_sql/Meta/mri_parameter_form_meta.sql
@@ -2,5 +2,3 @@ INSERT INTO test_names (Test_name, Full_name, Sub_group) SELECT 'mri_parameter_f
 
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('mri_parameter_form', 'mri_parameter_form_page1', 'Page 1', 1);
 
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V1', 'Y' FROM cohort WHERE title = 'Fresh';
